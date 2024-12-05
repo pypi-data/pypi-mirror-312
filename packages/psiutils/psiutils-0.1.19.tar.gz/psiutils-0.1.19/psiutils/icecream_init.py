09fb7197f47e37775583b3920bfc7b911ddeeb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:48 +0000

    Add package psiutils/0.1.19/psiutils-0.1.19.tar.gz

---
+++ packages/psiutils/psiutils-0.1.19.tar.gz/psiutils-0.1.19/psiutils/icecream_init.py
@@ -0,0 +1,28 @@
+"""
+    Set up icecream for an application.
+    This will need to be called before other modules if
+    'ic()' is used at the module level.
+
+    Usage:
+
+        from icecream_init import ic_init
+        ic_init()
+
+"""
+
+
+def ic_init():
+    try:
+        from icecream import ic, install
+        ic.configureOutput(includeContext=True)
+        install()
+
+    except ImportError:  # Graceful fallback if IceCream isn't installed.
+        def get_ic(*args):
+            if not args:
+                return None
+            if len(args) == 1:
+                return args[0]
+            return args
+        # ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)
+        ic = get_ic()
---
+++ packages/psiutils/psiutils-0.1.19.tar.gz/psiutils-0.1.19/psiutils/menus.py
@@ -0,0 +1,51 @@
+"""Menu class for Tkinter applications."""
+import tkinter as tk
+from tkinter import TclError
+
+
+class Menu(tk.Menu):
+    def __init__(self, root: tk.Tk, menu_items: list = []) -> None:
+        super().__init__(root)
+        self.menu_items = menu_items
+        for menu_item in menu_items:
+            self.add_command(label=menu_item.text, command=menu_item.command,
+                             underline= menu_item.underline,)
+
+    def enable(self, enable: bool = True) -> None:
+        enable_menu_items(self, self.menu_items, enable)
+
+
+class MenuItem():
+    def __init__(
+            self,
+            text: str,
+            command: object,
+            dimmable: bool = False,
+            **kwargs: dict,
+            ) -> None:
+
+        self.text: str = text
+        self.command: object = command
+        self.dimmable = dimmable
+        self.underline = None
+
+        if 'disabled' in kwargs:
+            if kwargs['disabled']:
+                self.disable()
+        if 'underline' in kwargs:
+            self.underline = kwargs['underline']
+
+    def __repr__(self) -> str:
+        return f'MenuItem: {self.text}'
+
+
+def enable_menu_items(menu: Menu, menu_items: list, enable: bool) -> None:
+    state = tk.NORMAL
+    if not enable:
+        state = tk.DISABLED
+    for menu_item in menu_items:
+        try:
+            if menu_item.dimmable:
+                menu.entryconfig(menu_item.text, state=state)
+        except TclError:
+            pass
