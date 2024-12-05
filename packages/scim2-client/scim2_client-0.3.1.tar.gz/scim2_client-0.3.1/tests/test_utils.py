4213963448830c726c6f591ea0b702987303b55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:36 +0000

    Add package scim2-client/0.3.1/scim2_client-0.3.1.tar.gz

---
+++ packages/scim2-client/scim2_client-0.3.1.tar.gz/scim2_client-0.3.1/tests/test_utils.py
@@ -0,0 +1,16 @@
+from scim2_models import EnterpriseUser
+from scim2_models import Group
+from scim2_models import ServiceProviderConfig
+from scim2_models import User
+
+from scim2_client.engines.httpx import SyncSCIMClient
+
+
+def test_guess_resource_endpoint():
+    client = SyncSCIMClient(None, resource_models=[User[EnterpriseUser], Group])
+    assert client.resource_endpoint(Group) == "/Groups"
+    assert client.resource_endpoint(User) == "/Users"
+    assert client.resource_endpoint(User[EnterpriseUser]) == "/Users"
+
+    # This one is special as it does not take an ending 's'
+    assert client.resource_endpoint(ServiceProviderConfig) == "/ServiceProviderConfig"
