ef9f848e552950706104813475e4fc2469e16c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:42 +0000

    Add package team3-chassis/1.1.13/team3_chassis-1.1.13.tar.gz

---
+++ packages/team3-chassis/team3_chassis-1.1.13.tar.gz/team3_chassis-1.1.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="team3_chassis",
+    version="1.1.13",
+    packages=find_packages(),
+    install_requires=[],
+    author="team3",
+    author_email="gorka.etxebarria@alumni.mondragon.edu",
+    description="A reusable library for microservices",
+)
