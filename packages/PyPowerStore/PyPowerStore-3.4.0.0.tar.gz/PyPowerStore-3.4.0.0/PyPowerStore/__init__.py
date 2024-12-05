60c06f27c3acb391e5293fe7bbd0a227273b4630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:36 +0000

    Add package PyPowerStore/3.4.0.0/PyPowerStore-3.4.0.0.tar.gz

---
+++ packages/PyPowerStore/PyPowerStore-3.4.0.0.tar.gz/PyPowerStore-3.4.0.0/PyPowerStore/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2024, Dell Technologies
+"""__init__.py."""
+
+__title__ = 'PyPowerStore'
+__version__ = '3.3.0.0'
+__author__ = 'Dell Technologies or its subsidiaries'
+__copyright__ = 'Copyright 2024 Dell Technologies'
