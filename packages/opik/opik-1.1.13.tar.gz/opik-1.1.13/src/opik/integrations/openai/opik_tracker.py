8f53d9472fc3b19ffd10a064e4b104c88f8bff9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:52 +0000

    Add package opik/1.1.13/opik-1.1.13.tar.gz

---
+++ packages/opik/opik-1.1.13.tar.gz/opik-1.1.13/src/opik/integrations/openai/opik_tracker.py
@@ -0,0 +1,47 @@
+from typing import Optional, Union
+
+import openai
+
+from . import chat_completion_chunks_aggregator, openai_decorator
+
+
+def track_openai(
+    openai_client: Union[openai.OpenAI, openai.AsyncOpenAI],
+    project_name: Optional[str] = None,
+) -> Union[openai.OpenAI, openai.AsyncOpenAI]:
+    """Adds Opik tracking to an OpenAI client.
+
+    Tracks calls to `openai_client.chat.completions.create()`, it includes support for streaming model.
+    Can be used within other Opik-tracked functions.
+
+    Args:
+        openai_client: An instance of OpenAI or AsyncOpenAI client.
+        project_name: The name of the project to log data.
+
+    Returns:
+        The modified OpenAI client with Opik tracking enabled.
+    """
+    decorator_factory = openai_decorator.OpenaiTrackDecorator()
+    if not hasattr(openai_client.chat.completions.create, "opik_tracked"):
+        completions_create_decorator = decorator_factory.track(
+            type="llm",
+            name="chat_completion_create",
+            generations_aggregator=chat_completion_chunks_aggregator.aggregate,
+            project_name=project_name,
+        )
+        openai_client.chat.completions.create = completions_create_decorator(
+            openai_client.chat.completions.create
+        )
+
+    if not hasattr(openai_client.beta.chat.completions.parse, "opik_tracked"):
+        completions_create_decorator = decorator_factory.track(
+            type="llm",
+            name="chat_completion_parse",
+            generations_aggregator=chat_completion_chunks_aggregator.aggregate,
+            project_name=project_name,
+        )
+        openai_client.beta.chat.completions.parse = completions_create_decorator(
+            openai_client.beta.chat.completions.parse
+        )
+
+    return openai_client
