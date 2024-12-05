ec9e5edd83499d7639aa6b427c3a967dd395ffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:57 +0000

    Add package tilebox-datasets/0.30.0/tilebox_datasets-0.30.0.tar.gz

---
+++ packages/tilebox-datasets/tilebox_datasets-0.30.0.tar.gz/tilebox_datasets-0.30.0/tilebox/datasets/aio/client.py
@@ -0,0 +1,24 @@
+from _tilebox.grpc.aio.channel import open_channel
+from _tilebox.grpc.aio.error import with_pythonic_errors
+from tilebox.datasets.aio.timeseries import TimeseriesDataset
+from tilebox.datasets.client import Client as BaseClient
+from tilebox.datasets.client import token_from_env
+from tilebox.datasets.datasetsv1.tilebox_pb2_grpc import TileboxServiceStub
+from tilebox.datasets.group import Group
+from tilebox.datasets.service import TileboxDatasetService
+
+
+class Client:
+    def __init__(self, *, url: str = "https://api.tilebox.com", token: str | None = None) -> None:
+        channel = open_channel(url, token_from_env(url, token))
+        service = TileboxDatasetService(with_pythonic_errors(TileboxServiceStub(channel)))
+        self._client = BaseClient(service)
+
+    async def datasets(self) -> Group:
+        return await self._client.datasets(TimeseriesDataset)
+
+    async def dataset(self, slug: str) -> TimeseriesDataset:
+        return await self._client.dataset(slug, TimeseriesDataset)
+
+    async def _dataset_by_id(self, dataset_id: str) -> TimeseriesDataset:
+        return await self._client._dataset_by_id(dataset_id, TimeseriesDataset)  # noqa: SLF001
