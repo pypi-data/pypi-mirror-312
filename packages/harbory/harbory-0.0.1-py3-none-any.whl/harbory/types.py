8eed0cded69a272e2753aa3b1bc82c5288daec49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:10 +0000

    Add package harbory/0.0.1/harbory-0.0.1-py3-none-any.whl

---
+++ packages/harbory/harbory-0.0.1-py3-none-any.whl/harbory/types.py
@@ -0,0 +1,29 @@
+import dataclasses
+from typing import Any, ClassVar, Protocol, TypeVar, Union, runtime_checkable
+
+from pydantic import BaseModel, JsonValue  # noqa: F401
+
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
+T_contra = TypeVar("T_contra", contravariant=True)
+
+DataContainer = Union["IDataclass", "INamedTuple", BaseModel, dict[str, Any]]
+
+S_DataContainer = TypeVar("S_DataContainer", bound=DataContainer)
+T_DataContainer = TypeVar("T_DataContainer", bound=DataContainer)
+
+T_NamedTuple = TypeVar("T_NamedTuple", bound="INamedTuple")
+
+
+@runtime_checkable
+class IDataclass(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, dataclasses.Field]]
+
+
+@runtime_checkable
+class INamedTuple(Protocol):
+    _fields: ClassVar[tuple[str, ...]]
+
+    def _asdict(self) -> dict[str, Any]: ...
+
+    def _replace(self: T_NamedTuple, **kwargs: Any) -> T_NamedTuple: ...
