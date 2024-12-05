10b3f019eb55f5d780358ba6bde0b3d9346e9792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:08 +0000

    Add package ta-formula/1.1.2/ta_formula-1.1.2-cp39-cp39-macosx_14_0_arm64.whl

---
+++ packages/ta-formula/ta_formula-1.1.2-cp39-cp39-macosx_14_0_arm64.whl/ta_formula/dataflow_misc.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+from .datasource import get_backend
+
+
+def prepare_arguments(datasources, datas_struct):
+    ret = []
+    for symbols, intervals in zip(zip(*datasources), datas_struct):
+        symbols_by_db = defaultdict(set)
+        for db, _symbol in symbols:
+            symbols_by_db[db].add(_symbol)
+        for db, _symbols_by_db in symbols_by_db.items():
+            ret.append((db, "prepare", list(_symbols_by_db), intervals))
+    return ret
+
+
+def parse_datasources(datasources):
+    """
+    Params:
+
+        - datasources: `[["{symbol}@{bid}", ...], ...]`
+    """
+    ret = []
+    for dss in datasources:
+        b = []
+        for ds in dss:
+            symbol, bid = ds.split("@")
+            b.append((get_backend(bid), symbol))
+        ret.append(b)
+    return ret
