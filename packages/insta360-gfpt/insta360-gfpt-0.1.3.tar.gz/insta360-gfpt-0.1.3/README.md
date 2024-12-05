bbe3de25c90487e152d33673efda05c5dca848da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:12 +0000

    Add package insta360-gfpt/0.1.3/insta360-gfpt-0.1.3.tar.gz

---
+++ packages/insta360-gfpt/insta360-gfpt-0.1.3.tar.gz/insta360-gfpt-0.1.3/README.md
@@ -0,0 +1,8 @@
+# My Python Library
+
+This is a Python library with a command-line interface.
+
+## Installation
+
+```sh
+pip install insta360-gfpt
---
+++ packages/insta360-gfpt/insta360-gfpt-0.1.3.tar.gz/insta360-gfpt-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='insta360-gfpt',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的依赖项
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_insta360_gfpt = insta360_gfpt.cli:main',
+        ],
+    },
+    author='rain',
+    author_email='chenrunming@insta360.com',
+    description='insta360 gfpt',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.insta360.com/test2/production-testing-tools/insta360_gfpt.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
