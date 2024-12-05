9b44b0cd5289d96a8b12e6c72ca15e28520f9c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:08 +0000

    Add package dify_plugin/0.0.1b35/dify_plugin-0.0.1b35.tar.gz

---
+++ packages/dify_plugin/dify_plugin-0.0.1b35.tar.gz/dify_plugin-0.0.1b35/dify_plugin/entities/model/speech2text.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from ...entities.model import BaseModelConfig, ModelType
+
+
+class Speech2TextModelConfig(BaseModelConfig):
+    """
+    Model class for speech2text model config.
+    """
+
+    model_type: ModelType = ModelType.SPEECH2TEXT
+
+    model_config = ConfigDict(protected_namespaces=())
+
+    
+class Speech2TextResult(BaseModel):
+    """
+    Model class for rerank result.
+    """
+    result: str
