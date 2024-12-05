31dacb2dda7aa1fa9addda146226227c241cdfad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:32 +0000

    Add package stimulsoft-reports/2024.4.5/stimulsoft_reports-2024.4.5-py3-none-any.whl

---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.4.5-py3-none-any.whl/stimulsoft_reports/__init__.py
@@ -0,0 +1,4 @@
+from .classes.StiHandler import StiHandler
+from .classes.StiLicenseStatic import StiLicenseStatic as StiLicense
+from .classes.StiNodeJs import StiNodeJs
+from .classes.StiResult import StiResult
---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.4.5-py3-none-any.whl/stimulsoft_reports/report/__init__.py
@@ -0,0 +1,3 @@
+from .dictionary.StiVariable import StiVariable
+from .StiPagesRange import StiPagesRange
+from .StiReport import StiReport
