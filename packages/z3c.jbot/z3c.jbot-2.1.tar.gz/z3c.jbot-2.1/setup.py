3d4016e4872f1417c86b46650b9a71a92c9ea3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:43 +0000

    Add package z3c.jbot/2.1/z3c.jbot-2.1.tar.gz

---
+++ packages/z3c.jbot/z3c.jbot-2.1.tar.gz/z3c.jbot-2.1/setup.py
@@ -0,0 +1,56 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='z3c.jbot',
+    version='2.1',
+    description="Drop-in template overrides.",
+    long_description=(open('README.rst').read() + "\n" +
+                      open('CHANGES.rst').read()),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Zope',
+        'Framework :: Zope :: 3',
+        'Framework :: Zope :: 5',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    keywords='page template override',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    url='https://github.com/zopefoundation/z3c.jbot',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.8',
+    install_requires=[
+        'setuptools',
+        'zope.pagetemplate',
+        'zope.component',
+        'zope.configuration',
+        'zope.security',
+        'zope.publisher',
+    ],
+    extras_require={
+        'test': [
+            'Zope',
+            'Products.BTreeFolder2',
+            'Products.CMFCore',
+            'plone.resource',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
