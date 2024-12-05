da2d9b114065a3555cd23f9d0f1197f6ea883bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:40 +0000

    Add package yh-olap/1.0.1/yh_olap-1.0.1.tar.gz

---
+++ packages/yh-olap/yh_olap-1.0.1.tar.gz/yh_olap-1.0.1/src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yh_olap',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'pyotp',
+        'selenium',
+        'webdriver_manager',
+        'requests_toolbelt',
+    ],
+)
