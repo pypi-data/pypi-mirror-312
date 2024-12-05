df6ee313777a65a8c931efa13ffa2991da17324c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:26 +0000

    Add package npqtools/0.1.5/npqtools-0.1.5-py3-none-any.whl

---
+++ packages/npqtools/npqtools-0.1.5-py3-none-any.whl/npqtools/__init__.py
@@ -0,0 +1,7 @@
+from problems.Clique.QuboClique import QUBOClique
+from problems.Hamiltonian.QuboHam import QUBOHam
+from problems.Knapsack.QuboKnapsack import QUBOKnapsack
+from problems.MaxWeightClique.QuboMaxWeightClique import QUBOMaxWeightClique
+from problems.Salesman.QuboSalesman import QUBOSalesman
+
+all = ['Clique', 'QuboHam', 'QuboKnapsack', 'MaxWeightClique', 'QuboSalesman']
