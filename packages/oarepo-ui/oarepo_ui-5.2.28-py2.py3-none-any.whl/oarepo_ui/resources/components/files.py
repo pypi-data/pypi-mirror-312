0809860e528e22cbf7bf633ce12635fa640a1dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:36 +0000

    Add package oarepo-ui/5.2.28/oarepo_ui-5.2.28-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.2.28-py2.py3-none-any.whl/oarepo_ui/resources/components/files.py
@@ -0,0 +1,20 @@
+from oarepo_runtime.datastreams.utils import get_file_service_for_record_service
+
+from .base import UIResourceComponent
+
+
+class FilesComponent(UIResourceComponent):
+    def before_ui_edit(self, *, api_record, extra_context, identity, **kwargs):
+        from ..resource import RecordsUIResource
+
+        if not isinstance(self.resource, RecordsUIResource):
+            return
+
+        file_service = get_file_service_for_record_service(
+            self.resource.api_service, record=api_record
+        )
+        files = file_service.list_files(identity, api_record["id"])
+        extra_context["files"] = files.to_dict()
+
+    def before_ui_detail(self, **kwargs):
+        self.before_ui_edit(**kwargs)
