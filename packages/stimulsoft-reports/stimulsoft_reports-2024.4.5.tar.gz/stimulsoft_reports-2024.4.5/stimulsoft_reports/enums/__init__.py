ae59261da4c78e5b21a147a25a85045ff46ca83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:33 +0000

    Add package stimulsoft-reports/2024.4.5/stimulsoft_reports-2024.4.5.tar.gz

---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.4.5.tar.gz/stimulsoft_reports-2024.4.5/stimulsoft_reports/enums/__init__.py
@@ -0,0 +1,7 @@
+from stimulsoft_data_adapters.enums import (StiDatabaseType, StiDataCommand, StiFrameworkType)
+
+from .Encoding import Encoding
+from .PaperKind import PaperKind
+from .StiEventType import StiEventType
+from .StiHtmlMode import StiHtmlMode
+from .StiReportType import StiReportType
