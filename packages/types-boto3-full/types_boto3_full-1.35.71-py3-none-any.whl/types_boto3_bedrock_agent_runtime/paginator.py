805888a9db065ad7ed2bdb52d1a265578ae4dea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:44 +0000

    Add package types-boto3-full/1.35.71/types_boto3_full-1.35.71-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.35.71-py3-none-any.whl/types_boto3_bedrock_agent_runtime/paginator.py
@@ -0,0 +1,85 @@
+"""
+Type annotations for bedrock-agent-runtime service client paginators.
+
+[Open documentation](https://youtype.github.io/types_boto3_docs/types_boto3_bedrock_agent_runtime/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from types_boto3_bedrock_agent_runtime.client import AgentsforBedrockRuntimeClient
+    from types_boto3_bedrock_agent_runtime.paginator import (
+        GetAgentMemoryPaginator,
+        RetrievePaginator,
+    )
+
+    session = Session()
+    client: AgentsforBedrockRuntimeClient = session.client("bedrock-agent-runtime")
+
+    get_agent_memory_paginator: GetAgentMemoryPaginator = client.get_paginator("get_agent_memory")
+    retrieve_paginator: RetrievePaginator = client.get_paginator("retrieve")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    GetAgentMemoryRequestGetAgentMemoryPaginateTypeDef,
+    GetAgentMemoryResponseTypeDef,
+    RetrieveRequestRetrievePaginateTypeDef,
+    RetrieveResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("GetAgentMemoryPaginator", "RetrievePaginator")
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class GetAgentMemoryPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/paginator/GetAgentMemory.html#AgentsforBedrockRuntime.Paginator.GetAgentMemory)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_bedrock_agent_runtime/paginators/#getagentmemorypaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[GetAgentMemoryRequestGetAgentMemoryPaginateTypeDef]
+    ) -> _PageIterator[GetAgentMemoryResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/paginator/GetAgentMemory.html#AgentsforBedrockRuntime.Paginator.GetAgentMemory.paginate)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_bedrock_agent_runtime/paginators/#getagentmemorypaginator)
+        """
+
+
+class RetrievePaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/paginator/Retrieve.html#AgentsforBedrockRuntime.Paginator.Retrieve)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_bedrock_agent_runtime/paginators/#retrievepaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[RetrieveRequestRetrievePaginateTypeDef]
+    ) -> _PageIterator[RetrieveResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/paginator/Retrieve.html#AgentsforBedrockRuntime.Paginator.Retrieve.paginate)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_bedrock_agent_runtime/paginators/#retrievepaginator)
+        """
