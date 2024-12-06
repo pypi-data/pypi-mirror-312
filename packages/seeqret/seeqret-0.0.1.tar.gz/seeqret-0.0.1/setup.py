de493862f5f069a53957a4c6ab39dc94534298b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:19 +0000

    Add package seeqret/0.0.1/seeqret-0.0.1.tar.gz

---
+++ packages/seeqret/seeqret-0.0.1.tar.gz/seeqret-0.0.1/setup.py
@@ -0,0 +1,36 @@
+"""
+Safely transferring code secrets.
+"""
+
+import setuptools
+
+version = '0.0.1'
+
+setuptools.setup(
+    name='seeqret',
+    version=version,
+    author='Bjorn',
+    url='https://github/thebjorn/seeqret.git',
+    install_requires=[
+        'rsa',
+        'cryptography',
+        'Click',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ],
+    entry_points={
+        'console_scripts': [
+            'seeqret=seeqret.main:cli',
+        ],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
