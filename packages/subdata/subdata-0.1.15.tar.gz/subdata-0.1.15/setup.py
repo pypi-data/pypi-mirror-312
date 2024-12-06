c8ad5ef687d9fdbc438237f77282e664986aa243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:44 +0000

    Add package subdata/0.1.15/subdata-0.1.15.tar.gz

---
+++ packages/subdata/subdata-0.1.15.tar.gz/subdata-0.1.15/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='subdata',  # Replace with your package’s name
+    version='0.1.15',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[ # List your dependencies here
+        'pandas',
+        'numpy',
+        'huggingface_hub',
+        'pyarrow',
+        # 'fastparquet'
+    ],
+    url = 'https://github.com/frohleon/subdata_library',
+    author='Leon Fröhling',  
+    author_email='leon.froehling@gesis.org',
+    description='A library for automatically creating targeted hate speech datasets.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+
+)
