49c0c5590ac4857780606027f0fc12547bc1a60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:11 +0000

    Add package microcotb/0.4.0/microcotb-0.4.0.tar.gz

---
+++ packages/microcotb/microcotb-0.4.0.tar.gz/microcotb-0.4.0/src/microcotb/platform/rp2040/__init__.py
@@ -0,0 +1,12 @@
+from .pin import PinWrapper
+from ..features import Features
+
+Features.SleepMsUs = True
+Features.TicksUs = True
+
+import sys 
+import io
+def exception_as_str(e:Exception):
+    buf = io.StringIO()
+    sys.print_exception(e, buf)
+    return buf.getvalue()
