f05046415aede041b6b7d8bb21b4cfa292c0cd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:08:59 +0000

    Add package dialogflow-handler/0.1.6/dialogflow_handler-0.1.6.tar.gz

---
+++ packages/dialogflow-handler/dialogflow_handler-0.1.6.tar.gz/dialogflow_handler-0.1.6/dialogflow_handler/agent.py
@@ -0,0 +1,35 @@
+import os
+import google.cloud.dialogflow as dialogflow
+from google.api_core.exceptions import DeadlineExceeded, InvalidArgument
+import uuid
+
+class Agent:
+    def __init__(self, credentials_path, project_id, language = 'en'):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+        self.project_id = project_id
+        self.language = language
+        self.session_id = str(uuid.uuid4())
+        self.session_client = dialogflow.SessionsClient()
+        self.session = self.session_client.session_path(self.project_id, self.session_id)
+
+    def get_response(self, query, timeout=5):
+        self.text_input = dialogflow.TextInput(text=query, language_code=self.language)
+        self.query_input = dialogflow.QueryInput(text=self.text_input)
+        try:
+            self.response = self.session_client.detect_intent(session=self.session,
+                                                              query_input=self.query_input,
+                                                              timeout=timeout)
+            self.update()
+        except DeadlineExceeded:
+            return "DeadlineExceeded"
+        except InvalidArgument:
+            return "InvalidArgument"
+        except Exception as e:
+            return f"An unexpected error occurred: {str(e)}"
+
+    def update(self):
+        self.query_text = self.response.query_result.query_text
+        self.detected_intent = self.response.query_result.intent.display_name
+        self.detected_intent_confidence = self.response.query_result.intent_detection_confidence 
+        self.fulfillment_text = self.response.query_result.fulfillment_text
+        self.fulfillment_message = self.response.query_result.fulfillment_messages
---
+++ packages/dialogflow-handler/dialogflow_handler-0.1.6.tar.gz/dialogflow_handler-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dialogflow-handler',
+    version='0.1.6',
+    description='Another Dialogflow API wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Vince Swu',
+    author_email='vinceswu88@gmail.com',
+    url='https://github.com/ecnivs/dialogflow-handler',
+    packages=find_packages(),
+    install_requires=[
+        'google-cloud-dialogflow',
+        'google-api-core'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
