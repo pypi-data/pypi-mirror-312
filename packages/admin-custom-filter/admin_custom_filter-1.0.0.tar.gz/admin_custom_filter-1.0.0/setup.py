6788116b97dcc71a338c90e1e0ee96a899448eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:13 +0000

    Add package admin-custom-filter/1.0.0/admin_custom_filter-1.0.0.tar.gz

---
+++ packages/admin-custom-filter/admin_custom_filter-1.0.0.tar.gz/admin_custom_filter-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="admin_custom_filter",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['templates/admin/*']},
+    install_requires=[
+        'bleach>=6.2.0'
+    ],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Abdulaziz Baqaleb",
+    author_email="ab.ah.bq@gmail.com",
+    maintainer="Abdulaziz Baqaleb",
+    maintainer_email="ab.ah.bq@gmail.com",
+    license="MIT License"
+)
