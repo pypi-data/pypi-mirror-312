a7538994a1991187ddcc29dc0cc38af50a73bc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:49 +0000

    Add package TeamWebQaUPT/1.1.3/teamwebqaupt-1.1.3.tar.gz

---
+++ packages/TeamWebQaUPT/teamwebqaupt-1.1.3.tar.gz/teamwebqaupt-1.1.3/TeamWebQaUPT/conftest.py
@@ -0,0 +1,1 @@
+from TeamWebQaUPT.webdriver_config import driver  
---
+++ packages/TeamWebQaUPT/teamwebqaupt-1.1.3.tar.gz/teamwebqaupt-1.1.3/TeamWebQaUPT/webdriver_config.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
+def get_driver(browser_name):
+    """
+    Devuelve una instancia de WebDriver remoto.
+    """
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        driver = webdriver.Remote(
+            command_executor='http://localhost:4444',
+            options=options
+        )
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        driver = webdriver.Remote(
+            command_executor='http://localhost:4444',
+            options=options
+        )
+    elif browser_name == "edge":
+        options = EdgeOptions()
+        driver = webdriver.Remote(
+            command_executor='http://localhost:4444',
+            options=options
+        )
+    else:
+        raise ValueError(f"Navegador '{browser_name}' no es compatible.")
+    
+    driver.maximize_window()
+    return driver
+
+@pytest.fixture(scope="session", params=["chrome", "firefox", "edge"])
+def driver(request):
+   
+    driver = get_driver(request.param)
+    yield driver
+    driver.quit()
