a8a4e043b8e55a2091c8f6662e0e8e6196d00edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:15 +0000

    Add package quantflow/0.2.8/quantflow-0.2.8-py3-none-any.whl

---
+++ packages/quantflow/quantflow-0.2.8-py3-none-any.whl/quantflow/utils/types.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from typing import Optional, Union
+
+import pandas as pd
+import numpy as np
+import numpy.typing as npt
+
+Number = Decimal
+Numbers = Union[int, float, np.number]
+NumberType = Union[float, int, str, Number]
+Vector = Union[int, float, complex, np.ndarray, pd.Series]
+FloatArray = npt.NDArray[np.float64]
+IntArray = npt.NDArray[np.int_]
+FloatArrayLike = FloatArray | float
+
+
+def as_number(num: Optional[NumberType] = None) -> Number:
+    return Number(0 if num is None else str(num))
+
+
+def as_float(num: Optional[NumberType] = None) -> float:
+    return float(0 if num is None else num)
+
+
+def as_array(n: Vector) -> np.ndarray:
+    """Convert an input into an array"""
+    if isinstance(n, int):
+        return np.arange(n)
+    else:
+        return np.asarray(n)
