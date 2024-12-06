ee0ad03e6e4ec4b42e65573f32710cc3f9a3b9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:32 +0000

    Add package dummy-data-generator-id/0.2.3/dummy-data-generator-id-0.2.3.tar.gz

---
+++ packages/dummy-data-generator-id/dummy-data-generator-id-0.2.3.tar.gz/dummy-data-generator-id-0.2.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dummy-data-generator-id",
+    version="0.2.3",
+    author="Brian Adi",
+    author_email="uix.brianadi@gmail.com",
+    description="A library for generating realistic dummy data for Indonesian context",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bri-anadi/dummy-data-generator-id",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'uuid'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.7",
+    keywords="dummy-data generator indonesia random",
+)
