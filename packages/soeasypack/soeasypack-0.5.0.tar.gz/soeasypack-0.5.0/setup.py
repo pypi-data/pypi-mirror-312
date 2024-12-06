da134b5006d39ed5b90f6f0b196b0d3c727f5b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:03:39 +0000

    Add package soeasypack/0.5.0/soeasypack-0.5.0.tar.gz

---
+++ packages/soeasypack/soeasypack-0.5.0.tar.gz/soeasypack-0.5.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8')as fp:
+      long_description = fp.read()
+
+setup(name='soeasypack',
+      description='简易精准打包python项目和依赖环境',
+      author='xmqsvip',
+      author_email='xmqsvip@qq.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      package_data={
+      'soeasypack': ['dep_exe/**/*']},
+      include_package_data=True,
+      license='MIT',
+      version='0.5.0',
+      zip_safe=False,
+      url = "https://github.com/XMQSVIP/SoEasyPack",
+      classifiers=[
+            'Programming Language :: Python :: 3',
+      ],
+      )
