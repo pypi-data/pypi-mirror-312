e2c12ff365d905f8cdf98da52b6fc5a23dae0aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:39 +0000

    Add package nonebot-plugin-osubot/6.13.1/nonebot_plugin_osubot-6.13.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-6.13.1-py3-none-any.whl/nonebot_plugin_osubot/matcher/url_match.py
@@ -0,0 +1,19 @@
+from nonebot import on_regex
+from nonebot.typing import T_State
+from nonebot.params import RegexGroup
+from nonebot_plugin_alconna import UniMessage
+
+from ..draw import draw_map_info
+
+url_match = on_regex("https://osu.ppy.sh/beatmapsets/([0-9]+)#([^/]+/[0-9]+)")
+url_1 = "https://osu.direct/api/d/"
+url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
+
+
+@url_match.handle()
+async def _url(state: T_State, bid: tuple = RegexGroup()):
+    beatmap_id = bid[1].split("/")[1]
+    url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
+    state["target"] = beatmap_id
+    m = await draw_map_info(beatmap_id, [])
+    await (UniMessage.image(raw=m) + "\n" + url_total).finish(reply_to=True)
