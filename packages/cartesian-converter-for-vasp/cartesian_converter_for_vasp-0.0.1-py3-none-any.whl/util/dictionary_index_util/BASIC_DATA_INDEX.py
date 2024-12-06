b481f846815e005d13ec893183dbb44ebb22ae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:05 +0000

    Add package cartesian-converter-for-vasp/0.0.1/cartesian_converter_for_vasp-0.0.1-py3-none-any.whl

---
+++ packages/cartesian-converter-for-vasp/cartesian_converter_for_vasp-0.0.1-py3-none-any.whl/util/dictionary_index_util/BASIC_DATA_INDEX.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class BasicDataIndex(Enum):
+    NAME            = 'name'
+    SCALING_FACTOR  = 'scaling_factor'
+    LATTICE_VECTORS = 'lattice_vectors'
+    ATOM_STAT       = 'atom_stat'
