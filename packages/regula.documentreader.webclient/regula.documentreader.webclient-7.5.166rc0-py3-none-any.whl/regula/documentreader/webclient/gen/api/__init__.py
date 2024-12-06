985d7fbdf2ec44dc8abf766a184bdffe155e95a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:48 +0000

    Add package regula.documentreader.webclient/7.5.166rc0/regula.documentreader.webclient-7.5.166rc0-py3-none-any.whl

---
+++ packages/regula.documentreader.webclient/regula.documentreader.webclient-7.5.166rc0-py3-none-any.whl/regula/documentreader/webclient/gen/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from regula.documentreader.webclient.gen.api.default_api import DefaultApi
+from regula.documentreader.webclient.gen.api.process_api import ProcessApi
+from regula.documentreader.webclient.gen.api.transaction_api import TransactionApi
