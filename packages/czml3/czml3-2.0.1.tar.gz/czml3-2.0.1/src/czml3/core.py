b921a7f4b363f0c3460bac9622a2208423f04418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:42 +0000

    Add package czml3/2.0.1/czml3-2.0.1.tar.gz

---
+++ packages/czml3/czml3-2.0.1.tar.gz/czml3-2.0.1/src/czml3/core.py
@@ -0,0 +1,88 @@
+from typing import Any
+from uuid import uuid4
+
+from pydantic import Field, model_serializer
+
+from czml3.types import StringValue
+
+from .base import BaseCZMLObject
+from .properties import (
+    Billboard,
+    Box,
+    Clock,
+    Corridor,
+    Cylinder,
+    Ellipse,
+    Ellipsoid,
+    Label,
+    Model,
+    Orientation,
+    Path,
+    Point,
+    Polygon,
+    Polyline,
+    Position,
+    Rectangle,
+    Tileset,
+    ViewFrom,
+    Wall,
+)
+from .types import IntervalValue, Sequence, TimeInterval
+
+CZML_VERSION = "1.0"
+
+
+class Preamble(BaseCZMLObject):
+    """The preamble packet."""
+
+    id: str = Field(default="document")
+    version: str = Field(default=CZML_VERSION)
+    name: None | str = Field(default=None)
+    description: None | str = Field(default=None)
+    clock: None | Clock | IntervalValue = Field(default=None)
+
+
+class Packet(BaseCZMLObject):
+    """A CZML Packet.
+
+    See https://github.com/AnalyticalGraphicsInc/czml-writer/wiki/Packet
+    for further information.
+    """
+
+    id: str = Field(default_factory=lambda _: str(uuid4()))
+    delete: None | bool = Field(default=None)
+    name: None | str = Field(default=None)
+    parent: None | str = Field(default=None)
+    description: None | str | StringValue = Field(default=None)
+    availability: None | TimeInterval | list[TimeInterval] | Sequence = Field(
+        default=None
+    )
+    properties: None | Any = Field(default=None)
+    position: None | Position = Field(default=None)
+    orientation: None | Orientation = Field(default=None)
+    viewFrom: None | ViewFrom = Field(default=None)
+    billboard: None | Billboard = Field(default=None)
+    box: None | Box = Field(default=None)
+    corridor: None | Corridor = Field(default=None)
+    cylinder: None | Cylinder = Field(default=None)
+    ellipse: None | Ellipse = Field(default=None)
+    ellipsoid: None | Ellipsoid = Field(default=None)
+    label: None | Label = Field(default=None)
+    model: None | Model = Field(default=None)
+    path: None | Path = Field(default=None)
+    point: None | Point = Field(default=None)
+    polygon: None | Polygon = Field(default=None)
+    polyline: None | Polyline = Field(default=None)
+    rectangle: None | Rectangle = Field(default=None)
+    tileset: None | Tileset = Field(default=None)
+    wall: None | Wall = Field(default=None)
+
+
+class Document(BaseCZMLObject):
+    """A CZML document, consisting on a list of packets."""
+
+    packets: list[Packet | Preamble]
+
+    @model_serializer
+    def custom_serializer(self):
+        return list(self.packets)
---
+++ packages/czml3/czml3-2.0.1.tar.gz/czml3-2.0.1/tests/test_types.py
@@ -0,0 +1,434 @@
+import datetime as dt
+
+import astropy.time
+import pytest
+from pydantic import ValidationError
+
+from czml3.types import (
+    Cartesian2Value,
+    Cartesian3Value,
+    CartographicDegreesListValue,
+    CartographicDegreesValue,
+    CartographicRadiansListValue,
+    CartographicRadiansValue,
+    DistanceDisplayConditionValue,
+    EpochValue,
+    FontValue,
+    IntervalValue,
+    NearFarScalarValue,
+    NumberValue,
+    ReferenceValue,
+    RgbafValue,
+    RgbaValue,
+    TimeInterval,
+    UnitQuaternionValue,
+    check_reference,
+    format_datetime_like,
+)
+
+
+def test_invalid_near_far_scalar_value():
+    with pytest.raises(TypeError) as excinfo:
+        NearFarScalarValue(values=[0, 3.2, 1, 4, 2, 1])
+
+    assert "Input values must have either 4 or N * 5 values, " in excinfo.exconly()
+
+
+def test_distance_display_condition_is_invalid():
+    with pytest.raises(TypeError):
+        DistanceDisplayConditionValue(
+            values=[0, 150, 15000000, 300, 10000, 15000000, 600]
+        )
+
+
+def test_distance_display_condition():
+    expected_result = """[
+    0.0,
+    150.0,
+    15000000.0,
+    300.0,
+    10000.0,
+    15000000.0,
+    600.0,
+    150.0,
+    15000000.0
+]"""
+    dist = DistanceDisplayConditionValue(
+        values=[0, 150, 15000000, 300, 10000, 15000000, 600, 150, 15000000]
+    )
+    assert str(dist) == expected_result
+
+
+def test_cartographic_radian_list():
+    expected_result = """[
+    0.0,
+    1.0,
+    0.0
+]"""
+    car = CartographicRadiansListValue(values=[0, 1, 0])
+    assert str(car) == expected_result
+
+
+def test_invalid_cartograpic_radian_list():
+    with pytest.raises(TypeError) as excinfo:
+        CartographicRadiansListValue(values=[1])
+    assert (
+        "Invalid values. Input values should be arrays of size 3 * N"
+        in excinfo.exconly()
+    )
+
+
+def test_cartograpic_degree_list():
+    expected_result = """[
+    15.0,
+    25.0,
+    50.0
+]"""
+    car = CartographicDegreesListValue(values=[15, 25, 50])
+    assert str(car) == expected_result
+
+
+def test_invalid_cartograpic_degree_list():
+    with pytest.raises(TypeError) as excinfo:
+        CartographicDegreesListValue(values=[15, 25, 50, 30])
+    assert (
+        "Invalid values. Input values should be arrays of size 3 * N"
+        in excinfo.exconly()
+    )
+
+
+@pytest.mark.parametrize("values", [[2, 2], [5, 5, 5, 5, 5]])
+def test_bad_cartesian3_raises_error(values):
+    with pytest.raises(TypeError) as excinfo:
+        Cartesian3Value(values=values)
+
+    assert "Input values must have either 3 or N * 4 values" in excinfo.exconly()
+    assert str(Cartesian3Value()) == "[]"
+
+
+@pytest.mark.parametrize("values", [[2, 2, 2, 2, 2], [5, 5, 5, 5, 5]])
+def test_bad_cartesian2_raises_error(values):
+    with pytest.raises(TypeError) as excinfo:
+        Cartesian2Value(values=values)
+
+    assert "Input values must have either 2 or N * 3 values" in excinfo.exconly()
+    assert str(Cartesian2Value()) == "{}"
+
+
+def test_reference_value():
+    expected_result = '"id#property"'
+    reference = ReferenceValue(string="id#property")
+
+    assert str(reference) == expected_result
+
+
+def test_invalid_reference_value():
+    with pytest.raises(TypeError) as excinfo:
+        ReferenceValue(string="id")
+
+    assert (
+        "Invalid reference string format. Input must be of the form id#property"
+        in excinfo.exconly()
+    )
+
+
+def test_font_value():
+    expected_result = '"20px sans-serif"'
+    font = FontValue(font="20px sans-serif")
+
+    assert str(font) == expected_result
+
+
+def test_font_property_value():
+    expected_result = "20px sans-serif"
+    font = FontValue(font="20px sans-serif")
+
+    assert font.font == expected_result
+
+
+def test_bad_rgba_size_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbaValue(values=[0, 0, 255])
+
+    assert "Input values must have either 4 or N * 5 values, " in excinfo.exconly()
+
+
+def test_bad_rgba_4_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbaValue(values=[256, 0, 0, 255])
+
+    assert "Color values must be integers in the range 0-255." in excinfo.exconly()
+
+
+def test_bad_rgba_5_color_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbaValue(values=[0, 0.1, 0.3, 0.3, 256])
+
+    assert "Color values must be integers in the range 0-255." in excinfo.exconly()
+
+
+def test_bad_rgbaf_size_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbafValue(values=[0, 0, 0.1])
+
+    assert "Input values must have either 4 or N * 5 values, " in excinfo.exconly()
+
+
+def test_bad_rgbaf_4_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbafValue(values=[0.3, 0, 0, 1.4])
+
+    assert "Color values must be floats in the range 0-1." in excinfo.exconly()
+
+
+def test_bad_rgbaf_5_color_values_raises_error():
+    with pytest.raises(TypeError) as excinfo:
+        RgbafValue(values=[0, 0.1, 0.3, 0.3, 255])
+
+    assert "Color values must be floats in the range 0-1." in excinfo.exconly()
+
+
+def test_default_time_interval():
+    expected_result = '"0001-01-01T00:00:00Z/9999-12-31T23:59:59Z"'
+    time_interval = TimeInterval()
+
+    assert str(time_interval) == expected_result
+
+
+def test_bad_time_raises_error():
+    with pytest.raises(ValueError):
+        format_datetime_like("2019/01/01")
+
+
+def test_interval_value():
+    start = "2019-01-01T12:00:00.000000Z"
+    end = "2019-09-02T21:59:59.000000Z"
+
+    # value is a boolean
+    assert (
+        str(IntervalValue(start=start, end=end, value=True))
+        == """{
+    "interval": "2019-01-01T12:00:00.000000Z/2019-09-02T21:59:59.000000Z",
+    "boolean": true
+}"""
+    )
+
+    assert (
+        str(
+            IntervalValue(
+                start=start,
+                end=end,
+                value=[
+                    EpochValue(value=start),
+                    NumberValue(values=[1, 2, 3, 4]),
+                ],
+            )
+        )
+        == """{
+    "interval": "2019-01-01T12:00:00.000000Z/2019-09-02T21:59:59.000000Z",
+    "epoch": "2019-01-01T12:00:00.000000Z",
+    "number": [
+        1,
+        2,
+        3,
+        4
+    ]
+}"""
+    )
+
+
+def test_epoch_value():
+    epoch: str = "2019-01-01T12:00:00.000000Z"
+
+    assert (
+        str(EpochValue(value=epoch))
+        == """{
+    "epoch": "2019-01-01T12:00:00.000000Z"
+}"""
+    )
+
+    assert (
+        str(EpochValue(value=dt.datetime(2019, 1, 1, 12)))
+        == """{
+    "epoch": "2019-01-01T12:00:00.000000Z"
+}"""
+    )
+
+    with pytest.raises(ValueError):
+        str(EpochValue(value="test"))
+
+
+@pytest.mark.xfail(reason="NumberValue class requires further explanaition")
+def test_numbers_value():
+    expected_result = """{
+    "number": [
+        1,
+        2,
+        3,
+        4
+    ]
+}"""
+    numbers = NumberValue(values=[1, 2, 3, 4])
+
+    assert str(numbers) == expected_result
+
+    expected_result = """{
+    "number": 1.0
+}"""
+    numbers = NumberValue(values=1.0)
+
+    assert str(numbers) == expected_result
+
+    with pytest.raises(ValidationError):
+        NumberValue(values="test")  # type: ignore
+
+    with pytest.raises(ValidationError):
+        NumberValue(values=[1, "test"])  # type: ignore
+
+    with pytest.raises(ValidationError):
+        NumberValue(values=[1, 2, 3, 4, 5])
+
+
+@pytest.mark.xfail
+def test_astropy_time_retains_input_format():
+    # It would be nice to recover the input format,
+    # but it's difficult without conditionally depending on Astropy
+    expected_result = "2012-03-15T10:16:06.97400000000198Z"
+    time = astropy.time.Time(expected_result)
+
+    result = format_datetime_like(time)
+
+    assert result == expected_result
+
+
+def test_astropy_time_format():
+    expected_result = "2012-03-15T10:16:06.974Z"
+    time = astropy.time.Time("2012-03-15T10:16:06.97400000000198Z")
+
+    result = format_datetime_like(time)
+
+    assert result == expected_result
+
+
+def test_quaternion_value_is_invalid():
+    with pytest.raises(TypeError):
+        UnitQuaternionValue(values=[0, 0, 0, 1, 0])
+
+
+def test_quaternion_value():
+    expected_result = """[
+    0.0,
+    0.0,
+    0.0,
+    1.0
+]"""
+
+    result = UnitQuaternionValue(values=[0, 0, 0, 1])
+
+    assert str(result) == expected_result
+
+
+def test_cartographic_radians_value():
+    result = CartographicRadiansValue(values=[0, 0, 0, 1])
+    assert (
+        str(result)
+        == """[
+    0.0,
+    0.0,
+    0.0,
+    1.0
+]"""
+    )
+    result = CartographicRadiansValue(values=[0, 0, 1])
+    assert (
+        str(result)
+        == """[
+    0.0,
+    0.0,
+    1.0
+]"""
+    )
+    result = CartographicRadiansValue()
+    assert str(result) == """[]"""
+    with pytest.raises(TypeError):
+        CartographicRadiansValue(values=[0, 0, 1, 1, 1, 1])
+
+
+def test_cartographic_degrees_value():
+    result = CartographicDegreesValue(values=[0, 0, 0, 1])
+    assert (
+        str(result)
+        == """[
+    0.0,
+    0.0,
+    0.0,
+    1.0
+]"""
+    )
+    result = CartographicDegreesValue(values=[0, 0, 1])
+    assert (
+        str(result)
+        == """[
+    0.0,
+    0.0,
+    1.0
+]"""
+    )
+    result = CartographicDegreesValue()
+    assert str(result) == """[]"""
+    with pytest.raises(TypeError):
+        CartographicDegreesValue(values=[0, 0, 1, 1, 1, 1])
+
+
+def test_rgba_value():
+    assert (
+        str(RgbaValue(values=[30, 30, 30, 30]))
+        == """[
+    30.0,
+    30.0,
+    30.0,
+    30.0
+]"""
+    )
+    assert (
+        str(RgbaValue(values=[30, 30, 30, 30, 1]))
+        == """[
+    30.0,
+    30.0,
+    30.0,
+    30.0,
+    1.0
+]"""
+    )
+
+
+def test_rgbaf_value():
+    assert (
+        str(RgbafValue(values=[0.5, 0.5, 0.5, 0.5]))
+        == """[
+    0.5,
+    0.5,
+    0.5,
+    0.5
+]"""
+    )
+    assert (
+        str(RgbafValue(values=[0.5, 0.5, 0.5, 0.5, 1]))
+        == """[
+    0.5,
+    0.5,
+    0.5,
+    0.5,
+    1.0
+]"""
+    )
+
+
+def test_check_reference():
+    with pytest.raises(TypeError):
+        check_reference("thisthat")
+    assert check_reference("this#that") is None
+
+
+def test_format_datetime_like():
+    assert format_datetime_like(None) is None
