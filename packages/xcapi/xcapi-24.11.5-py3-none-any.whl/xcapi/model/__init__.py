07d6689cb783d36a5f77854eeaa6fde26109a9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:34:06 +0000

    Add package xcapi/24.11.5/xcapi-24.11.5-py3-none-any.whl

---
+++ packages/xcapi/xcapi-24.11.5-py3-none-any.whl/xcapi/model/__init__.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ClassMeta(object):
+
+    @staticmethod
+    def SerializeToString():
+        pass
+
+    @staticmethod
+    def FromString():
+        pass
