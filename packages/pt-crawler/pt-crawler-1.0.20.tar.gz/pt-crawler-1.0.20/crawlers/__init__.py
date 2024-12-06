12e5edc6cd530100c56d632098a07b52c8d7ca27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:33 +0000

    Add package pt-crawler/1.0.20/pt-crawler-1.0.20.tar.gz

---
+++ packages/pt-crawler/pt-crawler-1.0.20.tar.gz/pt-crawler-1.0.20/crawlers/__init__.py
@@ -0,0 +1,20 @@
+from .base import Crawler
+from .base import Torrent
+from .base import Promotion
+from .base import User
+
+from .chdbits import CHDBits
+from .mteam import MTeam
+from .fsm import FSM
+from .ttg import TTG
+from .pterclub import PTerClub
+from .redleaves import RedLeaves
+from .opencd import OpenCD
+from .ourbits import OurBits
+from .u2 import U2
+from .ubits import UBits
+from .lemonhd import LemonHD
+
+VERSION = '1.0.20'
+
+__all__ = ['VERSION']
