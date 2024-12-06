655b192f1c2c77b7db460e758f420ceee3075d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:22 +0000

    Add package subdata/0.1.0/subdata-0.1.0.tar.gz

---
+++ packages/subdata/subdata-0.1.0.tar.gz/subdata-0.1.0/subdata/__init__.py
@@ -0,0 +1,7 @@
+import SubjData.download
+import SubjData.process
+
+from SubjData.main import get_target_info
+from SubjData.main import get_category_info
+from SubjData.main import create_target_dataset
+from SubjData.main import create_category_dataset
