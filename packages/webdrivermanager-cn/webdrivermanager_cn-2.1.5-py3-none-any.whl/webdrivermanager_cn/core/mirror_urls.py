6dfc398accabb6827888e2f2d87e5cf8446cc585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:13 +0000

    Add package webdrivermanager-cn/2.1.5/webdrivermanager_cn-2.1.5-py3-none-any.whl

---
+++ packages/webdrivermanager-cn/webdrivermanager_cn-2.1.5-py3-none-any.whl/webdrivermanager_cn/core/mirror_urls.py
@@ -0,0 +1,24 @@
+class VersionApi:
+    ChromeDriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/ChromeDriverLastVersion.json'
+    ChromeDriverLastPastVersion = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/ChromeDriverLatestPatchVersion.json'
+    GeckodriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/GeckodriverLastVersion.json'
+
+
+class PublicMirror:
+    EdgeDriverUrl = 'https://msedgedriver.azureedge.net'
+    ChromeDriver = 'https://googlechromelabs.github.io/chrome-for-testing'
+    GeckodriverApi = 'https://api.github.com/repos/mozilla/geckodriver/releases'
+
+
+class AliMirror:
+    ChromeDriverUrl = 'https://registry.npmmirror.com/-/binary/chromedriver'
+    ChromeDriverUrlNew = 'https://registry.npmmirror.com/-/binary/chrome-for-testing'
+    GeckodriverUrl = 'https://registry.npmmirror.com/-/binary/geckodriver'
+    EdgeDriverUrl = 'https://registry.npmmirror.com/-/binary/edgedriver'
+
+
+class HuaweiMirror:
+    ChromeDriverUrl = 'https://mirrors.huaweicloud.com/chromedriver'
+    ChromeDriverVersion = 'https://mirrors.huaweicloud.com/chromedriver/.index.json'
+    GeckodriverUrl = 'https://mirrors.huaweicloud.com/geckodriver'
+    GeckodriverVersion = 'https://mirrors.huaweicloud.com/geckodriver/.index.json'
