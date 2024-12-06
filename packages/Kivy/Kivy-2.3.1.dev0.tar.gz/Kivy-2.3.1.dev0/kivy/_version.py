0bd0e0187c10fab3674c2e06b97cf28fed3f25a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:05 +0000

    Add package Kivy/2.3.1.dev0/Kivy-2.3.1.dev0.tar.gz

---
+++ packages/Kivy/Kivy-2.3.1.dev0.tar.gz/Kivy-2.3.1.dev0/kivy/_version.py
@@ -0,0 +1,18 @@
+# This file is imported from __init__.py and exec'd from setup.py
+
+MAJOR = 2
+MINOR = 3
+MICRO = 1
+RELEASE = False
+
+__version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+if not RELEASE:
+    # if it's a rcx release, it's not proceeded by a period. If it is a
+    # devx release, it must start with a period
+    __version__ += '.dev0'
+
+
+_kivy_git_hash = '41416f130d8a41adc048ac8a6d6dfdb380078eb1'
+_kivy_build_date = '20241130'
+
