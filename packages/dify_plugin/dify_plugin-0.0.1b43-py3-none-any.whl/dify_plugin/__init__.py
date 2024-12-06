0605671de53f0b8b4518209ff60a594f84ce7bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:30 +0000

    Add package dify_plugin/0.0.1b43/dify_plugin-0.0.1b43-py3-none-any.whl

---
+++ packages/dify_plugin/dify_plugin-0.0.1b43-py3-none-any.whl/dify_plugin/__init__.py
@@ -0,0 +1,50 @@
+from gevent import monkey
+
+# patch all the blocking calls
+monkey.patch_all(sys=True)
+
+from .plugin import Plugin  # noqa: E402
+from .config.config import DifyPluginEnv  # noqa: E402
+
+from .interfaces.endpoint import Endpoint  # noqa: E402
+from .interfaces.model import ModelProvider  # noqa: E402
+from .interfaces.model.large_language_model import LargeLanguageModel  # noqa: E402
+from .interfaces.model.text_embedding_model import TextEmbeddingModel  # noqa: E402
+from .interfaces.model.rerank_model import RerankModel  # noqa: E402
+from .interfaces.model.tts_model import TTSModel  # noqa: E402
+from .interfaces.model.speech2text_model import Speech2TextModel  # noqa: E402
+from .interfaces.model.moderation_model import ModerationModel  # noqa: E402
+from .interfaces.tool import Tool, ToolProvider  # noqa: E402
+
+from .interfaces.model.openai_compatible.provider import OAICompatProvider  # noqa: E402
+from .interfaces.model.openai_compatible.llm import OAICompatLargeLanguageModel  # noqa: E402
+from .interfaces.model.openai_compatible.text_embedding import OAICompatEmbeddingModel  # noqa: E402
+from .interfaces.model.openai_compatible.speech2text import OAICompatSpeech2TextModel  # noqa: E402
+from .interfaces.model.openai_compatible.tts import OAICompatText2SpeechModel  # noqa: E402
+from .interfaces.model.openai_compatible.rerank import OAICompatRerankModel  # noqa: E402
+
+from .invocations.file import File  # noqa: E402
+
+__all__ = [
+    "Plugin",
+    "DifyPluginEnv",
+    "Endpoint",
+    "ToolProvider",
+    "Tool",
+    "ModelProvider",
+    "LargeLanguageModel",
+    "TextEmbeddingModel",
+    "RerankModel",
+    "TTSModel",
+    "Speech2TextModel",
+    "ModerationModel",
+
+    "OAICompatProvider",
+    "OAICompatLargeLanguageModel",
+    "OAICompatEmbeddingModel",
+    "OAICompatSpeech2TextModel",
+    "OAICompatText2SpeechModel",
+    "OAICompatRerankModel",
+    
+    "File",
+]
