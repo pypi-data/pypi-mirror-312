db9d5f3a55b93d5328d7963dd9faa818a8680756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:08:56 +0000

    Add package elia-py/0.3.1/elia-py-0.3.1.tar.gz

---
+++ packages/elia-py/elia-py-0.3.1.tar.gz/elia-py-0.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as file_requirements:
+    requirements = file_requirements.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as file_readme:
+    long_description = file_readme.read()
+
+setup(
+    name='elia-py',
+    version='0.3.1',
+    packages=find_packages(),
+    url='https://github.com/quintinnicolas/elia-py',
+    author='Nicolas Quintin',
+    author_email='nicolasquintin92@gmail.com',
+    description='Python3 client for the webservices of Elia',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    install_requires=requirements,
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_data={
+        'elia-py': ['LICENSE.txt', 'README.md'],
+    },
+)
