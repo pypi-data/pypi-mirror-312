c8b48a09fe9fdf565fae2ef28a862fc02477a7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:05 +0000

    Add package ultra-framework/0.0.6/ultra_framework-0.0.6.tar.gz

---
+++ packages/ultra-framework/ultra_framework-0.0.6.tar.gz/ultra_framework-0.0.6/ultra_framework/repositories/base_repository_factory.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Self, Type, Any
+
+from sqlalchemy.orm import Session
+from ultra_framework.repositories.crud_repository import CRUDRepository
+
+
+class BaseRepositoryFactory(ABC):
+
+    def __init__(self, session: Session):
+        self._session = session
+
+    @property
+    def session(self) -> Session:
+        return self._session
+
+    @classmethod
+    @abstractmethod
+    def create_factory(cls, session: Session) -> Self: ...
+
+    def make_repository(self, repository_class: Type[CRUDRepository]) -> Any:
+        return repository_class(self.session)
