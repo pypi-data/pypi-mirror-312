6460667b1cac846e53b32c5732cd2318cb86c035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:57 +0000

    Add package prompt_bottle/0.1.5/prompt_bottle-0.1.5.tar.gz

---
+++ packages/prompt_bottle/prompt_bottle-0.1.5.tar.gz/prompt_bottle-0.1.5/src/prompt_bottle/__init__.py
@@ -0,0 +1,21 @@
+from prompt_bottle.bottle import PromptBottle
+from prompt_bottle.presets.simple import simple_bottle
+from prompt_bottle.tags.tags import (
+    PBTag,
+    pb_audio,
+    pb_img_url,
+    pb_mp3_audio,
+    pb_tag,
+    pb_wav_audio,
+)
+
+__all__ = [
+    "PBTag",
+    "PromptBottle",
+    "pb_audio",
+    "pb_img_url",
+    "pb_mp3_audio",
+    "pb_tag",
+    "pb_wav_audio",
+    "simple_bottle",
+]
