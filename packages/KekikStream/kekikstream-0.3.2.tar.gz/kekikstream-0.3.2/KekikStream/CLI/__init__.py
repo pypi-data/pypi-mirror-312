1d4b80c39460b2b3608d65fdce82d3743b0ac952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:31 +0000

    Add package KekikStream/0.3.2/kekikstream-0.3.2.tar.gz

---
+++ packages/KekikStream/kekikstream-0.3.2.tar.gz/kekikstream-0.3.2/KekikStream/CLI/__init__.py
@@ -0,0 +1,4 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from Kekik.cli     import konsol, cikis_yap, hata_salla, log_salla, hata_yakala, bellek_temizle, temizle
+from .check_update import check_and_update_package
