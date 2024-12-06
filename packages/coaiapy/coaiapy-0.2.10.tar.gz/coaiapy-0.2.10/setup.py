c920ef88829a8804b27f1974ed073540b83e22fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:51 +0000

    Add package coaiapy/0.2.10/coaiapy-0.2.10.tar.gz

---
+++ packages/coaiapy/coaiapy-0.2.10.tar.gz/coaiapy-0.2.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coaiapy',
+    version = "0.2.10",
+    author='Jean GUillaume ISabelle',
+    author_email='jgi@jgwill.com',
+    description='A Python package for audio transcription, synthesis, and tagging using Boto3.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jgwill/coaiapy',
+    packages=find_packages(
+        include=["coaiapy", "test-*.py"], exclude=["test*log", "*test*csv", "*test*png"]
+    ),
+    #package_dir={'': 'coaiapy'},
+    install_requires=[
+        'boto3',
+        'mutagen',
+        'certifi',
+        'charset-normalizer',
+        'idna',
+        'redis',
+        'requests',
+        'markdown',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
