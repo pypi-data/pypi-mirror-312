2554821bc004cb637bc765a7ca64f622ef33b858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:01 +0000

    Add package bbstrader/0.1.94/bbstrader-0.1.94.tar.gz

---
+++ packages/bbstrader/bbstrader-0.1.94.tar.gz/bbstrader-0.1.94/bbstrader/__ini__.py
@@ -0,0 +1,18 @@
+"""
+Simplified Investment & Trading Toolkit
+=======================================
+
+"""
+__author__      = "Bertin Balouki SIMYELI"
+__copyright__   = "2023-2024 Bertin Balouki SIMYELI"
+__email__       = "bbalouki@outlook.com"
+__license__     = "MIT"
+__version__     = '0.1.91'
+
+
+from bbstrader import btengine
+from bbstrader import metatrader
+from bbstrader import models
+from bbstrader import trading
+from bbstrader import tseries
+from .config import config_logger
