cbf2e41b0f160a3e5f026bb66890197c7b50e761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:56 +0000

    Add package orgecc-pylib/0.1.0/orgecc_pylib-0.1.0-py3-none-any.whl

---
+++ packages/orgecc-pylib/orgecc_pylib-0.1.0-py3-none-any.whl/pylibtypes/__init__.py
@@ -0,0 +1,8 @@
+"""Miscellaneous functions
+"""
+
+__version__ = '1.0'
+__author__ = 'Elifarley'
+
+from .named_subclass_factory import create_named_subclass
+from .folder_based_class_attrs import load_class_attrs_from_folder, FolderBasedAttrsError
