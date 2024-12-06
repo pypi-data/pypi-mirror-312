86f13771e06c8f1a8b8144d062b49862aba8e859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:41 +0000

    Add package wellcode-cli/0.1.37/wellcode_cli-0.1.37-py3-none-any.whl

---
+++ packages/wellcode-cli/wellcode_cli-0.1.37-py3-none-any.whl/wellcode_cli/commands/__init__.py
@@ -0,0 +1,16 @@
+from .chat import chat
+from .chat_interface import chat_interface
+from .completion import completion
+from .config import config
+from .report import report
+from .review import review
+
+__all__ = [
+    "review",
+    "config",
+    "chat",
+    "report",
+    "chat_interface",
+    "config",
+    "completion",
+]
