91035085d42a40f3d79482902109637fcdbd93f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:48 +0000

    Add package eazyml-cf/0.0.13/eazyml-cf-0.0.13.tar.gz

---
+++ packages/eazyml-cf/eazyml-cf-0.0.13.tar.gz/eazyml-cf-0.0.13/eazyml_cf/__init__.py
@@ -0,0 +1,8 @@
+from . import compiledeazyml
+compiledeazyml.libeazymlcfpython_cython_submodule()
+
+from .cfr_helper import (
+                        sk_feature_selection,
+                        sk_model_building,
+                        ez_cf_inference
+                        )
