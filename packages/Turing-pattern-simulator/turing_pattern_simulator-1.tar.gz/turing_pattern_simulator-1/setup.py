50445cccfffe4d273e8b9e779f36583accaa9ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:05 +0000

    Add package Turing-pattern-simulator/1/turing_pattern_simulator-1.tar.gz

---
+++ packages/Turing-pattern-simulator/turing_pattern_simulator-1.tar.gz/turing_pattern_simulator-1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Turing_pattern_simulator',
+    version='1',
+    description='A Python-based tool for constructing, verifying, and visualizing Turing Pattern based on reaction-diffusion models.',
+    author='Zihao Song',
+    author_email='zhsong@uw.edu',
+    url='https://github.com/songzi123songzi/Turing-Pattern-Simulator',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    python_requires='>=3.6',
+)
