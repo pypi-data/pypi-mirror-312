2f9166751bd9d6b4f479c0fa4799fe27ae142bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:59 +0000

    Add package wakapi-anyide/0.6.8/wakapi_anyide-0.6.8-cp312-abi3-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/wakapi-anyide/wakapi_anyide-0.6.8-cp312-abi3-manylinux_2_17_s390x.manylinux2014_s390x.whl/wakapi_anyide/_rust/watch/__init__.pyi
@@ -0,0 +1,33 @@
+from enum import IntEnum
+from os import PathLike
+from typing import List, Self
+
+
+class WatchEventType(IntEnum):  # undocumented
+    """
+    A type of watch event.
+    """
+    
+    Create = 0
+    Delete = 1
+    Modify = 2
+
+
+class WatchEvent:  # undocumented
+    """
+    A watch event.
+    """
+    
+    kind: WatchEventType
+    target: str
+
+
+class Watch:  # undocumented
+    """
+    Watch a directory and its subdirectory for changes.
+    """
+    
+    def __init__(self, polling: bool): ...
+    def add_watch(self, to_watch: PathLike | str, is_recursive: bool): ...
+    def __aiter__(self) -> Self: ...
+    async def __anext__(self) -> List[WatchEvent]: ...
