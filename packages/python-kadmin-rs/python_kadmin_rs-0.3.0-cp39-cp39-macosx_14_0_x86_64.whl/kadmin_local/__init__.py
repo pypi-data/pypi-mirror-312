8837a232ca3de314f0569714cf3f5534aad2d5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:45 +0000

    Add package python-kadmin-rs/0.3.0/python_kadmin_rs-0.3.0-cp39-cp39-macosx_14_0_x86_64.whl

---
+++ packages/python-kadmin-rs/python_kadmin_rs-0.3.0-cp39-cp39-macosx_14_0_x86_64.whl/kadmin_local/__init__.py
@@ -0,0 +1,10 @@
+from kadmin_local._lib import KAdmin, Policy, Principal, Params, DbArgs, __version__
+
+__all__ = (
+    "__version__",
+    "KAdmin",
+    "Policy",
+    "Principal",
+    "Params",
+    "DbArgs",
+)
