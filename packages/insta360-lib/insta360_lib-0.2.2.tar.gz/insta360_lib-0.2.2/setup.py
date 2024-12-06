3790e415c50311e0f788f8270223f0c0e4c2d88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:53 +0000

    Add package insta360-lib/0.2.2/insta360_lib-0.2.2.tar.gz

---
+++ packages/insta360-lib/insta360_lib-0.2.2.tar.gz/insta360_lib-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='insta360_lib',
+    version='0.2.2',
+    packages=find_packages(),
+    package_data={
+        'insta360_lib': ['dlls/*.dll', 'dlls/iac3/*.dll'],
+    },
+    include_package_data=True,
+    install_requires=[
+        "loguru"
+        # 列出你的依赖项
+    ],
+    entry_points={
+        # 如果有命令行工具，可以在这里定义
+    },
+    author='rain',
+    author_email='chenrunming@insta360.com',
+    description='insta360 算法库调用封装库',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.insta360.com/test2/production-testing-tools/insta360_lib.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
