c2255d3a07077fa651f2574177b4af38ff32079c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:33 +0000

    Add package pan-scm-sdk/0.3.3/pan_scm_sdk-0.3.3.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.3.tar.gz/pan_scm_sdk-0.3.3/scm/config/objects/__init__.py
@@ -0,0 +1,9 @@
+# scm/config/objects/__init__.py
+
+from .address import Address
+from .address_group import AddressGroup
+from .application import Application
+from .application_group import ApplicationGroup
+from .service import Service
+from .service_group import ServiceGroup
+from .tag import Tag
