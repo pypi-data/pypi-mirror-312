c6f12794ca82d06445df551a3b7a443d4e9b03f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:44 +0000

    Add package niho/0.2.0/niho-0.2.0.tar.gz

---
+++ packages/niho/niho-0.2.0.tar.gz/niho-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="niho",
+    version="0.2.0",
+    description="Niho is a simple HTTP server framework.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nihad Kerimli",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
