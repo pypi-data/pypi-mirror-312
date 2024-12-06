1ec7ff6c9d0f4516051c0e29c61c66a77803c3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:53 +0000

    Add package pyvlasiator/0.1.0/pyvlasiator-0.1.0-py3-none-any.whl

---
+++ packages/pyvlasiator/pyvlasiator-0.1.0-py3-none-any.whl/pyvlasiator/plot/__init__.py
@@ -0,0 +1,14 @@
+"""
+Plotting functionality API.
+
+This provides supports for plotting VLSV data via Matplotlib.
+"""
+
+from pyvlasiator.plot.plot import (
+    plot,
+    pcolormesh,
+    contour,
+    contourf,
+    streamplot,
+    vdfslice,
+)
