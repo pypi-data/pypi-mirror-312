6960f70c46124a2809d959e47bb844a17ecbc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:44 +0000

    Add package bw2io/0.9.1/bw2io-0.9.1.tar.gz

---
+++ packages/bw2io/bw2io-0.9.1.tar.gz/bw2io-0.9.1/bw2io/importers/__init__.py
@@ -0,0 +1,24 @@
+from .ecoinvent_lcia import EcoinventLCIAImporter
+from .ecospold1 import (
+    MultiOutputEcospold1Importer,
+    NoIntegerCodesEcospold1Importer,
+    SingleOutputEcospold1Importer,
+)
+from .ecospold1_lcia import Ecospold1LCIAImporter
+from .ecospold2 import SingleOutputEcospold2Importer
+from .ecospold2_biosphere import Ecospold2BiosphereImporter
+from .excel import CSVImporter, ExcelImporter
+from .excel_lcia import CSVLCIAImporter, ExcelLCIAImporter
+from .exiobase3_hybrid import Exiobase3HybridImporter
+from .exiobase3_monetary import Exiobase3MonetaryImporter
+from .simapro_block_csv import SimaProBlockCSVImporter
+from .simapro_csv import SimaProCSVImporter
+from .simapro_lcia_csv import SimaProLCIACSVImporter
+
+"""
+This module provides classes for importing Life Cycle Impact Assessment (LCIA) data
+from different file formats.
+
+"""
+
+# Import classes for LCIA data import
