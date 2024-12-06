4fff6d784803e963414e423fe05f3bf94156bc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:21 +0000

    Add package lyyliutongz/1.2/lyyliutongz-1.2.tar.gz

---
+++ packages/lyyliutongz/lyyliutongz-1.2.tar.gz/lyyliutongz-1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyyliutongz',
+    version='1.2',
+    author='yy',
+    author_email='',
+    description='lyy stock tdx liutongz for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    ],
+)
