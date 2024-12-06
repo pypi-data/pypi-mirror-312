98968d5ef96755568cd96908a70c7235ad19697b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:44 +0000

    Add package python-kadmin-rs/0.3.0/python_kadmin_rs-0.3.0-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/python-kadmin-rs/python_kadmin_rs-0.3.0-cp311-cp311-musllinux_1_2_aarch64.whl/kadmin/__init__.py
@@ -0,0 +1,10 @@
+from kadmin._lib import KAdmin, Policy, Principal, Params, DbArgs, __version__
+
+__all__ = (
+    "__version__",
+    "KAdmin",
+    "Policy",
+    "Principal",
+    "Params",
+    "DbArgs",
+)
