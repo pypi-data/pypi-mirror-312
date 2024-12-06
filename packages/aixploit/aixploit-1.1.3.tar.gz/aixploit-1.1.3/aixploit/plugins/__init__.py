0c2505fc20f85b4afc9000459573d24bf4b3da69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:34 +0000

    Add package aixploit/1.1.3/aixploit-1.1.3.tar.gz

---
+++ packages/aixploit/aixploit-1.1.3.tar.gz/aixploit-1.1.3/aixploit/plugins/__init__.py
@@ -0,0 +1,42 @@
+"""Plugins init"""
+
+
+from .integrity.DirectPromptInjection import PromptInjection
+#from .integrity.IndirectPromptInjection import IndirectPromptInjector, SingleIndirectPromptInjector
+#from .integrity.Language import LanguageInjector, SingleLanguageInjector
+#from .integrity.Code import CodeInjector, SingleCodeInjector
+#from .integrity.Topics import TopicInjector, SingleTopicInjector
+#from .availability.TokenLimit import TokenFlooder
+#from .abuse.Bias import BiasInjector, SingleBiasInjector
+#from .abuse.Competitor import CompetitorInjector, SingleCompetitorInjector
+#from .abuse.Toxicity import ToxicityInjector, SingleToxicityInjector
+#from .privacy.PII import PIIInjector, SinglePIIInjector, PIIExtractor, SinglePIIExtractor
+#from .privacy.SensitiveData import SensitiveDataInjector, SingleSensitiveDataInjector, SensitiveDataExtractor, SingleSensitiveDataExtractor
+
+__all__ = [
+    "PromptInjection",
+
+    #"IndirectPromptInjector",
+    #"SingleIndirectPromptInjector",
+    #"CompetitorInjector",
+    #"SingleCompetitorInjector",
+    #"TopicInjector",
+    #"SingleTopicInjector",
+    #"LanguageInjector",
+    #"SingleLanguageInjector",
+    #"CodeInjector",
+    #"SingleCodeInjector",
+    #"TokenFlooder",
+    #"BiasInjector",
+    #"SingleBiasInjector",
+    #"PIIInjector",
+    #"SinglePIIInjector",
+    #"PIIExtractor",
+    #"SinglePIIExtractor", 
+    #"SensitiveDataInjector",
+    #"SingleSensitiveDataInjector",
+    #"SensitiveDataExtractor",
+    #"SingleSensitiveDataExtractor",
+    #"ToxicityInjector",
+    #"SingleToxicityInjector",
+]
---
+++ packages/aixploit/aixploit-1.1.3.tar.gz/aixploit-1.1.3/test/test.py
@@ -0,0 +1,42 @@
+import sys, os
+from datetime import datetime
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))   # Automatically detect the current folder # Adjust this path as necessary
+from dotenv import load_dotenv
+load_dotenv()
+from aixploit.plugins import PromptInjection
+from aixploit.core import run
+
+
+target1 = ["Ollama", "http://localhost:11434/v1", "mistral"]
+target2 = ["Openai", "", "gpt-3.5-turbo"]
+
+
+attackers = [
+    PromptInjection("quick"),
+
+    ]
+
+
+start_time = datetime.now()
+print('Redteaming exercise started at : ', start_time.strftime("%H:%M:%S"))  
+
+#conversation, attack_prompts_malicious, success_rates_percentage, total_tokens, total_cost = run(attackers, target1, os.getenv("OLLAMA_API_KEY"))
+conversation, attack_prompts_malicious, success_rates_percentage, total_tokens, total_cost = run(attackers, target2, os.getenv("OPENAI_KEY"))
+
+for idx, attacker in enumerate(attackers):  # {{ edit_1 }}
+    try:
+        print('Attacker: ', attacker.__class__.__name__ )  
+        prompts = conversation[idx]  # Get the conversation for the current attacker
+        print(f' \U00002705  Number of prompts tested for attacker {idx + 1}: {len(prompts)}')  # {{ edit_2 }}
+        malicious_prompts = attack_prompts_malicious[idx]
+        print(f' \U00002705  Number of successful prompts for attacker {idx + 1}: {len(malicious_prompts)}')
+        print(f' \U00002705  Attack success rate for attacker {idx + 1}: {success_rates_percentage[idx] * 100:.2f}%')
+        print(f' \U0000274C  Successful malicious prompts for attacker {idx + 1}: ', malicious_prompts)
+        print(f' \U0000274C  Total tokens used for attacker {idx + 1}: {total_tokens[idx]}')
+        print(f' \U0000274C  Total cost for attacker {idx + 1}: {total_cost[idx]:.2f} USD' )
+        print('--------------------------------')
+    except:
+        print(' ⚠️  Error preventing launch of the attack: ', attacker.__class__.__name__)
+
+print('Redteaming exercise ended at : ', datetime.now().strftime("%H:%M:%S"))
+print('Total time taken: ', datetime.now() - start_time)
