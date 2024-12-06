3e9d58ccbf3dd9d762cc3f447effa7350fa26f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:24 +0000

    Add package best403unlocker_py/0.1.1/best403unlocker_py-0.1.1.tar.gz

---
+++ packages/best403unlocker_py/best403unlocker_py-0.1.1.tar.gz/best403unlocker_py-0.1.1/best403unlocker_py/cli.py
@@ -0,0 +1,60 @@
+import platform
+import click
+from .main import read_config, scan_dns_servers, set_dns, sort_dict, write_dns_config
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+@click.option(
+    "--url", default="developers.google.com", help="URL to use for DNS search"
+)
+def cli(ctx,url):
+    if ctx.invoked_subcommand is None:
+        default()
+
+
+@click.command()
+@click.option(
+    "--url", default="developers.google.com", help="URL to use for DNS search"
+)
+def search_dns(url):
+    dns_servers = read_config()
+    results = scan_dns_servers(url, dns_servers)
+    sorted_dns_servers = sort_dict(results)
+    write_dns_config(sorted_dns_servers)
+    for dns, time in results.items():
+        print(f"{dns}: {time} seconds")
+    # set_dns(dns_servers)
+    os_type = platform.system().lower()
+
+    if os_type == "windows":
+        return sorted_dns_servers
+
+    print("DNS servers have been searched and set successfully.")
+    return sorted_dns_servers
+
+
+@click.command()
+@click.argument("dns_servers", nargs=-1, required=True)
+def set_custom_dns(dns_servers):
+    if not dns_servers:
+        print("Error: At least one DNS server must be provided.")
+        return
+    set_dns(dns_servers)
+
+
+@click.command()
+@click.option(
+    "--url", default="developers.google.com", help="URL to use for DNS search"
+)
+def default(url="developers.google.com"):
+    sorted_dns_servers = search_dns.callback(url)
+    set_dns(sorted_dns_servers[:2])
+    print("DNS servers have been searched and set successfully.")
+
+
+cli.add_command(search_dns)
+cli.add_command(set_custom_dns)
+
+if __name__ == "__main__":
+    cli()
