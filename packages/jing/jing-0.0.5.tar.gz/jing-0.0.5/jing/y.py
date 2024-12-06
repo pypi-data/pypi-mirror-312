c8ce1d480d04b12e7fcc6a4422fa1185b29b5a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:03:40 +0000

    Add package jing/0.0.5/jing-0.0.5.tar.gz

---
+++ packages/jing/jing-0.0.5.tar.gz/jing-0.0.5/jing/y.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf8 -*-
+
+from jing.data_center import DataCenter
+from jing.reference import Reference
+
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore")
+
+pd.set_option('display.max_columns', None)
+
+class Y:
+    def __init__(self, _market="us", _date="", _code="") -> None:
+        self.market = _market
+        self.date = _date
+        self.code = _code
+
+        self.dataCenter = DataCenter(self.market)
+        self.df = self.dataCenter.one('IONQ', self.date) 
+        self.ref = Reference(self.df)
+
+if __name__=="__main__":
+    y = Y("us", _date="2024-09-27", _code='IONQ')
+    ref = y.ref
+    print(ref.date(0), ref.ma20(0), ref.ma50(0), ref.ma200(0), ref.vma50(0), ref.macd(0), ref.diff(0), ref.dea(0))
+
