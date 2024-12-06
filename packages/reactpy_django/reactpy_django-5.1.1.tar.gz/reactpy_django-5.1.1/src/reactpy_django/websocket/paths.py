4b218fd3a1fcfe8129533d409194203d90dac23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:19 +0000

    Add package reactpy_django/5.1.1/reactpy_django-5.1.1.tar.gz

---
+++ packages/reactpy_django/reactpy_django-5.1.1.tar.gz/reactpy_django-5.1.1/src/reactpy_django/websocket/paths.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from reactpy_django.config import REACTPY_URL_PREFIX
+from reactpy_django.websocket.consumer import ReactpyAsyncWebsocketConsumer
+
+REACTPY_WEBSOCKET_ROUTE = path(
+    f"{REACTPY_URL_PREFIX}/<str:dotted_path>/<uuid:uuid>/<int:has_args>/",
+    ReactpyAsyncWebsocketConsumer.as_asgi(),
+)
+"""A URL path for :class:`ReactpyAsyncWebsocketConsumer`.
+
+Required since the `reverse()` function does not exist for Django Channels, but ReactPy needs
+to know the current websocket path.
+"""
