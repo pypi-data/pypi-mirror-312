3e9842d8118ac08af0219c868f030450c13dc4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:22 +0000

    Add package freaky/1.0.0a4/freaky-1.0.0a4-py3-none-any.whl

---
+++ packages/freaky/freaky-1.0.0a4-py3-none-any.whl/freaky/__init__.py
@@ -0,0 +1,6 @@
+from .encode import encode
+from .encode import encode_cli as _encode_cli
+from .decode import decode
+from .decode import decode_cli as _decode_cli
+
+from .entropy import average_entropy
