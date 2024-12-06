c69eca666d3addb592b21e540439c30cdfd24e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:36 +0000

    Add package TAN64/0.1/tan64-0.1.tar.gz

---
+++ packages/TAN64/tan64-0.1.tar.gz/tan64-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    version='0.1',
+    name='TAN64',
+    author='TAN64',
+    description='TAN ENCODE',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3'
+
+    ]
+)
+
+
