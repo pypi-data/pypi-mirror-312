50b3e07b33950c2df92ef20ea42e6193f5d8bec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:48 +0000

    Add package swarms/6.4.7/swarms-6.4.7-py3-none-any.whl

---
+++ packages/swarms/swarms-6.4.7-py3-none-any.whl/swarms/artifacts/__init__.py
@@ -0,0 +1,9 @@
+from swarms.artifacts.base_artifact import BaseArtifact
+from swarms.artifacts.text_artifact import TextArtifact
+from swarms.artifacts.main_artifact import Artifact
+
+__all__ = [
+    "BaseArtifact",
+    "TextArtifact",
+    "Artifact",
+]
