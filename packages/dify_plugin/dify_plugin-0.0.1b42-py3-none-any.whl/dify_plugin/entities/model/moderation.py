5b33f19d48ec13679b071101b38bacaee09637b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:47 +0000

    Add package dify_plugin/0.0.1b42/dify_plugin-0.0.1b42-py3-none-any.whl

---
+++ packages/dify_plugin/dify_plugin-0.0.1b42-py3-none-any.whl/dify_plugin/entities/model/moderation.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from ...entities.model import BaseModelConfig, ModelType
+
+
+class ModerationModelConfig(BaseModelConfig):
+    """
+    Model class for moderation model config.
+    """
+
+    model_type: ModelType = ModelType.MODERATION
+
+    model_config = ConfigDict(protected_namespaces=())
+
+
+class ModerationResult(BaseModel):
+    """
+    Model class for moderation result.
+    """
+    result: bool
