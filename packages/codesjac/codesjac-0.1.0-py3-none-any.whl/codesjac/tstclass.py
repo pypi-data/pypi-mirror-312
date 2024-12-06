737b74a205a67de7464910f60e9324d518fbc865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:29 +0000

    Add package codesjac/0.1.0/codesjac-0.1.0-py3-none-any.whl

---
+++ packages/codesjac/codesjac-0.1.0-py3-none-any.whl/codesjac/tstclass.py
@@ -0,0 +1,11 @@
+
+
+class TestClass:
+    def hello_world(self):
+        return "Hello World!"
+
+    def add (self, a, b):
+        return a + b
+
+    def subtract (self, a, b):
+        return a - b
