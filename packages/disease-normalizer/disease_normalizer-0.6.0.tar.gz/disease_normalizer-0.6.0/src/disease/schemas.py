e065b5b54a778b7456e55d147f6cc866705938ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:36 +0000

    Add package disease-normalizer/0.6.0/disease_normalizer-0.6.0.tar.gz

---
+++ packages/disease-normalizer/disease_normalizer-0.6.0.tar.gz/disease_normalizer-0.6.0/src/disease/schemas.py
@@ -0,0 +1,424 @@
+"""Contains data models for representing VICC normalized disease records."""
+
+import datetime
+from enum import Enum, IntEnum
+from typing import Literal
+
+from ga4gh.core import domain_models
+from pydantic import BaseModel, ConfigDict, StrictBool, StrictStr
+
+from disease import __version__
+
+
+class MatchType(IntEnum):
+    """Define string constraints for use in Match Type attributes."""
+
+    CONCEPT_ID = 100
+    LABEL = 80
+    ALIAS = 60
+    XREF = 60
+    ASSOCIATED_WITH = 60
+    FUZZY_MATCH = 20
+    NO_MATCH = 0
+
+
+class SourceName(str, Enum):
+    """Define string constraints to ensure consistent capitalization."""
+
+    NCIT = "NCIt"
+    MONDO = "Mondo"
+    DO = "DO"
+    ONCOTREE = "OncoTree"
+    OMIM = "OMIM"
+
+
+class SourceIDAfterNamespace(Enum):
+    """Define string constraints after namespace."""
+
+    NCIT = "C"
+    MONDO = ""
+    DO = ""  # noqa: PIE796
+    ONCOTREE = ""  # noqa: PIE796
+    OMIM = ""  # noqa: PIE796
+
+
+class NamespacePrefix(Enum):
+    """Define string constraints for how concept ID namespace prefixes are
+    stored.
+    """
+
+    # built-in sources
+    NCIT = "ncit"
+    MONDO = "mondo"
+    DOID = "DOID"
+    DO = DOID
+    OMIM = "MIM"
+    ONCOTREE = "oncotree"
+    # external sources
+    COHD = "cohd"
+    DECIPHER = "decipher"
+    EFO = "efo"
+    GARD = "gard"
+    HP = "HP"
+    HPO = HP
+    ICD9 = "icd9"
+    ICD9CM = "icd9.cm"
+    ICD10 = "icd10"
+    ICD10WHO = ICD10
+    ICD10CM = "icd10.cm"
+    ICD11 = "icd11"
+    ICDO = "icdo"
+    IDO = "ido"
+    IMDRF = "imdrf"
+    KEGG = "kegg.disease"
+    MEDDRA = "meddra"
+    MEDGEN = "medgen"
+    MESH = "mesh"
+    MF = "mf"
+    MP = "MP"
+    MPATH = "mpath"
+    NIFSTD = "nifstd"
+    OBI = "obi"
+    OGMS = "ogms"
+    ORPHANET = "orphanet"
+    PATO = "pato"
+    SCDO = "scdo"
+    UMLS = "umls"
+    WIKIPEDIA = "wikipedia.en"
+    WIKIDATA = "wikidata"
+
+
+class SourcePriority(IntEnum):
+    """Define priorities for sources in building merged concepts."""
+
+    NCIT = 1
+    MONDO = 2
+    OMIM = 3
+    ONCOTREE = 4
+    DO = 5
+
+
+class Disease(BaseModel):
+    """Define disease record."""
+
+    label: StrictStr
+    concept_id: StrictStr
+    aliases: list[StrictStr] = []
+    xrefs: list[StrictStr] = []
+    associated_with: list[StrictStr] = []
+    pediatric_disease: bool | None = None
+    oncologic_disease: bool | None = None
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "label": "Von Hippel-Lindau Syndrome",
+                "concept_id": "ncit:C3105",
+                "aliases": [
+                    "Von Hippel-Lindau Syndrome (VHL)",
+                    "Von Hippel-Lindau Disease",
+                    "Cerebroretinal Angiomatosis",
+                    "von Hippel-Lindau syndrome",
+                    "VHL syndrome",
+                ],
+                "xrefs": [],
+                "associated_with": ["umls:C0019562"],
+                "pediatric_disease": None,
+                "oncologic_disease": None,
+            }
+        }
+    )
+
+
+class RecordType(str, Enum):
+    """Record item types."""
+
+    IDENTITY = "identity"
+    MERGER = "merger"
+
+
+class RefType(str, Enum):
+    """Reference item types."""
+
+    # Must be in descending MatchType order.
+    LABEL = "label"
+    ALIASES = "alias"
+    XREFS = "xref"
+    ASSOCIATED_WITH = "associated_with"
+
+
+class DataLicenseAttributes(BaseModel):
+    """Define constraints for data license attributes."""
+
+    non_commercial: StrictBool
+    share_alike: StrictBool
+    attribution: StrictBool
+
+
+class SourceMeta(BaseModel):
+    """Metadata for a given source to return in response object."""
+
+    data_license: StrictStr
+    data_license_url: StrictStr
+    version: StrictStr
+    data_url: StrictStr | None = None
+    rdp_url: StrictStr | None = None
+    data_license_attributes: DataLicenseAttributes
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "data_license": "CC BY 4.0",
+                "data_license_url": "https://creativecommons.org/licenses/by/4.0/legalcode",
+                "version": "21.01d",
+                "data_url": "https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/archive/21.01d_Release/",
+                "rdp_url": "http://reusabledata.org/ncit.html",
+                "data_license_attributes": {
+                    "non_commercial": False,
+                    "attribution": True,
+                    "share_alike": False,
+                },
+            }
+        }
+    )
+
+
+class SourceSearchMatches(BaseModel):
+    """Container for matching information from an individual source."""
+
+    match_type: MatchType
+    records: list[Disease] = []
+    source_meta_: SourceMeta
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "match_type": 80,
+                "records": [
+                    {
+                        "label": "non-small cell lung carcinoma",
+                        "concept_id": "mondo:0005233",
+                        "aliases": [
+                            "non-small cell lung carcinoma (disease)",
+                            "non-small cell carcinoma of lung",
+                            "non-small cell lung cancer",
+                            "non-small cell cancer of lung",
+                            "non-small cell carcinoma of the lung",
+                            "non-small cell cancer of the lung",
+                            "NSCLC",
+                            "NSCLC - non-small cell lung cancer",
+                        ],
+                        "xrefs": ["DOID:3908", "ncit:C2926", "oncotree:NSCLC"],
+                        "associated_with": [
+                            "mesh:D002289",
+                            "efo:0003060",
+                            "umls:C0007131",
+                        ],
+                    }
+                ],
+                "source_meta_": {
+                    "data_license": "CC BY 4.0",
+                    "data_license_url": "https://creativecommons.org/licenses/by/4.0/legalcode",
+                    "version": "2023-07-03",
+                    "data_url": "https://mondo.monarchinitiative.org/pages/download/",
+                    "rdp_url": "http://reusabledata.org/monarch.html",
+                    "data_license_attributes": {
+                        "non_commercial": False,
+                        "attribution": True,
+                        "share_alike": False,
+                    },
+                },
+            }
+        }
+    )
+
+
+class ServiceMeta(BaseModel):
+    """Metadata regarding the disease-normalization service."""
+
+    name: Literal["disease-normalizer"] = "disease-normalizer"
+    version: StrictStr
+    response_datetime: datetime.datetime
+    url: Literal["https://github.com/cancervariants/disease-normalization"] = (
+        "https://github.com/cancervariants/disease-normalization"
+    )
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "disease-normalizer",
+                "version": __version__,
+                "response_datetime": "2021-04-05T16:44:15.367831",
+                "url": "https://github.com/cancervariants/disease-normalization",
+            }
+        }
+    )
+
+
+class NormalizationService(BaseModel):
+    """Response containing one or more merged records and source data."""
+
+    query: StrictStr
+    warnings: dict | None = None
+    match_type: MatchType
+    normalized_id: str | None = None
+    disease: domain_models.Disease | None = None
+    source_meta_: dict[SourceName, SourceMeta] | None = None
+    service_meta_: ServiceMeta
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "query": "childhood leukemia",
+                "warnings": None,
+                "match_type": 80,
+                "normalized_id": "ncit:C4989",
+                "disease": {
+                    "id": "normalize.disease.ncit:C4989",
+                    "type": "Disease",
+                    "label": "Childhood Leukemia",
+                    "aliases": [
+                        "childhood leukemia (disease)",
+                        "leukemia",
+                        "pediatric leukemia (disease)",
+                        "Leukemia",
+                        "leukemia (disease) of childhood",
+                    ],
+                    "mappings": [
+                        {
+                            "coding": {"code": "0004355", "system": "mondo"},
+                            "relation": "relatedMatch",
+                        },
+                        {
+                            "coding": {"code": "7757", "system": "doid"},
+                            "relation": "relatedMatch",
+                        },
+                        {
+                            "coding": {"code": "C1332977", "system": "umls"},
+                            "relation": "relatedMatch",
+                        },
+                    ],
+                    "extensions": [
+                        {
+                            "name": "pediatric_disease",
+                            "value": True,
+                        },
+                    ],
+                },
+                "source_meta_": {
+                    "NCIt": {
+                        "data_license": "CC BY 4.0",
+                        "data_license_url": "https://creativecommons.org/licenses/by/4.0/legalcode",
+                        "version": "21.01d",
+                        "data_url": "https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/archive/21.01d_Release/",
+                        "rdp_url": "http://reusabledata.org/ncit.html",
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "attribution": True,
+                            "share_alike": False,
+                        },
+                    },
+                    "Mondo": {
+                        "data_license": "CC BY 4.0",
+                        "data_license_url": "https://creativecommons.org/licenses/by/4.0/legalcode",
+                        "version": "20210129",
+                        "data_url": "https://mondo.monarchinitiative.org/pages/download/",
+                        "rdp_url": "http://reusabledata.org/monarch.html",
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "attribution": True,
+                            "share_alike": False,
+                        },
+                    },
+                    "DO": {
+                        "data_license": "CC0 1.0",
+                        "data_license_url": "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
+                        "version": "20210305",
+                        "data_url": "http://www.obofoundry.org/ontology/doid.html",
+                        "rdp_url": None,
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "attribution": False,
+                            "share_alike": False,
+                        },
+                    },
+                },
+                "service_meta_": {
+                    "name": "disease-normalizer",
+                    "version": __version__,
+                    "response_datetime": "2021-04-05T16:44:15.367831",
+                    "url": "https://github.com/cancervariants/disease-normalization",
+                },
+            }
+        }
+    )
+
+
+class SearchService(BaseModel):
+    """Core response schema containing matches for each source"""
+
+    query: StrictStr
+    warnings: dict | None = None
+    source_matches: dict[SourceName, SourceSearchMatches]
+    service_meta_: ServiceMeta
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "query": "nsclc",
+            "source_matches": {
+                "OMIM": {
+                    "records": [],
+                    "source_meta_": {
+                        "data_license": "custom",
+                        "data_license_url": "https://omim.org/help/agreement",
+                        "version": "20231030",
+                        "data_url": "https://www.omim.org/downloads",
+                        "rdp_url": "http://reusabledata.org/omim.html",
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "attribution": True,
+                            "share_alike": True,
+                        },
+                    },
+                },
+                "DO": {
+                    "records": [
+                        {
+                            "label": "lung non-small cell carcinoma",
+                            "concept_id": "DOID:3908",
+                            "aliases": [
+                                "Non-small cell lung cancer",
+                                "NSCLC",
+                                "non-small cell lung carcinoma",
+                            ],
+                            "xrefs": ["ncit:C2926"],
+                            "associated_with": [
+                                "mesh:D002289",
+                                "kegg.disease:05223",
+                                "efo:0003060",
+                                "umls:C0007131",
+                            ],
+                            "match_type": 60,
+                        }
+                    ],
+                    "source_meta_": {
+                        "data_license": "CC0 1.0",
+                        "data_license_url": "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
+                        "version": "2023-07-24",
+                        "data_url": "http://www.obofoundry.org/ontology/doid.html",
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "attribution": False,
+                            "share_alike": False,
+                        },
+                    },
+                },
+            },
+            "service_meta_": {
+                "name": "disease-normalizer",
+                "version": "0.4.0.dev1",
+                "response_datetime": "2023-10-31T10:53:30.890262",
+                "url": "https://github.com/cancervariants/disease-normalization",
+            },
+        }
+    )
---
+++ packages/disease-normalizer/disease_normalizer-0.6.0.tar.gz/disease_normalizer-0.6.0/tests/unit/test_query.py
@@ -0,0 +1,320 @@
+"""Test the disease querying method."""
+
+from datetime import datetime
+
+import pytest
+from ga4gh.core import domain_models, entity_models
+
+from disease.query import InvalidParameterException, QueryHandler
+from disease.schemas import MatchType, SourceName
+
+
+@pytest.fixture(scope="module")
+def query_handler(database):
+    """Build query handler test fixture"""
+    return QueryHandler(database)
+
+
+@pytest.fixture(scope="module")
+def neuroblastoma():
+    """Create neuroblastoma fixture."""
+    return domain_models.Disease(
+        type="Disease",
+        id="normalize.disease.ncit:C3270",
+        label="Neuroblastoma",
+        mappings=[
+            {
+                "coding": {"code": "0005072", "system": "mondo"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "NBL", "system": "oncotree"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "769", "system": "doid"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "C0027819", "system": "umls"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "9500/3", "system": "icdo"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "0000621", "system": "efo"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "7185", "system": "gard"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "D009447", "system": "mesh"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "635", "system": "orphanet"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "C2751421", "system": "umls"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "CN205405", "system": "umls"},
+                "relation": "relatedMatch",
+            },
+        ],
+        alternativeLabels=[
+            "NB",
+            "neuroblastoma",
+            "Neural Crest Tumor, Malignant",
+            "Neuroblastoma (Schwannian Stroma-poor)",
+            "neuroblastoma (Schwannian Stroma-poor)",
+            "Neuroblastoma (Schwannian Stroma-Poor)",
+            "Neuroblastoma, NOS",
+            "NEUROBLASTOMA, MALIGNANT",
+            "Neuroblastoma (NBL)",
+            "neural Crest tumor, malignant",
+            "neuroblastoma, malignant",
+        ],
+        extensions=[entity_models.Extension(name="oncologic_disease", value=True)],
+    )
+
+
+@pytest.fixture(scope="module")
+def skin_myo():
+    """Create a test fixture for skin myopithelioma"""
+    return domain_models.Disease(
+        type="Disease",
+        id="normalize.disease.ncit:C167370",
+        label="Skin Myoepithelioma",
+        alternativeLabels=["Cutaneous Myoepithelioma"],
+        extensions=[],
+    )
+
+
+@pytest.fixture(scope="module")
+def mafd2():
+    """Create a test fixture for major affective disorder 2. Query should not
+    include a "pediatric_disease" Extension object.
+    """
+    return domain_models.Disease(
+        type="Disease",
+        id="normalize.disease.mondo:0010648",
+        label="major affective disorder 2",
+        alternativeLabels=[
+            "MAFD2",
+            "MDI",
+            "MDX",
+            "MANIC-DEPRESSIVE ILLNESS",
+            "BPAD",
+            "MAFD2",
+            "BIPOLAR AFFECTIVE DISORDER",
+            "MANIC-DEPRESSIVE PSYCHOSIS, X-LINKED",
+            "major affective disorder 2, X-linked dominant",
+            "MAJOR affective disorder 2",
+            "bipolar affective disorder",
+            "major affective disorder 2",
+            "manic-depressive illness",
+            "manic-depressive psychosis, X-linked",
+        ],
+        mappings=[
+            {
+                "coding": {"code": "309200", "system": "mim"},
+                "relation": "relatedMatch",
+            },
+            {
+                "coding": {"code": "C564108", "system": "mesh"},
+                "relation": "relatedMatch",
+            },
+        ],
+        extensions=[],
+    )
+
+
+def compare_disease(actual, fixture):
+    """Verify correctness of returned Disease core object against test fixture."""
+    assert actual.normalized_id == fixture.id.split("normalize.disease.")[-1]
+    actual = actual.disease
+    actual_keys = actual.model_dump(exclude_none=True).keys()
+    fixture_keys = fixture.model_dump(exclude_none=True).keys()
+    assert actual_keys == fixture_keys
+    assert actual.id == fixture.id
+    assert actual.type == fixture.type
+    assert actual.label == fixture.label
+
+    assert bool(actual.mappings) == bool(fixture.mappings)
+    if actual.mappings:
+        no_matches = []
+        for actual_mapping in actual.mappings:
+            match = None
+            for fixture_mapping in fixture.mappings:
+                if actual_mapping == fixture_mapping:
+                    match = actual_mapping
+                    break
+            if not match:
+                no_matches.append(actual_mapping)
+        assert no_matches == [], no_matches
+        assert len(actual.mappings) == len(fixture.mappings)
+
+    assert bool(actual.alternativeLabels) == bool(fixture.alternativeLabels)
+    if actual.alternativeLabels:
+        assert set(actual.alternativeLabels) == set(fixture.alternativeLabels)
+
+    def get_extension(extensions, name):
+        matches = [e for e in extensions if e.name == name]
+        if len(matches) > 1:
+            raise AssertionError
+        if len(matches) == 1:
+            return matches[0]
+        return None
+
+    assert bool(actual.extensions) == bool(fixture.extensions)
+    if actual.extensions:
+        ext_actual = actual.extensions
+        ext_fixture = fixture.extensions
+
+        ped_actual = get_extension(ext_actual, "pediatric_disease")
+        ped_fixture = get_extension(ext_fixture, "pediatric_disease")
+        assert (ped_actual is None) == (ped_fixture is None)
+        if ped_actual and ped_fixture:
+            assert ped_actual.value == ped_fixture.value
+            assert ped_actual.value
+
+        onco_actual = get_extension(ext_actual, "oncologic_disease")
+        onco_fixture = get_extension(ext_fixture, "oncologic_disease")
+        assert (onco_actual is None) == (onco_fixture is None)
+        if onco_actual and onco_fixture:
+            assert onco_actual.value == onco_fixture.value
+            assert onco_actual.value
+
+
+def test_query(query_handler):
+    """Test that query returns properly-structured response."""
+    resp = query_handler.search("Neuroblastoma")
+    assert resp.query == "Neuroblastoma"
+    matches = resp.source_matches
+    assert isinstance(matches, dict)
+    assert len(matches) == 5
+    ncit = matches["NCIt"]
+    assert len(ncit.records) == 1
+    ncit_record = ncit.records[0]
+    assert ncit_record.label == "Neuroblastoma"
+
+
+def test_query_specify_query_handlers(query_handler):
+    """Test inclusion and exclusion of sources in query."""
+    # test full inclusion
+    sources = "ncit,mondo,do,oncotree,omim"
+    resp = query_handler.search("Neuroblastoma", incl=sources, excl="")
+    matches = resp.source_matches
+    assert len(matches) == 5
+    assert SourceName.NCIT in matches
+    assert SourceName.MONDO in matches
+    assert SourceName.DO in matches
+    assert SourceName.ONCOTREE in matches
+    assert SourceName.OMIM in matches
+
+    # test full exclusion
+    resp = query_handler.search("Neuroblastoma", excl=sources)
+    matches = resp.source_matches
+    assert len(matches) == 0
+
+    # test case insensitive
+    resp = query_handler.search("Neuroblastoma", excl="NCIt")
+    matches = resp.source_matches
+    assert SourceName.NCIT not in matches
+    resp = query_handler.search("Neuroblastoma", incl="nCiT")
+    matches = resp.source_matches
+    assert SourceName.NCIT in matches
+
+    # test error on invalid source names
+    with pytest.raises(InvalidParameterException):
+        resp = query_handler.search("Neuroblastoma", incl="nct")
+
+    # test error for supplying both incl and excl args
+    with pytest.raises(InvalidParameterException):
+        resp = query_handler.search("Neuroblastoma", incl="mondo", excl="ncit")
+
+
+def test_normalize_query(query_handler, neuroblastoma, mafd2):
+    """Test that normalized endpoint correctly resolves queries, and utilizes
+    the VOD schema.
+    """
+    response = query_handler.normalize("Neuroblastoma")
+    assert response.match_type == MatchType.LABEL
+    assert response.warnings is None
+    compare_disease(response, neuroblastoma)
+    assert len(response.source_meta_) == 4
+    assert SourceName.NCIT in response.source_meta_
+    assert SourceName.DO in response.source_meta_
+    assert SourceName.MONDO in response.source_meta_
+    assert SourceName.ONCOTREE in response.source_meta_
+
+    response = query_handler.normalize("MAFD2")
+    assert response.match_type == MatchType.ALIAS
+    assert response.warnings is None
+    compare_disease(response, mafd2)
+    assert len(response.source_meta_) == 2
+    assert SourceName.MONDO in response.source_meta_
+
+
+def test_normalize_non_mondo(query_handler, skin_myo, neuroblastoma):
+    """Test that normalize endpoint returns records not in Mondo groups."""
+    response = query_handler.normalize("Skin Myoepithelioma")
+    assert response.match_type == MatchType.LABEL
+    assert len(response.source_meta_) == 1
+    compare_disease(response, skin_myo)
+    assert SourceName.NCIT in response.source_meta_
+
+    response = query_handler.normalize("Cutaneous Myoepithelioma")
+    assert response.match_type == MatchType.ALIAS
+    assert len(response.source_meta_) == 1
+    compare_disease(response, skin_myo)
+    assert SourceName.NCIT in response.source_meta_
+
+    response = query_handler.normalize("orphanet:635")
+    assert response.match_type == MatchType.XREF
+    assert len(response.source_meta_) == 4
+    compare_disease(response, neuroblastoma)
+    assert SourceName.NCIT in response.source_meta_
+    assert SourceName.DO in response.source_meta_
+    assert SourceName.MONDO in response.source_meta_
+    assert SourceName.ONCOTREE in response.source_meta_
+
+
+def test_service_meta(query_handler):
+    """Test service meta info in response."""
+    test_query = "glioma"
+
+    response = query_handler.search(test_query)
+    service_meta = response.service_meta_
+    assert service_meta.name == "disease-normalizer"
+    assert service_meta.version
+    assert isinstance(service_meta.response_datetime, datetime)
+    assert service_meta.url == "https://github.com/cancervariants/disease-normalization"
+
+    response = query_handler.normalize(test_query)
+    service_meta = response.service_meta_
+    assert service_meta.name == "disease-normalizer"
+    assert service_meta.version
+    assert isinstance(service_meta.response_datetime, datetime)
+    assert service_meta.url == "https://github.com/cancervariants/disease-normalization"
+
+    response = query_handler.search("this-will-not-normalize")
+    service_meta = response.service_meta_
+    assert service_meta.name == "disease-normalizer"
+    assert service_meta.version
+    assert isinstance(service_meta.response_datetime, datetime)
+    assert service_meta.url == "https://github.com/cancervariants/disease-normalization"
+
+    response = query_handler.normalize("this-will-not-normalize")
+    service_meta = response.service_meta_
+    assert service_meta.name == "disease-normalizer"
+    assert service_meta.version
+    assert isinstance(service_meta.response_datetime, datetime)
+    assert service_meta.url == "https://github.com/cancervariants/disease-normalization"
