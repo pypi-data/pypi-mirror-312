db764f8f24870f9b12b9f66288c20cfbfc70a970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:56 +0000

    Add package hrequests/0.9.2/hrequests-0.9.2-py3-none-any.whl

---
+++ packages/hrequests/hrequests-0.9.2-py3-none-any.whl/hrequests/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "hrequests"
+__description__ = "Webscraping for humans"
+__version__ = "0.9.2"
+__author__ = "daijro"
+__license__ = "MIT"
+
+# version of hrequests-cgo library
+BRIDGE_VERSION = '3.'
---
+++ packages/hrequests/hrequests-0.9.2-py3-none-any.whl/hrequests/exceptions.py
@@ -0,0 +1,42 @@
+class ClientException(IOError):
+    '''Error with the TLS client'''
+
+
+class BrowserException(Exception):
+    '''Base exceptions for render instances'''
+
+
+class EnableMockHumanException(BrowserException):
+    '''Exception raised when mock human is disabled, but captcha is called'''
+
+
+class BrowserTimeoutException(BrowserException):
+    '''Exception raised when playwright throws a timeout error'''
+
+
+class NotRenderedException(Exception):
+    '''Raise when the user tries to interact with an element that is not in a BrowserSession'''
+
+
+class JavascriptException(BrowserException):
+    '''Exception raised when a javascript error occurs'''
+
+
+class CacheDisabledError(BrowserException):
+    '''Tried to go back when cache was disabled'''
+
+
+class SelectorNotFoundException(Exception):
+    '''Exception raised when a css selector is not found'''
+
+
+class EncodingNotFoundException(Exception):
+    '''Exception raised when no encoding is detected'''
+
+
+class ProxyFormatException(ClientException):
+    '''Exception raised when a proxy format is not supported'''
+
+
+class MissingLibraryException(ClientException):
+    '''Exception raised when the browsing libraries are not installed'''
