1956b81858bf18f8ea077a7058e6ef4a9eddc2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:41 +0000

    Add package gptize/0.5.0/gptize-0.5.0-py3-none-any.whl

---
+++ packages/gptize/gptize-0.5.0-py3-none-any.whl/src/main.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import logging
+from .gptizer import GPTizer
+from .settings import Settings
+
+
+def parse_arguments():
+    default_output = Settings.default_output_file()
+    parser = argparse.ArgumentParser(
+        description="Gptize is a tool designed to concatenate the contents of project files into a single text file. It's specifically tailored for creating datasets that can be uploaded into ChatGPT for analysis or training.")
+    parser.add_argument("target", nargs='?', type=str, default=os.getcwd(),
+                        help="Target file or directory to process (default: current directory)")
+    parser.add_argument("-o", "--output", type=str, default=default_output,
+                        help=f"Output file path (default: {default_output})")
+    parser.add_argument("--ignore", type=str, default='.gptignore',
+                        help="Custom .gitignore file for gptize (default: .gptignore)")
+    parser.add_argument("--repo-root", type=str, default=os.getcwd(),
+                        help="Root directory of the repository where .gitignore is located (default: current directory)")
+    parser.add_argument("--debug", action="store_true",
+                        help="Enable debug logging (saves to gptize.log in the current directory)")
+    return parser.parse_args()
+
+
+def setup_logging(debug: bool):
+    log_level = logging.DEBUG if debug else logging.INFO
+    log_format = '%(asctime)s [%(levelname)s]: %(message)s'
+    logging.basicConfig(level=log_level, format=log_format)
+    if debug:
+        logging.getLogger().addHandler(logging.FileHandler('gptize.log'))
+
+
+def main():
+    args = parse_arguments()
+    setup_logging(args.debug)
+
+    try:
+        gptizer = GPTizer()
+        if os.path.isdir(args.target):
+            gptizer.process_directory(args.target, args.repo_root, args.ignore)
+        elif os.path.isfile(args.target):
+            gptizer.process_file(args.target, args.repo_root, args.ignore)
+        else:
+            raise ValueError(f"Invalid target: {args.target}")
+        output_file_name = Settings.custom_output_file(gptizer.project.name, args.target)
+        if args.output == Settings.default_output_file():
+            args.output = output_file_name
+        combined_content = gptizer.combine_files()
+        with open(args.output, 'w', encoding='utf-8') as file:
+            file.write(combined_content)
+            logging.info(f"Files were combined into {args.output}")
+    except FileNotFoundError as e:
+        logging.error(f"File not found: {e}")
+    except ValueError as e:
+        logging.error(f"ValueError occurred: {e}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gptize/gptize-0.5.0-py3-none-any.whl/src/settings.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import os
+
+
+class Settings:
+    DEFAULT_ENCODINGS = ['utf-8', 'latin-1', 'cp1252']
+    IGNORED_DIRECTORIES = ['.git', '.svn', '__pycache__']
+    GITIGNORE_PATH = '.gitignore'
+    MAX_FILE_SIZE_BYTES_LIMIT = 512 * 1024 * 1024  # 512 MB
+    MAX_TOKEN_COUNT_LIMIT = 2000000  # 2 million tokens
+    WARN_LINES_COUNT = 700
+    GPT4O_CONTEXT_WINDOW = 128000  # Token context window for GPT-4o
+    TOKEN_MODEL_NAME = 'o200k_base'
+    TOP_TOKEN_FILES_COUNT = 10
+
+    @staticmethod
+    def default_output_file():
+        """Returns the default output file name with the current date and time."""
+        current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        return f"gptize-output-{current_time}.txt"
+
+    @staticmethod
+    def custom_output_file(project_name: str, target: str):
+        """
+        Returns the output file name, including the project name and the current date and time.
+
+        Parameters:
+        project_name (str): The name of the project to be included in the file name.
+        target (str): The target file or directory path.
+        """
+        base_name = os.path.basename(target).replace(' ', '_')
+        if not base_name or os.path.isdir(target):
+            base_name = 'folder' if os.path.isdir(target) else 'file'
+
+        # Use the project name in the output file name if provided
+        project_part = project_name.replace(' ', '_') if project_name else base_name
+
+        current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        return f"gptize-output-{project_part}-{current_time}.txt"
