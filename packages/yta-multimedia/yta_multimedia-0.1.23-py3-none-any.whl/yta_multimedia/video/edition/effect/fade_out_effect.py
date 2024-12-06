fb25bca8dd44bf73e9e911fd099d3b95b4fc00d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:02 +0000

    Add package yta-multimedia/0.1.23/yta_multimedia-0.1.23-py3-none-any.whl

---
+++ packages/yta-multimedia/yta_multimedia-0.1.23-py3-none-any.whl/yta_multimedia/video/edition/effect/fade_out_effect.py
@@ -0,0 +1,33 @@
+from yta_multimedia.video.edition.effect.m_effect import MEffect as Effect
+from yta_multimedia.video.parser import VideoParser
+from yta_general_utils.color import Color, ColorString
+from moviepy import Clip
+from moviepy.video.fx import FadeOut as MoviepyFadeOut
+from typing import Union
+
+
+class FadeOutEffect(Effect):
+    """
+    This effect will make the video disappear 
+    progressively lasting the provided 'duration' 
+    time or the whole clip duration if None
+    'duration' provided.
+
+    The 'color' provided must be a valid color string, 
+    array, tuple or Color instance, or will be set as 
+    pure black if None provided.
+    """
+    def apply(self, video: Clip, duration: float, color: Union[list, tuple, str, Color] = None) -> Clip:
+        # TODO: This is not working properly yet
+        #PythonValidator.validate_method_params(BlinkEffect.apply, locals(), ['video'])
+        video = VideoParser.to_moviepy(video)
+
+        if not duration:
+            duration = video.duration
+
+        if color is None:
+            color = Color.parse(ColorString.BLACK).as_rgb_array()
+        else:
+            color = Color.parse(color).as_rgb_array()
+
+        return MoviepyFadeOut(duration, color).apply(video)
