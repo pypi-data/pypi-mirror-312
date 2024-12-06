c2c2309910d0f44c62b5a2143837ee52893dbe85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:30 +0000

    Add package django-afip/13.1.0/django_afip-13.1.0.tar.gz

---
+++ packages/django-afip/django_afip-13.1.0.tar.gz/django_afip-13.1.0/tests/test_management.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import pytest
+from django.core import management
+
+from django_afip.models import GenericAfipType
+
+
+@pytest.mark.django_db
+def test_afip_metadata_command() -> None:
+    assert len(GenericAfipType.SUBCLASSES) == 7
+
+    for model in GenericAfipType.SUBCLASSES:
+        # TYPING: mypy doesn't know about `.objects`.
+        assert model.objects.count() == 0  # type: ignore[attr-defined]
+
+    management.call_command("afipmetadata")
+
+    for model in GenericAfipType.SUBCLASSES:
+        # TYPING: mypy doesn't know about `.objects`.
+        assert model.objects.count() > 0  # type: ignore[attr-defined]
