cd314bf95fa6a85d2a5aa94bd360033be6300ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:09 +0000

    Add package DownloadKit/2.0.7/DownloadKit-2.0.7.tar.gz

---
+++ packages/DownloadKit/DownloadKit-2.0.7.tar.gz/DownloadKit-2.0.7/setup.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+from DownloadKit import __version__
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="DownloadKit",
+    version=__version__,
+    author="g1879",
+    author_email="g1879@qq.com",
+    description="一个简洁易用的多线程文件下载工具。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    keywords="DownloadKit",
+    url="https://gitee.com/g1879/DownloadKit",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "DataRecorder>=3.4.11"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    python_requires='>=3.6'
+)
