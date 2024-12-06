f9c3c58715b3092442230acdac7a34dd6284eeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:52 +0000

    Add package mlb-showdown-bot/3.9.3/mlb_showdown_bot-3.9.3-py3-none-any.whl

---
+++ packages/mlb-showdown-bot/mlb_showdown_bot-3.9.3-py3-none-any.whl/mlb_showdown_bot/__main__.py
@@ -0,0 +1,173 @@
+import argparse
+from pprint import pprint
+import os, json
+from pathlib import Path
+
+# MY PACKAGES
+try:
+    # ASSUME THIS IS A SUBMODULE IN A PACKAGE
+    from .baseball_ref_scraper import BaseballReferenceScraper
+    from .showdown_player_card import ShowdownPlayerCard
+    from .classes.stats_period import StatsPeriod
+    from .postgres_db import PostgresDB, PlayerArchive
+except ImportError:
+    # USE LOCAL IMPORT 
+    from baseball_ref_scraper import BaseballReferenceScraper
+    from showdown_player_card import ShowdownPlayerCard
+    from classes.stats_period import StatsPeriod
+    from postgres_db import PostgresDB, PlayerArchive
+
+parser = argparse.ArgumentParser(description="Generate a player's MLB Showdown stats in a given year")
+
+# REQUIRED INPUTS
+parser.add_argument('-n','--name', help='The first and last name of the player',required=True)
+parser.add_argument('-y','--year', help='The year of the player',required=True)
+
+# SET
+parser.add_argument('-s','--set',help='The showdown set meta to use (2000-2005)',default='2000')
+parser.add_argument('-e','--era',help='The baseball era to use.',default='DYNAMIC')
+
+# IMAGE
+parser.add_argument('-url','--image_url',help='URL link to Player background image',default=None)
+parser.add_argument('-path','--image_path',help='Path to Player background image on local machine',default=None)
+parser.add_argument('-o_path','--image_output_path',help='Path to folder for card image output',default='')
+parser.add_argument('-exp','--expansion',help='Add optional expansion logo (ex: TD, PR)',default='BS')
+parser.add_argument('-ed','--edition',help='Add optional edition (Values: None, Super Season, All-Star Game, Cooperstown Collection, Holiday, Nationality, Rookie Season)',default='NONE')
+parser.add_argument('-num','--set_num',help='Assign card a set number',default='')
+parser.add_argument('-show','--show_image', action='store_true', help='Optionally open the final Player Card Image upon completion')
+parser.add_argument('-pl','--parallel', help='Optionally add image parallel design like Rainbow Foil, Black & White, Sparkle, etc.', default='NONE', type=str)
+parser.add_argument('-bor','--add_border', action='store_true', help='Optionally add border to player image')
+parser.add_argument('-dark','--dark_mode', action='store_true', help='Optionally toggle dark mode (2022+ sets only)')
+parser.add_argument('-sypls','--set_year_plus_one', action='store_true', help='Optionally add one to the set year on 04/05 set.')
+parser.add_argument('-htl','--hide_team_logo', action='store_true', help='Optionally remove all team logos and branding.')
+parser.add_argument('-sc','--secondary_color', action='store_true', help='Used secondary team color.')
+parser.add_argument('-mc','--is_multi_colored', action='store_true', help='Use multiple colors for chart image (CLASSIC, EXPANDED only)')
+parser.add_argument('-sh', '--stat_highlights_type', help='Type of stat highlights to use. (OLD_SCHOOL, MODERN, ALL)', default='NONE', type=str)
+
+# STATS
+parser.add_argument('-sp','--stats_period', help='Period to use for stats. Allowed options are REGULAR,DATES,POST,SPLIT', default='REGULAR', type=str)
+parser.add_argument('-start','--start_date', help='Optional Start Date for stats. Only available post-1900.', default=None, type=str)
+parser.add_argument('-end','--end_date', help='Optional End Date for stats. Only available post-1900.', default=None, type=str)
+parser.add_argument('-spl','--split_name', help='Create a card using the splits page on baseball reference', default=None, type=str)
+parser.add_argument('-co','--co_override',help='Manually select a command/out combination',default=None, type=str)
+parser.add_argument('-vs','--variable_spd', action='store_true', help='Optionally toggle variable speed (2000 + 2001 sets only)')
+parser.add_argument('-cv','--chart_version',help='Get alternate chart n away from most accurate',default=1)
+parser.add_argument('-yrt','--show_year_text', action='store_true', help='Optionally add separate year text to the image. Applies to 2000-2005 only.')
+parser.add_argument('-nick','--nickname_index', help='Optionally choose a nickname to show for images. Enter a number based on ordering from bref, max is 3', default=None, type=int)
+parser.add_argument('-wotc','--is_wotc', action='store_true', help='Try loading from WOTC cards.')
+
+# CACHE
+parser.add_argument('-isl','--ignore_showdown_library', action='store_true', help='Optionally force ignore Showdown Library, will create card live.')
+parser.add_argument('-ic','--ignore_cache', action='store_true', help='Ignore local cache')
+parser.add_argument('-ia','--ignore_archive', action='store_true', help='Ignore postgres showdown bot archive')
+parser.add_argument('-dc','--disable_cache_cleaning', action='store_true', help='Disable removal of cached files after minutes threshold.')
+
+
+args = parser.parse_args()
+
+def main():
+    """ Executed when running as a script """
+
+    name = args.name.title()
+    year = args.year
+    set = args.set
+    command_out_override = None if args.co_override is None else tuple([int(x) for x in args.co_override.split('-')])
+
+    stats_period = StatsPeriod(
+        type=args.stats_period,
+        year=year,
+        start_date=args.start_date,
+        end_date=args.end_date,
+        split=args.split_name
+    )
+    scraper = BaseballReferenceScraper(name=name,year=year,stats_period=stats_period,ignore_cache=args.ignore_cache, disable_cleaning_cache=args.disable_cache_cleaning)
+    
+    # FETCH REAL STATS FROM EITHER:
+    #  1. ARCHIVE: HISTORICAL DATA IN POSTGRES DB
+    #  2. SCRAPER: LIVE REQUEST FOR BREF/SAVANT DATA
+    
+    player_archive: PlayerArchive = None
+    archive_load_time: float = None
+    statline: dict[str: any] = None
+    data_source = 'Scraper'
+    if not args.ignore_archive:
+        postgres_db = PostgresDB(is_archive=True)
+        player_archive, archive_load_time = postgres_db.fetch_player_stats_from_archive(year=scraper.year_input, bref_id=scraper.baseball_ref_id, team_override=scraper.team_override, type_override=scraper.player_type_override, stats_period_type=stats_period.type)
+        postgres_db.close_connection()
+    
+    if player_archive:
+        statline = player_archive.stats
+        data_source = 'Archive'
+    else:
+        statline = scraper.player_statline()
+        data_source = scraper.source
+
+    # WOTC CARD
+    showdown: ShowdownPlayerCard = None
+    if args.is_wotc:
+
+        # LOAD FROM JSON
+        file_path = os.path.join(Path(os.path.dirname(__file__)), 'data', 'wotc_player_card_set.json')
+        with open(file_path, "r") as json_file:
+            wotc_set_data = json.load(json_file)
+
+        # MATCH ON ID
+        fields = [args.year, scraper.baseball_ref_id, args.set, args.expansion,]
+        if scraper.player_type_override:
+            fields.append(scraper.player_type_override)
+        showdown_card_id = "-".join(fields)
+
+        # GET CARD
+        wotc_card_data: dict = wotc_set_data.get('cards', {}).get(showdown_card_id, None)
+        if wotc_card_data:
+            showdown = ShowdownPlayerCard(**wotc_card_data)
+            showdown.print_player()
+            if args.show_image:
+                showdown.card_image(show=True)
+            pts = showdown.calculate_points(projected=showdown.projected, positions_and_defense=showdown.positions_and_defense, speed_or_ip=showdown.ip if showdown.is_pitcher else showdown.speed.speed)
+            print(f"\nProjected PTS: {pts.total_points}  Actual PTS: {showdown.points}  Diff: {showdown.points - pts.total_points}")
+            print(f"{pts.breakdown_str}")
+
+    # CREATE SHOWDOWN CARD FROM STATLINE
+    stats_period = scraper.stats_period or stats_period
+    if showdown is None:
+        showdown = ShowdownPlayerCard(
+            name=name,
+            year=year,
+            stats_period=stats_period,
+            stats=statline,
+            set=set,
+            era=args.era,
+            expansion=args.expansion,
+            edition=args.edition,
+            chart_version=args.chart_version,
+            player_image_url=args.image_url,
+            player_image_path=args.image_path,
+            card_img_output_folder_path=args.image_output_path,
+            print_to_cli=True,
+            show_image=args.show_image,
+            set_number=str(args.set_num),
+            command_out_override=command_out_override,
+            add_image_border=args.add_border,
+            is_dark_mode=args.dark_mode,
+            is_variable_speed_00_01=args.variable_spd,
+            parallel=args.parallel,
+            show_year_text=args.show_year_text,
+            set_year_plus_one=args.set_year_plus_one,
+            hide_team_logo=args.hide_team_logo,
+            use_secondary_color=args.secondary_color,
+            is_multi_colored=args.is_multi_colored,
+            stat_highlights_type=args.stat_highlights_type,
+            source=data_source,
+            disable_cache_cleaning=args.disable_cache_cleaning,
+            nickname_index=args.nickname_index,
+            ignore_cache=args.ignore_cache,
+            warnings=scraper.warnings
+        )
+
+    # PRINT TOTAL LOAD TIME
+    total_load_time = (scraper.load_time if scraper.load_time else 0.00) + (showdown.load_time if showdown.load_time else 0.00) + (archive_load_time if archive_load_time else 0.0)
+    print(f"LOAD TIME: {total_load_time:.2f}s")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mlb-showdown-bot/mlb_showdown_bot-3.9.3-py3-none-any.whl/mlb_showdown_bot/scripts/auto_image_suggestions.py
@@ -0,0 +1,120 @@
+import argparse
+from datetime import datetime
+import os
+from pprint import pprint
+import sys
+from pathlib import Path
+from prettytable import PrettyTable
+sys.path.append(os.path.join(Path(os.path.join(os.path.dirname(__file__))).parent))
+from postgres_db import PostgresDB, PlayerArchive
+
+parser = argparse.ArgumentParser(description="Search baseball reference for best auto images to add.")
+parser.add_argument('-pst','--player_subtype', help='Player Sub Types (POSITION_PLAYER, STARTING_PITCHER, RELIEF_PITCHER)', type=str, required=False, default=None)
+parser.add_argument('-hof','--hof', action='store_true', help='Only Hall of Fame Players', required=False)
+parser.add_argument('-v','--mvp', action='store_true', help='Only MVPs', required=False)
+parser.add_argument('-cy','--cya', action='store_true', help='Only CYAs', required=False)
+parser.add_argument('-gg','--gold_glove', action='store_true', help='Only Gold Glove Winners', required=False)
+parser.add_argument('-ys','--year_start', help='Optional year start filter', type=int, required=False, default=None)
+parser.add_argument('-ye','--year_end', help='Optional year end filter', type=int, required=False, default=None)
+parser.add_argument('-l','--limit', help='Optional limit', type=int, required=False, default=None)
+parser.add_argument('-tm', '--team', help='Optional team filter', required=False, default=None)
+parser.add_argument('-yt', '--year_threshold', help='Optional year threshold. Only includes images that are <= the threshold.', required=False, type=int, default=None)
+parser.add_argument('-sort', '--sort', help='Optional sort field', required=False, default='bWar')
+args = parser.parse_args()
+
+def fetch_image_file_list() -> list[str]:
+    file_names = []
+    path = os.environ.get('AUTO_IMAGE_PATH', None)
+    if not path:
+        return []
+    
+    for _, _, files in os.walk(path):
+        for name in files:
+            file_names.append(name)
+    return file_names
+
+def fetch_player_data() -> list[PlayerArchive]:
+
+    # LIST OF YEAR INTS FROM 1900 TO NOW
+    # GET CURRENT YEAR
+    current_year = datetime.now().year
+    year_list = list(range(1900, current_year + 1))
+    
+    # FILTER OUT YEARS BETWEEN YEAR START AND YEAR END ARGS
+    if args.year_start is not None:
+        year_list = [year for year in year_list if year >= args.year_start]
+    if args.year_end is not None:
+        year_list = [year for year in year_list if year <= args.year_end]
+
+    db = PostgresDB(is_archive=True)
+
+    if db.connection is None:
+        print("ERROR: NO CONNECTION TO DB")
+    
+    war_field = f"(case when length((stats->>'{args.sort}')) = 0 then 0.0 else (stats->>'{args.sort}')::float end)"
+    player_data = db.fetch_all_stats_from_archive(year_list=year_list, limit=args.limit, order_by=war_field, exclude_records_with_stats=False)
+
+    return player_data
+
+
+# GRAB DATA FROM BREF
+image_list = fetch_image_file_list()
+player_data = fetch_player_data()
+
+if len(player_data) == 0:
+    print("NO PLAYERS FOUND")
+
+player_tbl = PrettyTable(field_names=['Player', 'Team', 'Year', 'Position', 'G', 'GS', 'bWAR', 'OPS', 'ERA', 'HOF', 'MVP', 'CYA'])
+
+for player in player_data:
+    
+    bwar = player.stats.get('bWAR', 0)
+    is_hof = player.stats.get('is_hof', False)
+    awards = player.stats.get('award_summary', '').split(',')
+    games = str(player.g)
+    games_started = str(player.gs or '-')
+    era = str(player.stats.get('earned_run_avg', '-'))
+    ops = str(player.stats.get('onbase_plus_slugging', '-')).replace('0.','.')
+    mvp_str = 'X' if 'MVP-1' in awards else ''
+    cy_str = 'X' if 'CYA-1' in awards else ''
+    hof_str = 'X' if is_hof else ''
+
+    # SKIP IF PLAYER IS IN IMAGE LIST
+    images = [image for image in image_list if player.bref_id in image and f'({player.team_id})' in image and (abs(int(image.split('-')[1]) - player.year) <= args.year_threshold if args.year_threshold is not None else True)]    
+    if len(images) > 0:
+        continue
+
+    # TEAM CHECK
+    if args.team and args.team != player.team_id: continue
+
+    # HOF CHECK
+    if args.hof and not is_hof: continue
+
+    # MVP CHECK
+    if args.mvp and 'MVP-1' not in awards: continue
+
+    # CYA CHECK
+    if args.cya and 'CYA-1' not in awards: continue
+
+    # GG CHECK
+    if args.gold_glove and 'GG' not in awards: continue
+
+    if args.player_subtype and args.player_subtype.upper() != player.player_subtype: continue
+
+    # PRINT PLAYER'S NAME, TEAM, AND YEAR
+    player_tbl.add_row([
+        player.name,
+        player.team_id,
+        player.year,
+        ",".join(player.primary_positions),
+        games,
+        games_started,
+        bwar,
+        ops,
+        era,
+        hof_str,
+        mvp_str,
+        cy_str
+    ])
+
+print(player_tbl)
