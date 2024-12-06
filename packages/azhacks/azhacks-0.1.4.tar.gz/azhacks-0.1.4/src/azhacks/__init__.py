28ecca7a706e28d60473b3c03ec430145bc12707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:25 +0000

    Add package azhacks/0.1.4/azhacks-0.1.4.tar.gz

---
+++ packages/azhacks/azhacks-0.1.4.tar.gz/azhacks-0.1.4/src/azhacks/__init__.py
@@ -0,0 +1,11 @@
+from azhacks.container_manager import ContainerManager
+from azhacks.queue_manager import QueueManager
+from azhacks.storage_resource_manager import StorageResourceManager
+from azhacks.table_manager import TableManager
+
+__all__ = [
+    "ContainerManager",
+    "QueueManager",
+    "StorageResourceManager",
+    "TableManager",
+]
