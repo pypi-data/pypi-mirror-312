5454a89da68932c650334e336db6b642a0f492d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:09 +0000

    Add package seleniumproxy/0.1.1/seleniumproxy-0.1.1.tar.gz

---
+++ packages/seleniumproxy/seleniumproxy-0.1.1.tar.gz/seleniumproxy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seleniumproxy", 
+    version="0.1.1",  
+    description="Considering that there is no function that works very well natively in Selenium for using proxys with authentication in chromedriver, this is an effective way to solve this problem.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Kairo Trzeciak",
+    author_email="trz.kairodev@gmail.com",
+    url="https://github.com/kairodev/selenium-proxy",
+    license="MIT",
+    packages=find_packages(),  
+    install_requires=[
+        "selenium",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  
+)
