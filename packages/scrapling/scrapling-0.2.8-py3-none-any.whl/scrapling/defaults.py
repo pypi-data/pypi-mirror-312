9a9b1b24388c75d2506e52da838422d9670a639f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:41 +0000

    Add package scrapling/0.2.8/scrapling-0.2.8-py3-none-any.whl

---
+++ packages/scrapling/scrapling-0.2.8-py3-none-any.whl/scrapling/defaults.py
@@ -0,0 +1,6 @@
+from .fetchers import Fetcher, PlayWrightFetcher, StealthyFetcher
+
+# If you are going to use Fetchers with the default settings, import them from this file instead for a cleaner looking code
+Fetcher = Fetcher()
+StealthyFetcher = StealthyFetcher()
+PlayWrightFetcher = PlayWrightFetcher()
