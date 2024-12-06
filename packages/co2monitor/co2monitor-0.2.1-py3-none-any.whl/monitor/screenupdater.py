2b0bc73837eb1e6dc0d1207f612fd309f57b5c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:37 +0000

    Add package co2monitor/0.2.1/co2monitor-0.2.1-py3-none-any.whl

---
+++ packages/co2monitor/co2monitor-0.2.1-py3-none-any.whl/monitor/screenupdater.py
@@ -0,0 +1,35 @@
+import sys
+import threading
+
+from loguru import logger
+
+from monitor.api import EnergyData
+from monitor.handler import Handler
+
+try:
+    from monitor.screen import show_info
+except (ModuleNotFoundError, RuntimeError):
+    # A RuntimeError is thrown if the EPD library cannot find the required
+    # files to write to the screen.
+    if "screen" in sys.argv:
+        logger.error(
+            "We cannot communicate with the screen. "
+            + "Verify that the requirements are installed with `pip install co2monitor[screen]'. "
+            + "If that is not sufficient, verify that you are running on a raspberry pi. "
+            + "Check waveshare docs."
+        )
+        raise
+
+
+class ScreenUpdater(Handler):
+    def __init__(self):
+        super().__init__()
+        self.lock = threading.Lock()
+
+    def handle(self, data: EnergyData):
+        t = threading.Thread(target=lambda: self._update_screen(data))
+        t.start()
+
+    def _update_screen(self, data: EnergyData):
+        with self.lock:
+            show_info(data)
---
+++ packages/co2monitor/co2monitor-0.2.1-py3-none-any.whl/monitor/tray.py
@@ -0,0 +1,63 @@
+import signal
+import sys
+import tempfile
+
+from loguru import logger
+
+try:
+    from PIL import Image, ImageDraw, ImageFont
+    from PySide6.QtGui import QIcon
+    from PySide6.QtWidgets import QApplication, QSystemTrayIcon
+except ModuleNotFoundError:
+    if "tray" in sys.argv:
+        logger.error(
+            "The required dependencies were not installed - "
+            + "a system tray icon cannot be created. "
+            + "Install them via `pip install co2monitor[tray]'."
+        )
+        raise
+
+from monitor.api import EnergyData
+from monitor.handler import Handler
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
+class SystrayCreator(Handler):
+    def __init__(self):
+        super().__init__()
+        self.image_file = tempfile.NamedTemporaryFile(suffix=".png")
+
+        self.app = QApplication(sys.argv)
+        self.app.setQuitOnLastWindowClosed(False)
+
+        self.icon = QSystemTrayIcon()
+        self.icon.show()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        logger.debug("Exiting")
+        self.image_file.close()
+        self.app.exit()
+
+    def handle(self, data: EnergyData):
+        co2: float = data.co2_amount * 1000
+
+        image = self.image(str(int(co2)), self.font)
+        filename = self.image_file.name
+        image.save(filename)
+
+        self.icon.setIcon(QIcon(filename))
+        self.icon.setToolTip(
+            f"{data.datetime_ams.strftime('%H:%M')}: {co2:.2f}g CO₂/kWh"
+        )
+
+    @staticmethod
+    def image(text, font):
+        image = Image.new("RGB", (64, 64), "white")
+        draw = ImageDraw.Draw(image)
+        draw.text((1, 10), text, fill="black", font=font)
+        return image
+
+    @property
+    def font(self):
+        return ImageFont.load_default(size=36)
