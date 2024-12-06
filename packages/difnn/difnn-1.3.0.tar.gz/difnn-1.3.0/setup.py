37d30b687c949a4d5d2f7ebb210c64247babaa4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:02 +0000

    Add package difnn/1.3.0/difnn-1.3.0.tar.gz

---
+++ packages/difnn/difnn-1.3.0.tar.gz/difnn-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="difnn",  
+    version="1.3.0",
+    description="difnn",  
+    long_description=open("README.md").read(), 
+    long_description_content_type="text/markdown",
+    author="Forecaster",  
+    packages=find_packages(),  
+    package_data={
+        "difnn": ["forecast_utility.cp312-win_amd64.pyd"],  
+    },
+    include_package_data=True,  
+    install_requires=open("requirements.txt").read().splitlines(),  
+    entry_points={
+        "console_scripts": [
+            "difnn=difnn.main:main",  #
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  
+)
