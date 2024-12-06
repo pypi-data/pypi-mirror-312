4f2409f9802641bc1c19d7108061040507e29ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:10 +0000

    Add package chatrouter/1.0.5/chatrouter-1.0.5.tar.gz

---
+++ packages/chatrouter/chatrouter-1.0.5.tar.gz/chatrouter-1.0.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="chatrouter",
+    version="v1.0.5",
+    description="Typed router for chatbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="guangrei",
+    author_email="myawn@pm.me",
+    url="https://github.com/cirebon-dev/chatrouter",
+    packages=["chatrouter"],
+    package_data={"chatrouter": ["py.typed"]},
+    license="MIT",
+    platforms="any",
+    install_requires=["typing-extensions", "types-aiofiles"],
+)
