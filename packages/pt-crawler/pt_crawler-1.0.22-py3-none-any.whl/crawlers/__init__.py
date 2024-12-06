f72e8c13554d9845a85253c0fb29fb726a361f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:31 +0000

    Add package pt-crawler/1.0.22/pt_crawler-1.0.22-py3-none-any.whl

---
+++ packages/pt-crawler/pt_crawler-1.0.22-py3-none-any.whl/crawlers/__init__.py
@@ -0,0 +1,22 @@
+from .base import Crawler
+from .base import Torrent
+from .base import Promotion
+from .base import User
+
+from .chdbits import CHDBits
+from .mteam import MTeam
+from .fsm import FSM
+from .ttg import TTG
+from .pterclub import PTerClub
+from .redleaves import RedLeaves
+from .opencd import OpenCD
+from .ourbits import OurBits
+from .u2 import U2
+from .ubits import UBits
+from .lemonhd import LemonHD
+from .keepfrds import KeepFriends
+from .hdtime import HDTime
+
+VERSION = '1.0.22'
+
+__all__ = ['VERSION']
