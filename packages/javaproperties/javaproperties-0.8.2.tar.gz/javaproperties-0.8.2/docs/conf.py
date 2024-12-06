7c1cdc74de57f236a4e4ccc0998e7cfb40212654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:31 +0000

    Add package javaproperties/0.8.2/javaproperties-0.8.2.tar.gz

---
+++ packages/javaproperties/javaproperties-0.8.2.tar.gz/javaproperties-0.8.2/docs/conf.py
@@ -0,0 +1,46 @@
+from javaproperties import __version__
+
+project = "javaproperties"
+author = "John T. Wodder II"
+copyright = "2016-2024 John T. Wodder II"  # noqa: A001
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+]
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+}
+# NOTE: Do not set 'inherited-members', as it will cause all of the
+# MutableMapping methods to be listed under `Properties`.
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+exclude_patterns = ["_build"]
+source_suffix = ".rst"
+source_encoding = "utf-8"
+master_doc = "index"
+version = __version__
+release = __version__
+today_fmt = "%Y %b %d"
+default_role = "py:obj"
+pygments_style = "sphinx"
+
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "prev_next_buttons_location": "both",
+}
+html_last_updated_fmt = "%Y %b %d"
+html_show_sourcelink = True
+html_show_sphinx = True
+html_show_copyright = True
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
