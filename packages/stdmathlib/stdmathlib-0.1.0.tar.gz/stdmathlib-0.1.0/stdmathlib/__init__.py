d8eff7f2f7a98b1d61776e2de30ce3c16f509500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:32 +0000

    Add package stdmathlib/0.1.0/stdmathlib-0.1.0.tar.gz

---
+++ packages/stdmathlib/stdmathlib-0.1.0.tar.gz/stdmathlib-0.1.0/stdmathlib/__init__.py
@@ -0,0 +1,12 @@
+# stdmath/__init__.py
+
+from .algebra import solve_linear, quadratic_formula
+from .ode import derivative
+
+__all__ = [
+    'solve_linear',
+    'quadratic_formula',
+    'solve_equation',
+    'derivative',
+    'solve_equation',
+]
