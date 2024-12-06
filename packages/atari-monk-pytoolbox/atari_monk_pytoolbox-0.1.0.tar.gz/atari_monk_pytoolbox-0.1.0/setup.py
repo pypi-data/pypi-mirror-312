91807e08a646d101af83309709374798ff950470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:17 +0000

    Add package atari-monk-pytoolbox/0.1.0/atari_monk_pytoolbox-0.1.0.tar.gz

---
+++ packages/atari-monk-pytoolbox/atari_monk_pytoolbox-0.1.0.tar.gz/atari_monk_pytoolbox-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atari_monk_pytoolbox",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={},
+    install_requires=[],
+    description="A tools package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="atari monk",
+    author_email="atari.monk1@gmail.com",
+    url="https://github.com/atari-monk/pytoolbox",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.12.0",
+)
