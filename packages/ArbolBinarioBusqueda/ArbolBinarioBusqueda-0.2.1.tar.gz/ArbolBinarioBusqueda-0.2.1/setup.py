7a6f1d73ece5faa18889b617a29cbc68d746c2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:18 +0000

    Add package ArbolBinarioBusqueda/0.2.1/ArbolBinarioBusqueda-0.2.1.tar.gz

---
+++ packages/ArbolBinarioBusqueda/ArbolBinarioBusqueda-0.2.1.tar.gz/ArbolBinarioBusqueda-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ArbolBinarioBusqueda",  # Nombre del paquete (único en PyPI)
+    version="0.2.1",    # Versión del paquete
+    author="Felipe Morales",
+    author_email="felipe.morales.querol@gmail.com",
+    description="Mostrar una implementación simple y totalmente en español con fines académicos",
+    long_description=open("README.md").read(),  # Descripción larga desde README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/felipemoralesquerol/arbolbinariobusqueda",  # URL del repositorio
+    packages=find_packages(),  # Busca automáticamente los submódulos
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  # Versión mínima de Python requerida
+    install_requires=[        # Dependencias del paquete
+        
+    ],
+)
