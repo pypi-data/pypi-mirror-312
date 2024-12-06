d3dbe539a64ba15ab41c5005552c45c307be93db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:14 +0000

    Add package ageqrp/0.1.0/ageqrp-0.1.0-py3-none-any.whl

---
+++ packages/ageqrp/ageqrp-0.1.0-py3-none-any.whl/ageqrp/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Chris Joakim <christopher.joakim@gmail.com>"
+__version__ = "0.1.0"
+
+AUTHOR = __author__
+VERSION = __version__
+
+"""
+ageqrp library 
+"""
+
+from ageqrp.query_result_parser import QueryResultParser
