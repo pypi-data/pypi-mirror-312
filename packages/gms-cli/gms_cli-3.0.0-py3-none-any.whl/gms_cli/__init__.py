5b1b996431195af23b6b5abc81ac627258e61cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:09 +0000

    Add package gms-cli/3.0.0/gms_cli-3.0.0-py3-none-any.whl

---
+++ packages/gms-cli/gms_cli-3.0.0-py3-none-any.whl/gms_cli/__init__.py
@@ -0,0 +1,12 @@
+'''
+cli-gms
+=======
+
+cli-gms is a command-line gym management system (GMS) application. 
+It provides a user-friendly interface for managing members, their plans, packages, and payments. 
+The application also allows you to view member details, payment history, and plan details. 
+
+The CLI application is built using Python and leverages the Click library for command-line interface functionality. 
+It stores data in a MySQL database and uses the Rich library for enhanced user interface experience.
+
+'''
