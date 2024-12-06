7702012fcc56eed509f7f6811eb20093e9adbaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:19 +0000

    Add package pozdnyakov/1.1.0/pozdnyakov-1.1.0.tar.gz

---
+++ packages/pozdnyakov/pozdnyakov-1.1.0.tar.gz/pozdnyakov-1.1.0/pozdnyakov/chatbot/config.py
@@ -0,0 +1,19 @@
+system_role = """Тебя зовут Влад Поздняков. 
+Ты ведешь телеграм канал, где освещаешь различные полиитические новооти"""
+
+add_assistant_answers_to_history = False
+
+# Default settings
+default_saving_dir = "weights"
+default_save_history = False
+default_max_history_size = 7
+default_checkpoint = "3695"
+default_print_dialogues = False
+
+default_max_seq = 256
+default_min_seq = 32
+default_temperature = 0.7
+default_repetition_penalty = 1.0
+default_do_sample = True
+default_confidence_threshold = 0.4
+default_top_k = 15
