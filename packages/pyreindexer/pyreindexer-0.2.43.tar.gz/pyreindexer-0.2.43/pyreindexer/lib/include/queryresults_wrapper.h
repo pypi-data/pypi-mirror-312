92057d9be84ee7a3c8e23c60e2e4b6444e674e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:22:26 +0000

    Add package pyreindexer/0.2.43/pyreindexer-0.2.43.tar.gz

---
+++ packages/pyreindexer/pyreindexer-0.2.43.tar.gz/pyreindexer-0.2.43/pyreindexer/lib/include/queryresults_wrapper.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "core/type_consts.h"
+#include "reindexerinterface.h"
+
+#ifdef PYREINDEXER_CPROTO
+#include "client/cororeindexer.h"
+#else
+#include "core/reindexer.h"
+#endif
+
+namespace pyreindexer {
+
+#ifdef PYREINDEXER_CPROTO
+using DBInterface = ReindexerInterface<reindexer::client::CoroReindexer>;
+using QueryResultsT = reindexer::client::CoroQueryResults;
+#else
+using DBInterface = ReindexerInterface<reindexer::Reindexer>;
+using QueryResultsT = reindexer::QueryResults;
+#endif
+
+class QueryResultsWrapper {
+public:
+	QueryResultsWrapper() : qresPtr(kResultsJson) {}
+	size_t Count() const { return qresPtr.Count(); }
+	void GetItemJSON(reindexer::WrSerializer& wrser, bool withHdrLen) { itPtr.GetJSON(wrser, withHdrLen); }
+	void Next() {
+		assert(db_);
+		db_->FetchResults(*this);
+	}
+
+	const std::vector<reindexer::AggregationResult>& GetAggregationResults() & { return qresPtr.GetAggregationResults(); }
+	const std::vector<reindexer::AggregationResult>& GetAggregationResults() && = delete;
+
+private:
+	friend DBInterface;
+
+	void iterInit() { itPtr = qresPtr.begin(); }
+
+	DBInterface* db_ = nullptr;
+	QueryResultsT qresPtr;
+	QueryResultsT::Iterator itPtr;
+};
+
+}  // namespace pyreindexer
