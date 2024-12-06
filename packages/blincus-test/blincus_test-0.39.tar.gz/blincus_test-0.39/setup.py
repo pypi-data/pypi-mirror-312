78f1269e55f0d85bf084823a67cbd5f778555a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:14 +0000

    Add package blincus-test/0.39/blincus_test-0.39.tar.gz

---
+++ packages/blincus-test/blincus_test-0.39.tar.gz/blincus_test-0.39/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blincus-test",
+    version="0.39",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1"
+    ],
+    author="Quixines",
+    author_email="nathan@quixines.com",
+    description="A Blincus payment processing and messaging package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nathan2002-hash/test-blincus-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
