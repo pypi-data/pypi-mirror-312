63a0bbc1aa44d94bce31a315ec2ea6ee0529f634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:55 +0000

    Add package dummy-data-generator-id/0.1.3/dummy-data-generator-id-0.1.3.tar.gz

---
+++ packages/dummy-data-generator-id/dummy-data-generator-id-0.1.3.tar.gz/dummy-data-generator-id-0.1.3/dummy_data_generator_id/__init__.py
@@ -0,0 +1,9 @@
+"""
+Dummy Data Generator Id Library
+A comprehensive library for generating realistic dummy data for Indonesian context.
+"""
+
+from .generator import DummyDataGeneratorId
+
+__version__ = "0.1.3"
+__all__ = ['DummyDataGeneratorId']
