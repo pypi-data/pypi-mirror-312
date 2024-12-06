d73644f1c67eddf6ea9d54e053b5d66fe86819bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:08 +0000

    Add package invenio-damap/0.1.2/invenio_damap-0.1.2-py2.py3-none-any.whl

---
+++ packages/invenio-damap/invenio_damap-0.1.2-py2.py3-none-any.whl/invenio_damap/services/errors.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2023-2024 TU Wien.
+#
+# Invenio-DAMAP is free software; you can redistribute it and/or modify
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Errors for InvenioDAMAP."""
+
+from invenio_i18n import gettext as _
+
+
+class InvenioDAMAPError(Exception):
+    """Base class for InvenioDAMAP errors."""
+
+    def __init__(self, *args: object):
+        """Constructor."""
+        super().__init__(*args)
