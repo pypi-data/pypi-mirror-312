4a3fc1b174ec8592deff4a15a0e79f4d621f1b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:33 +0000

    Add package crimsonslate_portfolio/1.0.0/crimsonslate_portfolio-1.0.0.tar.gz

---
+++ packages/crimsonslate_portfolio/crimsonslate_portfolio-1.0.0.tar.gz/crimsonslate_portfolio-1.0.0/crimsonslate_portfolio/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CrimsonslatePortfolioConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "crimsonslate_portfolio"
+    label = "portfolio"
