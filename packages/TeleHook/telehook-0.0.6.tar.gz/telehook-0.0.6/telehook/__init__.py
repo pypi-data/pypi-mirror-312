7604a3c02bfd66cfdfd78277b62149e1ff3629fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:10 +0000

    Add package TeleHook/0.0.6/telehook-0.0.6.tar.gz

---
+++ packages/TeleHook/telehook-0.0.6.tar.gz/telehook-0.0.6/telehook/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.6"
+
+#from telehook.main import TeleClient, Filters
+from telehook.testclient import testclient
+ 
+__all__ = [
+    #"TeleClient",
+    "testclient"
+    #"Filters"
+]
