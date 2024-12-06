93dbe1085b9cfa8b9167048dbd62287d9e5d9814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:09 +0000

    Add package unxt/1.0.0/unxt-1.0.0-py3-none-any.whl

---
+++ packages/unxt/unxt-1.0.0-py3-none-any.whl/unxt/_src/dimensions/__init__.py
@@ -0,0 +1,9 @@
+"""Dimensions module.
+
+This is the private implementation of the dimensions module.
+
+"""
+
+__all__ = ["dimension", "dimension_of", "AbstractDimension"]
+
+from .core import AbstractDimension, dimension, dimension_of
---
+++ packages/unxt/unxt-1.0.0-py3-none-any.whl/unxt/_src/units/__init__.py
@@ -0,0 +1,8 @@
+"""Units module.
+
+Copyright (c) 2023 Galactic Dynamics. All rights reserved.
+"""
+
+__all__ = ["unit", "unit_of"]
+
+from .core import unit, unit_of
