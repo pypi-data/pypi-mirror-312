4b38525aa38060596aa09e5094128103b47b4b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:21 +0000

    Add package long-s/1.0.33/long_s-1.0.33-py3-none-any.whl

---
+++ packages/long-s/long_s-1.0.33-py3-none-any.whl/long_s/__init__.py
@@ -0,0 +1,22 @@
+"""
+Filename: __init__.py
+Description: long-s is a tool that takes modern text and 
+             inserts the archaic letter of the long S (ſ) where it fits.
+
+Author: TravisGK
+Version: 1.0.2
+
+License: MIT License
+"""
+
+
+from ._convert import (
+    #using_developer_mode,
+    #enable_developer_mode,
+    #enable_debug_text,
+    #get_conversion_func,
+    convert_text_file,
+    convert_docx_file,
+    convert_odf_file,
+    convert,
+)
