8178788d067cf8a011d8d9b1ad6bb8146c5114d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:22 +0000

    Add package brats/0.0.10/brats-0.0.10-py3-none-any.whl

---
+++ packages/brats/brats-0.0.10-py3-none-any.whl/brats/__init__.py
@@ -0,0 +1,8 @@
+from brats.core.inpainting_algorithms import Inpainter
+from brats.core.segmentation_algorithms import (
+    AdultGliomaSegmenter,
+    AfricaSegmenter,
+    MeningiomaSegmenter,
+    MetastasesSegmenter,
+    PediatricSegmenter,
+)
