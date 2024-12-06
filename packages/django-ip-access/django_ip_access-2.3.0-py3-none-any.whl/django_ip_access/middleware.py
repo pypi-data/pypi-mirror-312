0fce19aef30fea5696128071725be994b6972ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:30:50 +0000

    Add package django-ip-access/2.3.0/django_ip_access-2.3.0-py3-none-any.whl

---
+++ packages/django-ip-access/django_ip_access-2.3.0-py3-none-any.whl/django_ip_access/middleware.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.core.cache import cache
+from ipware import get_client_ip
+
+
+class IpAccessMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user and request.user.is_authenticated:
+            return self.get_response(request)
+
+        ip, _ = get_client_ip(request)
+        cache_key = f"{settings.IP_ACCESS_CACHE_KEY_PREFIX}_{ip}"
+
+        if not cache.get(cache_key):
+            user = authenticate(request, ip=ip)
+            cache.set(cache_key, True, settings.IP_ACCESS_CACHE_TTL)
+
+            if user:
+                login(
+                    request, user, backend="django_ip_access.backends.IpAccessBackend"
+                )
+
+        return self.get_response(request)
