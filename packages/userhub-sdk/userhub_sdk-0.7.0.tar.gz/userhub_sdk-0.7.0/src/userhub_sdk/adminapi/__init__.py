972c1ab2bd2246b5c9abdae4f885fef2a312f78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:06 +0000

    Add package userhub-sdk/0.7.0/userhub_sdk-0.7.0.tar.gz

---
+++ packages/userhub-sdk/userhub_sdk-0.7.0.tar.gz/userhub_sdk-0.7.0/src/userhub_sdk/adminapi/__init__.py
@@ -0,0 +1,67 @@
+# Code generated. DO NOT EDIT.
+
+from typing import Optional
+
+from userhub_sdk import types
+from userhub_sdk._internal import constants
+from userhub_sdk._internal.http_transport import AsyncHttpTransport, HttpTransport
+
+from . import _client
+
+
+class AdminApi(_client.Client):
+    """
+    The client for the Admin API.
+
+    :param admin_key: The Admin API key (e.g. `sk_2vm8...`).
+    """
+
+    def __init__(
+        self,
+        admin_key: str,
+        *,
+        base_url: Optional[str] = None,
+    ):
+        if not base_url:
+            base_url = constants.API_BASE_URL
+
+        headers = {}
+
+        admin_key = (admin_key or "").strip()
+        if not admin_key:
+            raise types.UserHubError("admin_key required")
+
+        headers[constants.AUTH_HEADER] = f"Bearer {admin_key}"
+
+        transport = HttpTransport(base_url, headers)
+
+        super().__init__(transport)
+
+
+class AsyncAdminApi(_client.AsyncClient):
+    """
+    The async client for the Admin API.
+
+    :param admin_key: The Admin API key (e.g. `sk_2vm8...`).
+    """
+
+    def __init__(
+        self,
+        admin_key: str,
+        *,
+        base_url: Optional[str] = None,
+    ):
+        if not base_url:
+            base_url = constants.API_BASE_URL
+
+        headers = {}
+
+        admin_key = (admin_key or "").strip()
+        if not admin_key:
+            raise types.UserHubError("admin_key required")
+
+        headers[constants.AUTH_HEADER] = f"Bearer {admin_key}"
+
+        transport = AsyncHttpTransport(base_url, headers)
+
+        super().__init__(transport)
---
+++ packages/userhub-sdk/userhub_sdk-0.7.0.tar.gz/userhub_sdk-0.7.0/src/userhub_sdk/operationsv1/__init__.py
@@ -0,0 +1,9 @@
+# Code generated. DO NOT EDIT.
+
+from ._operation import Operation
+from ._operation_error import OperationError
+
+__all__ = [
+    "Operation",
+    "OperationError",
+]
