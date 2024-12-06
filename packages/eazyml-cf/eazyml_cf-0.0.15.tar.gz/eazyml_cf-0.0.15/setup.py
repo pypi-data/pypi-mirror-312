982b158b99db187dcff6c79562dea7a16caa27cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:52 +0000

    Add package eazyml-cf/0.0.15/eazyml_cf-0.0.15.tar.gz

---
+++ packages/eazyml-cf/eazyml_cf-0.0.15.tar.gz/eazyml_cf-0.0.15/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.15'
+DESCRIPTION = 'eazyml cf api'
+LONG_DESCRIPTION = 'eazyml cf api'
+
+# Setting up
+setup(
+    name="eazyml-cf",
+    version=VERSION,
+    author="Eazyml",
+    author_email="admin@ipsoftlabs.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    package_dir={"eazyml_cf":"./eazyml_cf"},
+    package_data={'' : ['*.py', '*.so', '*.dylib', '*.pyd']},
+    install_requires=['pandas',
+                      'matplotlib',
+                      'openpyxl',
+                      'scikit-learn',
+                      'scipy'
+                      ],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
