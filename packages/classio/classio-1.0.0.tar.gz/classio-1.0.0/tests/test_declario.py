41dfc0596b33dcacaccc65d9105f7e900de0e81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:53 +0000

    Add package classio/1.0.0/classio-1.0.0.tar.gz

---
+++ packages/classio/classio-1.0.0.tar.gz/classio-1.0.0/tests/test_declario.py
@@ -0,0 +1,28 @@
+"""Testing the classio.declario class decorator."""
+
+from dataclasses import dataclass
+import pandas as pd
+import dummio
+import dummio.pandas.df_parquet
+from classio import declario
+from pathlib import Path
+
+
+def test_declario(tmp_path: Path):
+    @declario(
+        io_modules={
+            "config": dummio.json,
+            "df": dummio.pandas.df_parquet,
+        }
+    )
+    @dataclass
+    class MyData:
+        config: dict[str, str]
+        df: pd.DataFrame
+
+    data = MyData(config={"a": "1"}, df=pd.DataFrame({"a": [1, 2, 3]}))
+    filepath = tmp_path / "data"
+    data.save(filepath=filepath)
+    data2 = MyData.load(filepath)
+    assert data2.config == data.config
+    assert data2.df.equals(data.df)
