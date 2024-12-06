81f314cc522dae78d76b2b3e352cc1e60e16741d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:58 +0000

    Add package reling/1.8.3/reling-1.8.3.tar.gz

---
+++ packages/reling/reling-1.8.3.tar.gz/reling-1.8.3/src/reling/types/__init__.py
@@ -0,0 +1,15 @@
+from .audio import Reader, Speed, Transcriber
+from .dialogue_exchange_data import DialogueExchangeData
+from .input import Input
+from .promise import Promise
+from .word_with_sense import WordWithSense
+
+__all__ = [
+    'DialogueExchangeData',
+    'Input',
+    'Promise',
+    'Reader',
+    'Speed',
+    'Transcriber',
+    'WordWithSense',
+]
