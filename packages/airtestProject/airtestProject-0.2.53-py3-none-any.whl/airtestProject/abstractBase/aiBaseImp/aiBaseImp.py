8a91560631e09a23d663082db8988c9023020772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:58 +0000

    Add package airtestProject/0.2.53/airtestProject-0.2.53-py3-none-any.whl

---
+++ packages/airtestProject/airtestProject-0.2.53-py3-none-any.whl/airtestProject/abstractBase/aiBaseImp/aiBaseImp.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from airtestProject.abstractBase.aiBase import aiBase
+"""
+ai操作类具体实现类
+"""
+
+
+class aiBaseImp(aiBase):
+
+    def aiClick(self, value, *args, **kwargs):
+        pass
