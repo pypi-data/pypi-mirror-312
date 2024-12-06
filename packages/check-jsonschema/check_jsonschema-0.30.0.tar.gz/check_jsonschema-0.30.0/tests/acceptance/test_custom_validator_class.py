c58c951e77979ce3acd5fb109951058cce076725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:27 +0000

    Add package check-jsonschema/0.30.0/check_jsonschema-0.30.0.tar.gz

---
+++ packages/check-jsonschema/check_jsonschema-0.30.0.tar.gz/check_jsonschema-0.30.0/tests/acceptance/test_custom_validator_class.py
@@ -0,0 +1,174 @@
+import json
+
+import pytest
+
+# define a calendar event schema and then use a custom validator to validate that there
+# are no events with "Occult" in their names
+SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "definitions": {
+        "calendar-event": {
+            "type": "object",
+            "properties": {
+                "title": {"type": "string"},
+                "start": {"type": "string", "format": "date-time"},
+                "end": {"type": "string", "format": "date-time"},
+            },
+        }
+    },
+    "properties": {
+        "events": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/calendar-event"},
+        },
+    },
+    "required": ["events"],
+}
+VALID_DOC = {
+    "events": [
+        {
+            "title": "Weekly Production Meeting",
+            "start": "2019-06-24T09:00:00-05:00",
+            "end": "2019-06-24T10:00:00-05:00",
+        },
+        {
+            "title": "Catch Up",
+            "start": "2019-06-24T10:00:00-05:00",
+            "end": "2019-06-24T10:30:00-05:00",
+        },
+    ]
+}
+INVALID_DOC = {
+    "events": [
+        {
+            "title": "Weekly Production Meeting",
+            "start": "2019-06-24T09:00:00-05:00",
+            "end": "2019-06-24T10:00:00-05:00",
+        },
+        {
+            "title": "Catch Up",
+            "start": "2019-06-24T10:00:00-05:00",
+            "end": "2019-06-24T10:30:00-05:00",
+        },
+        {
+            "title": "Occult Study Session",
+            "start": "2019-06-24T10:00:00-05:00",
+            "end": "2019-06-24T12:00:00-05:00",
+        },
+    ]
+}
+
+
+@pytest.fixture(autouse=True)
+def _foo_module(mock_module):
+    mock_module(
+        "foo.py",
+        """\
+import jsonschema
+
+class MyValidator:
+    def __init__(self, schema, *args, **kwargs):
+        self.schema = schema
+        self.real_validator = jsonschema.validators.Draft7Validator(
+            schema, *args, **kwargs
+        )
+
+    def iter_errors(self, data, *args, **kwargs):
+        yield from self.real_validator.iter_errors(data, *args, **kwargs)
+        for event in data["events"]:
+            if "Occult" in event["title"]:
+                yield jsonschema.exceptions.ValidationError(
+                    "Error! Occult event detected! Run!",
+                    validator=None,
+                    validator_value=None,
+                    instance=event,
+                    schema=self.schema,
+                )
+""",
+    )
+
+
+def test_custom_validator_class_can_detect_custom_conditions(run_line, tmp_path):
+    doc = tmp_path / "invalid.json"
+    doc.write_text(json.dumps(INVALID_DOC))
+
+    schema = tmp_path / "schema.json"
+    schema.write_text(json.dumps(SCHEMA))
+
+    result = run_line(
+        [
+            "check-jsonschema",
+            "--schemafile",
+            str(schema),
+            str(doc),
+        ]
+    )
+    assert result.exit_code == 0, result.stdout  # pass
+
+    result = run_line(
+        [
+            "check-jsonschema",
+            "--schemafile",
+            str(schema),
+            "--validator-class",
+            "foo:MyValidator",
+            str(doc),
+        ],
+    )
+    assert result.exit_code == 1  # fail
+    assert "Occult event detected" in result.stdout, result.stdout
+
+
+def test_custom_validator_class_can_pass_when_valid(run_line, tmp_path):
+    doc = tmp_path / "valid.json"
+    doc.write_text(json.dumps(VALID_DOC))
+
+    schema = tmp_path / "schema.json"
+    schema.write_text(json.dumps(SCHEMA))
+
+    result = run_line(
+        [
+            "check-jsonschema",
+            "--schemafile",
+            str(schema),
+            str(doc),
+        ]
+    )
+    assert result.exit_code == 0, result.stdout  # pass
+
+    result = run_line(
+        [
+            "check-jsonschema",
+            "--schemafile",
+            str(schema),
+            "--validator-class",
+            "foo:MyValidator",
+            str(doc),
+        ],
+    )
+    assert result.exit_code == 0  # pass
+
+
+@pytest.mark.parametrize(
+    "add_opts",
+    (
+        ["--builtin-schema", "vendor.github-workflows"],
+        ["--check-metaschema"],
+    ),
+)
+def test_custom_validator_class_is_incompatible_with_schema_opts(
+    run_line, tmp_path, add_opts
+):
+    doc = tmp_path / "instance.json"
+    doc.write_text("{}")
+    result = run_line(
+        [
+            "check-jsonschema",
+            "--validator-class",
+            "foo:MyValidator",
+            str(doc),
+        ]
+        + add_opts
+    )
+    assert result.exit_code == 2
+    assert "--validator-class can only be used with --schemafile" in result.stderr
---
+++ packages/check-jsonschema/check_jsonschema-0.30.0.tar.gz/check_jsonschema-0.30.0/tests/acceptance/test_format_regex_opts.py
@@ -0,0 +1,133 @@
+# test on a JavaScript regex which is not a valid python regex
+# `--format-regex=default` should accept it
+# `--format-regex=python` should reject it
+#
+# check these options against documents with invalid and valid python regexes to confirm
+# that they are behaving as expected
+import json
+
+import pytest
+
+FORMAT_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "properties": {"pattern": {"type": "string", "format": "regex"}},
+}
+
+ALWAYS_PASSING_DOCUMENT = {
+    "pattern": "ab*c",
+}
+
+ALWAYS_FAILING_DOCUMENT = {
+    "pattern": "a(b*c",
+}
+
+JS_REGEX_DOCUMENT = {
+    "pattern": "a(?<captured>)bc",
+}
+
+# taken from https://github.com/python-jsonschema/check-jsonschema/issues/25
+RENOVATE_DOCUMENT = {
+    "regexManagers": [
+        {
+            "fileMatch": ["^Dockerfile$"],
+            "matchStrings": ["ENV YARN_VERSION=(?<currentValue>.*?)\n"],
+            "depNameTemplate": "yarn",
+            "datasourceTemplate": "npm",
+        }
+    ]
+}
+
+
+@pytest.fixture(
+    params=[
+        ("--disable-formats", "regex"),
+        ("--format-regex", "default"),
+        ("--format-regex", "python"),
+    ]
+)
+def regexopts(request):
+    return request.param
+
+
+def test_regex_format_good(run_line_simple, tmp_path, regexopts):
+    schemafile = tmp_path / "schema.json"
+    schemafile.write_text(json.dumps(FORMAT_SCHEMA))
+
+    doc = tmp_path / "doc.json"
+    doc.write_text(json.dumps(ALWAYS_PASSING_DOCUMENT))
+
+    run_line_simple([*regexopts, "--schemafile", str(schemafile), str(doc)])
+
+
+def test_regex_format_accepts_non_str_inputs(run_line_simple, tmp_path, regexopts):
+    # potentially confusing, but a format checker is allowed to check non-str instances
+    # validate the format checker behavior on such a case
+    schemafile = tmp_path / "schema.json"
+    schemafile.write_text(
+        json.dumps(
+            {
+                "$schema": "http://json-schema.org/draft-07/schema",
+                "properties": {"pattern": {"type": "integer", "format": "regex"}},
+            }
+        )
+    )
+    doc = tmp_path / "doc.json"
+    doc.write_text(json.dumps({"pattern": 0}))
+    run_line_simple([*regexopts, "--schemafile", str(schemafile), str(doc)])
+
+
+def test_regex_format_bad(run_line, tmp_path, regexopts):
+    schemafile = tmp_path / "schema.json"
+    schemafile.write_text(json.dumps(FORMAT_SCHEMA))
+
+    doc = tmp_path / "doc.json"
+    doc.write_text(json.dumps(ALWAYS_FAILING_DOCUMENT))
+
+    expect_ok = regexopts == ("--disable-formats", "regex")
+
+    res = run_line(
+        [
+            "check-jsonschema",
+            *regexopts,
+            "--schemafile",
+            str(schemafile),
+            str(doc),
+        ],
+    )
+    if expect_ok:
+        assert res.exit_code == 0
+    else:
+        assert res.exit_code == 1
+        assert "is not a 'regex'" in res.stdout
+
+
+def test_regex_format_js_specific(run_line, tmp_path, regexopts):
+    schemafile = tmp_path / "schema.json"
+    schemafile.write_text(json.dumps(FORMAT_SCHEMA))
+
+    doc = tmp_path / "doc.json"
+    doc.write_text(json.dumps(JS_REGEX_DOCUMENT))
+
+    expect_ok = regexopts != ("--format-regex", "python")
+
+    res = run_line(
+        [
+            "check-jsonschema",
+            *regexopts,
+            "--schemafile",
+            str(schemafile),
+            str(doc),
+        ],
+    )
+    if expect_ok:
+        assert res.exit_code == 0
+    else:
+        assert res.exit_code == 1
+        assert "is not a 'regex'" in res.stdout
+
+
+def test_regex_format_in_renovate_config(run_line_simple, tmp_path):
+    doc = tmp_path / "doc.json"
+    doc.write_text(json.dumps(RENOVATE_DOCUMENT))
+
+    run_line_simple(["--builtin-schema", "vendor.renovate", str(doc)])
