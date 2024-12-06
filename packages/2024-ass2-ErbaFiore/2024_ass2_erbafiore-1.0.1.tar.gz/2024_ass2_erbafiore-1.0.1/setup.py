8bd48d52fa082bfda7e86ab41d5f768adc4d0203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:25 +0000

    Add package 2024-ass2-ErbaFiore/1.0.1/2024_ass2_erbafiore-1.0.1.tar.gz

---
+++ packages/2024-ass2-ErbaFiore/2024_ass2_erbafiore-1.0.1.tar.gz/2024_ass2_erbafiore-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='2024-ass2-ErbaFiore',
+    version='1.0.1',
+    setup_requires=["wheel"],
+    description='Assignment2 DevOps',
+    author='GruppoErbaFiore',
+    author_email='l.erba6@campus.unimib.it',
+    license = "MIT",
+    packages=find_packages(),
+    install_requires=[
+        'pymongo',
+        'pytest', 
+        'prospector',
+        'bandit',
+        'mkdocs',
+        'twine',
+    ],
+)
