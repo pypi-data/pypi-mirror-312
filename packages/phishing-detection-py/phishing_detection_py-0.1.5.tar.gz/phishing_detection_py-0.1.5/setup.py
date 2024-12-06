c9be939d7d719048296ad20e34d869908dba4cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:21 +0000

    Add package phishing-detection-py/0.1.5/phishing_detection_py-0.1.5.tar.gz

---
+++ packages/phishing-detection-py/phishing_detection_py-0.1.5.tar.gz/phishing_detection_py-0.1.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phishing-detection-py",
+    version="0.1.5",
+    description="A library for phishing detection using Hugging Face models.",
+    author="cmacha2",
+    author_email="cristiancmg127@gmail.com",
+    url="https://github.com/cmacha2/phishing-detection-py",
+    license="Apache 2.0",
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "imblearn",
+        "pyyaml",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points={
+        "console_scripts": [
+            "phishing-detection=phishing_detection.cli:main",
+        ],
+    },
+)
