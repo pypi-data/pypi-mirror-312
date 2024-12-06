5f5e5f4b5ea126488efe5021a2566fed46f5f668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:57 +0000

    Add package py-ad-principal/0.2.1/py_ad_principal-0.2.1.tar.gz

---
+++ packages/py-ad-principal/py_ad_principal-0.2.1.tar.gz/py_ad_principal-0.2.1/src/py_ad_principal/__init__.py
@@ -0,0 +1,16 @@
+from .py_ad_principal import (
+    AuthenticationContextConfig,
+    ActiveDirectoryError,
+    ActiveDirectoryPrincipal,
+    AuthenticationResult,
+    AuthenticationContext
+)
+
+__version__ = "0.2.1"
+__all__ = [
+    "AuthenticationContextConfig",
+    "ActiveDirectoryError",
+    "ActiveDirectoryPrincipal",
+    "AuthenticationResult",
+    "AuthenticationContext",
+]
