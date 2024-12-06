0bf8e056d9d419517954074054f7d581c771a521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:20 +0000

    Add package eazyml-augi/0.0.8/eazyml_augi-0.0.8.tar.gz

---
+++ packages/eazyml-augi/eazyml_augi-0.0.8.tar.gz/eazyml_augi-0.0.8/eazyml_augi/__init__.py
@@ -0,0 +1,4 @@
+from . import compiledeazyml
+compiledeazyml.libeazymlaugipython_cython_submodule()
+
+from .eazyml import ez_augi
---
+++ packages/eazyml-augi/eazyml_augi-0.0.8.tar.gz/eazyml_augi-0.0.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.8'
+DESCRIPTION = 'eazyml augi'
+LONG_DESCRIPTION = 'eazyml augi with linux and windows compatibility'
+
+# Setting up
+setup(
+    name="eazyml-augi",
+    version=VERSION,
+    author="Eazyml",
+    author_email="admin@ipsoftlabs.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    package_dir={"eazyml_augi":"./eazyml_augi"},
+    package_data={'' : ['*.py', '*.so', '*.dylib', '*.pyd']},
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+   ] 
+)
