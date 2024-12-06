9f2e0676cd331ac592777b2ca9115c8473738129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:37 +0000

    Add package tach/0.16.1/tach-0.16.1-cp312-cp312-win_amd64.whl

---
+++ packages/tach/tach-0.16.1-cp312-cp312-win_amd64.whl/tach/constants/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+PACKAGE_NAME = "tach"
+TOOL_NAME = "tach"
+CONFIG_FILE_NAME = TOOL_NAME
+PACKAGE_FILE_NAME = "package"
+ROOT_MODULE_SENTINEL_TAG = "<root>"
+
+DEFAULT_EXCLUDE_PATHS = ["tests", "docs", ".*__pycache__", ".*egg-info"]
+
+__all__ = [
+    "PACKAGE_NAME",
+    "TOOL_NAME",
+    "CONFIG_FILE_NAME",
+    "PACKAGE_FILE_NAME",
+    "ROOT_MODULE_SENTINEL_TAG",
+    "DEFAULT_EXCLUDE_PATHS",
+]
