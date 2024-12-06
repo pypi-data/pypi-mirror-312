ec934651a97677adbab24375c2944099e1230581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:19 +0000

    Add package optimex/0.0.1/optimex-0.0.1-py3-none-any.whl

---
+++ packages/optimex/optimex-0.0.1-py3-none-any.whl/optimex/__init__.py
@@ -0,0 +1,11 @@
+"""optimex."""
+
+__all__ = (
+    "__version__",
+    
+    # Add functions and variables you want exposed in `optimex.` namespace here
+)
+
+__version__ = "0.0.1"
+
+from . import test
