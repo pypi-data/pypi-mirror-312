fb28cb4cd9e13d99c50a862a68b3d2fa90adb8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:07:21 +0000

    Add package TANVIP/0.2/tanvip-0.2.tar.gz

---
+++ packages/TANVIP/tanvip-0.2.tar.gz/tanvip-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    version='0.2',
+    name='TANVIP',
+    author='TANVIP',
+    description='TAN ENCODE',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3'
+
+    ]
+)
+
+
