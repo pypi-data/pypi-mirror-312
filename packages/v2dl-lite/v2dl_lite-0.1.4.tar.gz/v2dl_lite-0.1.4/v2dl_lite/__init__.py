279e8ff932e7728e33298325b765febb8c5ca0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:35 +0000

    Add package v2dl-lite/0.1.4/v2dl_lite-0.1.4.tar.gz

---
+++ packages/v2dl-lite/v2dl_lite-0.1.4.tar.gz/v2dl_lite-0.1.4/v2dl_lite/__init__.py
@@ -0,0 +1,6 @@
+import sys
+
+if sys.version_info < (3, 10):
+    raise ImportError(
+        "You are using an unsupported version of Python. Only Python versions 3.10 and above are supported by v2dl-lite",
+    )
