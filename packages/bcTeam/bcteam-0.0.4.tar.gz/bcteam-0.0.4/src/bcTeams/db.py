c002afe9fc8a97d5215dc5ba018cc6a71018e5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:25 +0000

    Add package bcTeam/0.0.4/bcteam-0.0.4.tar.gz

---
+++ packages/bcTeam/bcteam-0.0.4.tar.gz/bcteam-0.0.4/src/bcTeams/db.py
@@ -0,0 +1,17 @@
+from abcddbbc.dcb import DatasBases
+check = DatasBases()
+
+class DataBase:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        print(check.set_uspass(username, password))
+    def get_code(self):
+    	print(check.get_code[username])
+    def set_code(self, code):
+        try:
+        	self.code = code
+        	check.set_code(code, username)
+        	print("You have successfully registered your code.\n")
+        except:
+        	print("Error ...")
