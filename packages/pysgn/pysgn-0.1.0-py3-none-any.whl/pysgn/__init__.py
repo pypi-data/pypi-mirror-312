316eee29d848eb2951df6b3932a95f41376cac4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:42 +0000

    Add package pysgn/0.1.0/pysgn-0.1.0-py3-none-any.whl

---
+++ packages/pysgn/pysgn-0.1.0-py3-none-any.whl/pysgn/__init__.py
@@ -0,0 +1,17 @@
+"""Python package for constructing synthetic geospatial networks."""
+
+import datetime
+
+from pysgn.geo_erdos_renyi_network import geo_erdos_renyi_network
+from pysgn.geo_watts_strogatz_network import geo_watts_strogatz_network
+
+__all__ = [
+    "geo_erdos_renyi_network",
+    "geo_watts_strogatz_network",
+]
+
+__title__ = "pysgn"
+__version__ = "0.1.0"
+__license__ = "MIT"
+_this_year = datetime.datetime.now(tz=datetime.timezone.utc).date().year
+__copyright__ = f"Copyright {_this_year} Wang Boyu"
