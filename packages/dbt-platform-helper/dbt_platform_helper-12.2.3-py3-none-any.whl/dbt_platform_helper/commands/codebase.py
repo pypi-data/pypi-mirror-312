e39467780036a22cf1e725e8ffcf85bdbc53d134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:33 +0000

    Add package dbt-platform-helper/12.2.3/dbt_platform_helper-12.2.3-py3-none-any.whl

---
+++ packages/dbt-platform-helper/dbt_platform_helper-12.2.3-py3-none-any.whl/dbt_platform_helper/commands/codebase.py
@@ -0,0 +1,146 @@
+import json
+import os
+
+import click
+
+from dbt_platform_helper.domain.codebase import Codebase
+from dbt_platform_helper.exceptions import ApplicationDeploymentNotTriggered
+from dbt_platform_helper.exceptions import ApplicationEnvironmentNotFoundError
+from dbt_platform_helper.exceptions import ApplicationNotFoundError
+from dbt_platform_helper.exceptions import CopilotCodebaseNotFoundError
+from dbt_platform_helper.exceptions import ImageNotFoundError
+from dbt_platform_helper.exceptions import NoCopilotCodebasesFoundError
+from dbt_platform_helper.exceptions import NotInCodeBaseRepositoryError
+from dbt_platform_helper.utils.click import ClickDocOptGroup
+from dbt_platform_helper.utils.git import CommitNotFoundError
+from dbt_platform_helper.utils.versioning import (
+    check_platform_helper_version_needs_update,
+)
+
+
+@click.group(chain=True, cls=ClickDocOptGroup)
+def codebase():
+    """Codebase commands."""
+    check_platform_helper_version_needs_update()
+
+
+@codebase.command()
+def prepare():
+    """Sets up an application codebase for use within a DBT platform project."""
+    try:
+        Codebase().prepare()
+    except NotInCodeBaseRepositoryError:
+        # TODO: Set exception message in the exceptions and just output the message in the command code
+        click.secho(
+            "You are in the deploy repository; make sure you are in the application codebase repository.",
+            fg="red",
+        )
+        raise click.Abort
+
+
+@codebase.command()
+@click.option("--app", help="AWS application name", required=True)
+@click.option(
+    "--with-images",
+    help="List up to the last 10 images tagged for this codebase",
+    default=False,
+    is_flag=True,
+)
+def list(app, with_images):
+    """List available codebases for the application."""
+    try:
+        Codebase().list(app, with_images)
+    except NoCopilotCodebasesFoundError:
+        click.secho(
+            f"""No codebases found for application "{app}""",
+            fg="red",
+        )
+        raise click.Abort
+    except ApplicationNotFoundError:
+        click.secho(
+            f"""The account "{os.environ.get("AWS_PROFILE")}" does not contain the application "{app}"; ensure you have set the environment variable "AWS_PROFILE" correctly.""",
+            fg="red",
+        )
+        raise click.Abort
+
+
+@codebase.command()
+@click.option("--app", help="AWS application name", required=True)
+@click.option(
+    "--codebase",
+    help="The codebase name as specified in the platform-config.yml file",
+    required=True,
+)
+@click.option("--commit", help="GitHub commit hash", required=True)
+def build(app, codebase, commit):
+    """Trigger a CodePipeline pipeline based build."""
+    try:
+        Codebase().build(app, codebase, commit)
+    except ApplicationNotFoundError:
+        click.secho(
+            f"""The account "{os.environ.get("AWS_PROFILE")}" does not contain the application "{app}"; ensure you have set the environment variable "AWS_PROFILE" correctly.""",
+            fg="red",
+        )
+        raise click.Abort
+    except CommitNotFoundError:
+        click.secho(
+            f'The commit hash "{commit}" either does not exist or you need to run `git fetch`.',
+            fg="red",
+        )
+        raise click.Abort
+    except ApplicationDeploymentNotTriggered:
+        click.secho(
+            f"Your build for {codebase} was not triggered.",
+            fg="red",
+        )
+        raise click.Abort
+
+
+@codebase.command()
+@click.option("--app", help="AWS application name", required=True)
+@click.option("--env", help="AWS Copilot environment", required=True)
+@click.option(
+    "--codebase",
+    help="The codebase name as specified in the platform-config.yml file",
+    required=True,
+)
+@click.option("--commit", help="GitHub commit hash", required=True)
+def deploy(app, env, codebase, commit):
+    try:
+        Codebase().deploy(app, env, codebase, commit)
+    except ApplicationNotFoundError:
+        # TODO: Set exception message in the exceptions and just output the message in the command code
+        click.secho(
+            f"""The account "{os.environ.get("AWS_PROFILE")}" does not contain the application "{app}"; ensure you have set the environment variable "AWS_PROFILE" correctly.""",
+            fg="red",
+        )
+        raise click.Abort
+    except ApplicationEnvironmentNotFoundError:
+        click.secho(
+            f"""The environment "{env}" either does not exist or has not been deployed.""",
+            fg="red",
+        )
+        raise click.Abort
+    except (
+        CopilotCodebaseNotFoundError,
+        # TODO: Catch this error earlier and throw a more meaningful error, maybe it's CopilotCodebaseNotFoundError?
+        json.JSONDecodeError,
+    ):
+        click.secho(
+            f"""The codebase "{codebase}" either does not exist or has not been deployed.""",
+            fg="red",
+        )
+        raise click.Abort
+    except ImageNotFoundError:
+        click.secho(
+            f'The commit hash "{commit}" has not been built into an image, try the '
+            "`platform-helper codebase build` command first.",
+            fg="red",
+        )
+        raise click.Abort
+    except ApplicationDeploymentNotTriggered:
+        click.secho(
+            f"Your deployment for {codebase} was not triggered.",
+            fg="red",
+        )
+        raise click.Abort
---
+++ packages/dbt-platform-helper/dbt_platform_helper-12.2.3-py3-none-any.whl/dbt_platform_helper/commands/conduit.py
@@ -0,0 +1,78 @@
+import click
+
+from dbt_platform_helper.constants import CONDUIT_ADDON_TYPES
+from dbt_platform_helper.domain.conduit import Conduit
+from dbt_platform_helper.exceptions import AddonNotFoundError
+from dbt_platform_helper.exceptions import AddonTypeMissingFromConfigError
+from dbt_platform_helper.exceptions import CreateTaskTimeoutError
+from dbt_platform_helper.exceptions import InvalidAddonTypeError
+from dbt_platform_helper.exceptions import NoClusterError
+from dbt_platform_helper.exceptions import ParameterNotFoundError
+from dbt_platform_helper.providers.secrets import SecretNotFoundError
+from dbt_platform_helper.utils.application import load_application
+from dbt_platform_helper.utils.click import ClickDocOptCommand
+from dbt_platform_helper.utils.versioning import (
+    check_platform_helper_version_needs_update,
+)
+
+CONDUIT_ACCESS_OPTIONS = ["read", "write", "admin"]
+
+
+@click.command(cls=ClickDocOptCommand)
+@click.argument("addon_name", type=str, required=True)
+@click.option("--app", help="Application name", required=True)
+@click.option("--env", help="Environment name", required=True)
+@click.option(
+    "--access",
+    default="read",
+    type=click.Choice(CONDUIT_ACCESS_OPTIONS),
+    help="Allow read, write or admin access to the database addons.",
+)
+def conduit(addon_name: str, app: str, env: str, access: str):
+    """Opens a shell for a given addon_name create a conduit connection to
+    interact with postgres, opensearch or redis."""
+    check_platform_helper_version_needs_update()
+    application = load_application(app)
+
+    try:
+        Conduit(application).start(env, addon_name, access)
+    except NoClusterError:
+        # TODO: Set exception message in the exceptions and just output the message in the command code, should be able to catch all errors in one block
+        click.secho(f"""No ECS cluster found for "{app}" in "{env}" environment.""", fg="red")
+        exit(1)
+    except SecretNotFoundError as err:
+        click.secho(
+            f"""No secret called "{err}" for "{app}" in "{env}" environment.""",
+            fg="red",
+        )
+        exit(1)
+    except CreateTaskTimeoutError:
+        click.secho(
+            f"""Client ({addon_name}) ECS task has failed to start for "{app}" in "{env}" environment.""",
+            fg="red",
+        )
+        exit(1)
+    except ParameterNotFoundError:
+        click.secho(
+            f"""No parameter called "/copilot/applications/{app}/environments/{env}/addons". Try deploying the "{app}" "{env}" environment.""",
+            fg="red",
+        )
+        exit(1)
+    except AddonNotFoundError:
+        click.secho(
+            f"""Addon "{addon_name}" does not exist.""",
+            fg="red",
+        )
+        exit(1)
+    except InvalidAddonTypeError as err:
+        click.secho(
+            f"""Addon type "{err.addon_type}" is not supported, we support: {", ".join(CONDUIT_ADDON_TYPES)}.""",
+            fg="red",
+        )
+        exit(1)
+    except AddonTypeMissingFromConfigError:
+        click.secho(
+            f"""The configuration for the addon {addon_name}, is missconfigured and missing the addon type.""",
+            fg="red",
+        )
+        exit(1)
