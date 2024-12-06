4e9ad180b66dece547cd8b978395b88ddf86c915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:40 +0000

    Add package rag-kmk/0.0.40/rag_kmk-0.0.40.tar.gz

---
+++ packages/rag-kmk/rag_kmk-0.0.40.tar.gz/rag_kmk-0.0.40/run.py
@@ -0,0 +1,33 @@
+from  rag_kmk import CONFIG
+from rag_kmk.knowledge_base import build_knowledge_base   
+from rag_kmk.vector_db import summarize_collection, retrieve_chunks, show_results
+from rag_kmk.chat_flow import generateAnswer, generate_LLM_answer, RAG_LLM, run_rag_pipeline  
+
+
+def main():
+       
+    '''
+    Load the documents from the sample_documents folder and print the first 200 characters of each document.        
+
+    '''
+    
+
+    # Load the documents
+    knowledge_base= build_knowledge_base(r'.\tests\sample_documents') 
+    print("-----------------"*4)
+    print(CONFIG)    
+
+    print("-----------------"*4)  
+    # Summarize the collection
+    if knowledge_base:
+        summarize_collection(knowledge_base)
+        run_rag_pipeline(RAG_LLM,knowledge_base)
+    else:
+        print("No documents loaded.")
+    print("-----------------"*4)
+    
+
+
+
+if __name__ == "__main__":
+    main()
