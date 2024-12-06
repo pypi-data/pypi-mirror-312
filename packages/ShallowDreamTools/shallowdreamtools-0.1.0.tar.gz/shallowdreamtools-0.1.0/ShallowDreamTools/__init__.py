7167537ce05ae9cf522650912f1a84ab4045af8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:12 +0000

    Add package ShallowDreamTools/0.1.0/shallowdreamtools-0.1.0.tar.gz

---
+++ packages/ShallowDreamTools/shallowdreamtools-0.1.0.tar.gz/shallowdreamtools-0.1.0/ShallowDreamTools/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from ShallowDreamTools.my_head import *
+from ShallowDreamTools.my_lambda import *
+from ShallowDreamTools.my_function import *
+name = "ShallowDreamTools - Congratulations!"
