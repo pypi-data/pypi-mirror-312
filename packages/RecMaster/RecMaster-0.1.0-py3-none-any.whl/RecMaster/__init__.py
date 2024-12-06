e3c755456282bf6899ae1843c64ce6b2f1295cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:23 +0000

    Add package RecMaster/0.1.0/RecMaster-0.1.0-py3-none-any.whl

---
+++ packages/RecMaster/RecMaster-0.1.0-py3-none-any.whl/RecMaster/__init__.py
@@ -0,0 +1,6 @@
+from .videoRecorder import RecorderUI, ScreenRecorder
+from .audio_recorder import AudioRecorderManager
+
+__version__ = "0.1.0"
+
+__all__ = ['RecorderUI', 'ScreenRecorder', 'AudioRecorderManager'] 
