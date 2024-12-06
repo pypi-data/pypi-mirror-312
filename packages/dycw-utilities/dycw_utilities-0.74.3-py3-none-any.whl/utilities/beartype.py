e6bf77a80f0781174d180658c1f2aea6bb14fa33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:03 +0000

    Add package dycw-utilities/0.74.3/dycw_utilities-0.74.3-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.74.3-py3-none-any.whl/utilities/beartype.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from asyncio import iscoroutinefunction
+from collections.abc import Callable
+from functools import partial, wraps
+from typing import Any, TypeVar, cast, overload
+
+from beartype import beartype
+
+_F = TypeVar("_F", bound=Callable[..., Any])
+
+
+@overload
+def beartype_cond(func: _F, /, *, enable: Callable[[], bool] | None = ...) -> _F: ...
+@overload
+def beartype_cond(
+    func: None = None, /, *, enable: Callable[[], bool] | None = ...
+) -> Callable[[_F], _F]: ...
+def beartype_cond(
+    func: _F | None = None, /, *, enable: Callable[[], bool] | None = None
+) -> _F | Callable[[_F], _F]:
+    """Apply `beartype` conditionally."""
+    if func is None:
+        result = partial(beartype_cond, enable=enable)
+        return cast(Callable[[_F], _F], result)
+
+    decorated = beartype(func)
+    if enable is None:
+        return decorated
+
+    if not iscoroutinefunction(func):
+
+        @wraps(func)
+        def beartype_sync(*args: Any, **kwargs: Any) -> Any:
+            if enable():
+                return decorated(*args, **kwargs)
+            return func(*args, **kwargs)
+
+        return cast(_F, beartype_sync)
+
+    @wraps(func)
+    async def beartype_async(*args: Any, **kwargs: Any) -> Any:
+        if enable():
+            return await decorated(*args, **kwargs)
+        return await func(*args, **kwargs)
+
+    return cast(_F, beartype_async)
+
+
+__all__ = ["beartype_cond"]
