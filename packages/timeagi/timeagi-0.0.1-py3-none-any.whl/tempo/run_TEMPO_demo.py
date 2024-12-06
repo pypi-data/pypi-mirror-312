05b04f4f87f228da5d48a8deae964b63e6a3fbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:10 +0000

    Add package timeagi/0.0.1/timeagi-0.0.1-py3-none-any.whl

---
+++ packages/timeagi/timeagi-0.0.1-py3-none-any.whl/tempo/run_TEMPO_demo.py
@@ -0,0 +1,25 @@
+# Third-party library imports
+import numpy as np
+import torch
+from numpy.random import choice
+# Local imports
+from models.TEMPO import TEMPO
+from utils.tools import load_data_from_huggingface
+
+
+model = TEMPO.load_pretrained_model(
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
+        repo_id = "Melady/TEMPO",
+        filename = "TEMPO-80M_v1.pth",
+        cache_dir = "./checkpoints/TEMPO_checkpoints"  
+)
+     
+print("Successfully loaded the model")
+
+input_data = np.random.rand(336)    # Random input data
+with torch.no_grad():
+        predicted_values = model.predict(input_data, pred_length=96)
+print("Predicted values:")
+print(predicted_values)
+
+############################################################################################################
