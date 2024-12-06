f40c8eb491837084f7c71a9981d33ad7920fbb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:20 +0000

    Add package ShallowDreamTools/0.1.4/ShallowDreamTools-0.1.4-py3-none-any.whl

---
+++ packages/ShallowDreamTools/ShallowDreamTools-0.1.4-py3-none-any.whl/ShallowDreamTools/my_head.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from pwn import *
+from LibcSearcher import LibcSearcher
+import tqdm
+from time import *
+from ctypes import *
+
+p = file_name = elf = libc_name = libc = libclib = ld_name = ld = ip = port = None
+def init(File=None,Libc=None,Ld=None,Url=None,arch="amd64",os='linux',log_level='debug'):
+    context(arch = arch,os=os,log_level=log_level)
+    global p,file_name,elf,libc_name,libc,libclib,ld_name,ld,ip,port
+
+    file_name = File; libc_name = Libc; ld_name = Ld
+    ip,port = None if not Url else (Url[:Url.find(':')].strip(), Url[Url.find(':') + 1:].strip())
+
+    if file_name: elf = ELF(file_name)
+    if libc_name: libc = ELF(libc_name)
+    if libc_name: libclib = cdll.LoadLibrary(libc_name)
+    if ld_name: ld = ELF(ld_name)
+
+def connect(local=0):
+    global p
+    if local: p = process(file_name)
+    else: p = remote(ip,port)
+
+def db():
+    gdb.attach(p)
