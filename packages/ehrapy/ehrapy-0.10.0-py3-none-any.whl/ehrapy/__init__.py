42b859b281c56f5b5eb881f16a90843eff15e069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:46 +0000

    Add package ehrapy/0.10.0/ehrapy-0.10.0-py3-none-any.whl

---
+++ packages/ehrapy/ehrapy-0.10.0-py3-none-any.whl/ehrapy/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for ehrapy."""
+
+__author__ = "Lukas Heumos"
+__email__ = "lukas.heumos@posteo.net"
+__version__ = "0.9.0"
+
+from ehrapy._settings import EhrapyConfig, ehrapy_settings
+
+settings: EhrapyConfig = ehrapy_settings
+
+from ehrapy import anndata as ad
+from ehrapy import data as dt
+from ehrapy import io
+from ehrapy import plot as pl
+from ehrapy import preprocessing as pp
+from ehrapy import tools as tl
+from ehrapy.core.meta_information import print_versions
