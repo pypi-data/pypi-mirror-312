a70c86e8b2ce41c6008bc1ad12e6cad926c831b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:27 +0000

    Add package karolis-s-mod1-atsiskaitymas/1.0.0/karolis_s_mod1_atsiskaitymas-1.0.0.tar.gz

---
+++ packages/karolis-s-mod1-atsiskaitymas/karolis_s_mod1_atsiskaitymas-1.0.0.tar.gz/karolis_s_mod1_atsiskaitymas-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "karolis_s_mod1_atsiskaitymas",
+    packages = find_packages(),
+    version = "1.0.0",
+    author = "Karolis",
+    author_email = "karolis.skruibis@codeacademylt.onmicrosoft.com",
+    install_requires = [
+        #install requests
+        #install lxml
+        #install csv
+        #install twine
+        #install setuptools
+        #install wheel
+    ]
+)
