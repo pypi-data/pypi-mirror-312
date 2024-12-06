77e177d114ac9a59dae71ac5077558d5e9bd6f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:47 +0000

    Add package NLPX/1.7.7/NLPX-1.7.7.tar.gz

---
+++ packages/NLPX/NLPX-1.7.7.tar.gz/NLPX-1.7.7/nlpx/utils.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+
+
+def subsequent_mask(size: int):
+	mask_shape = (1, size, size)
+	mask = np.triu(np.ones(mask_shape), k=1).astype(np.int8)
+	return torch.from_numpy(1 - mask)
+
+
+if __name__ == '__main__':
+	print(subsequent_mask(5))
