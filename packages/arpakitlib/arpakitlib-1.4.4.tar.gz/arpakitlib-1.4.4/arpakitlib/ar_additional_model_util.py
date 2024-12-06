60678723b68cfc3b6860df4f45dfc09f90292c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:29 +0000

    Add package arpakitlib/1.4.4/arpakitlib-1.4.4.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.4.4.tar.gz/arpakitlib-1.4.4/arpakitlib/ar_additional_model_util.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseAM(BaseModel):
+    model_config = ConfigDict(extra="forbid", arbitrary_types_allowed=True, from_attributes=True)
+    bus_data: dict[str, Any] = {}
