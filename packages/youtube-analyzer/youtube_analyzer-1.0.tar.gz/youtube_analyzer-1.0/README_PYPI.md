e257cf9a6d70b8b331f2f62e621a19a06c6bc8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:47 +0000

    Add package youtube-analyzer/1.0/youtube_analyzer-1.0.tar.gz

---
+++ packages/youtube-analyzer/youtube_analyzer-1.0.tar.gz/youtube_analyzer-1.0/README_PYPI.md
@@ -0,0 +1,38 @@
+<div align="center">
+<h1>youtube-analyzer<h1/>
+
+![Versão](https://img.shields.io/badge/version-1.0-orange)
+![Licença](https://img.shields.io/badge/license-MIT-orange)
+[![Sponsor](https://img.shields.io/badge/📖Documentation-green)](https://github.com/PauloCesar-dev404/youtube_analyzer/blob/main/Docs/document.md)
+<br><br>
+<i>Obtenha detalhes completos de vídeos do YouTube com esta biblioteca incrível!</i>
+<br>
+</div>
+
+### Funcionalidades
+- [x] Obter detalhes de um vídeo
+- [x] Obter legendas
+- [x] Tradução de legendas
+- [x] Analisar playlists
+- [x] Obter detalhes de URIs de vídeos
+- [x] Downloads de áudios e vídeos
+
+
+---
+
+### Contribuições
+Se tiver dúvidas ou sugestões, abra uma [issue aqui](https://github.com/PauloCesar-dev404/youtube_analyzer/issues).
+
+---
+
+### Apoie o Projeto com Pix!
+**Clique no código abaixo para copiá-lo automaticamente e ajudar a manter o desenvolvimento.**  
+```
+00020126580014br.gov.bcb.pix0136cb39b3a7-1a18-4d8d-9a33-996354ab93f55204000053039865802BR5925Paulo Cesar Ferro Fernand6009Sao Paulo62070503***6304E75F
+```
+
+---
+
+<i>youtube_analyzer! 🚀</i>
+
+
---
+++ packages/youtube-analyzer/youtube_analyzer-1.0.tar.gz/youtube_analyzer-1.0/setup.py
@@ -0,0 +1,40 @@
+from youtube_analyzer.__version__ import __version__, __autor__, __repo__, __source__, __lib__
+from setuptools import setup, find_packages
+
+# Lê o conteúdo do README.md
+with open('README_PYPI.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name="youtube-analyzer",
+    version=__version__,
+    description="Analize vídeos do youtube",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=__autor__,
+    author_email="paulocesar0073dev404@gmail.com",
+    license="MIT",
+    keywords=["youtube","youtube-analyzer","analizer youtube"],
+    install_requires=[
+        'requests',
+        'colorama',
+        'beautifulsoup4',
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms=["any"],
+    project_urls={
+        "Código Fonte": __source__,
+        "lib": __lib__,
+        'GitHub': __repo__,
+        "Bugs/Melhorias": f"{__repo__}/issues",
+        "Documentação": f"{__repo__}/wiki",
+    },
+
+    entry_points={
+        'console_scripts': [
+            'yt-downloader=youtube_analyzer.yt_downloader:main',
+        ],
+    },
+)
