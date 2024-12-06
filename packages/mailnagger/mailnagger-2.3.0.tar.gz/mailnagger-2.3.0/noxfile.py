cb1b7ac4f0b631369e474b5148d5f673cf335580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:28 +0000

    Add package mailnagger/2.3.0/mailnagger-2.3.0.tar.gz

---
+++ packages/mailnagger/mailnagger-2.3.0.tar.gz/mailnagger-2.3.0/noxfile.py
@@ -0,0 +1,120 @@
+# Copyright 2024 Timo Kankare <timo.kankare@iki.fi>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+
+import argparse
+import nox
+
+
+@nox.session(
+    name='tests-default',
+    venv_backend='venv',
+)
+def tests_default(session : nox.Session) -> None:
+    """Run unit tests with default python."""
+    tests(session)
+
+
+@nox.session(
+    python=['3.13', '3.12'],
+    venv_backend='venv',
+)
+def tests(session : nox.Session) -> None:
+    """Run unit tests"""
+    session.install('.')
+    session.install('pytest')
+    session.run('python', '-m', 'pytest')
+
+
+@nox.session
+def mypy(session : nox.Session) -> None:
+    """Run mypy type checker."""
+    session.install('mypy')
+    session.install(
+        "pygobject-stubs",
+        "--no-cache-dir",
+        env={"PYGOBJECT_STUB_CONFIG": "Gtk3"}
+    )
+    session.install('.[dev]')
+    session.run('python', '-m', 'mypy')
+
+
+@nox.session
+def flake8(session : nox.Session) -> None:
+    """Run flake8"""
+    session.install('flake8')
+    session.run('python', '-m', 'flake8', '--statistics')
+
+
+@nox.session(
+    default=False,
+)
+def build(session : nox.Session) -> None:
+    """Build sdist and wheel packages to dist."""
+    session.install("build")
+    session.run("python", "-m", "build")
+
+
+@nox.session(
+    default=False,
+)
+def dev(session : nox.Session) -> None:
+    """Set up a python development environment for the project at .env-dev."""
+
+    env = ".env-dev"
+
+    session.run("python", "-m", "venv", "--clear", env)
+
+    # Use the venv's interpreter to install the project along with
+    # all it's dev dependencies, this ensures it's installed in the right way
+    session.run(env + "/bin/python", "-m", "pip", "install", "-e", ".", external=True)
+    print("Activate environment: . " + env + "/bin/activate")
+
+
+@nox.session(
+    default=False,
+)
+def bumpversion(session : nox.Session) -> None:
+    """Bump version number"""
+    session.install("bump-my-version")
+
+    parser = argparse.ArgumentParser(
+        description="Bump version number,",
+        prog="nox -s bumpversion --",
+    )
+    parser.add_argument(
+        "part",
+        type=str,
+        nargs='?',
+        default=None,
+        help="The part of version to bump.",
+        choices=["major", "minor", "patch", "state", "count"],
+    )
+    args : argparse.Namespace = parser.parse_args(args=session.posargs)
+    if not args.part:
+        session.run("bump-my-version", "show-bump")
+    else:
+        part : str = args.part
+        session.run("bump-my-version", "bump", "-v", "--dry-run", part)
+        confirm = input(
+           f"You are about to bump the {part!r} version. Are you sure? [y/n]: "
+        )
+        if confirm.lower().strip() != "y":
+            session.error(f"You said no when prompted to bump the {part!r} version")
+        session.log(f"Bumping the {part!r} version")
+        session.run("bump-my-version", "bump", part)
+
---
+++ packages/mailnagger/mailnagger-2.3.0.tar.gz/mailnagger-2.3.0/setup.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+
+# To build Mailnagger run this script:
+# ./setup.py build
+# Then to install Mailnagger run pip as root:
+# pip install --break-system-packages .
+
+from setuptools import setup, find_packages
+from distutils.command.install_data import install_data
+from setuptools.command.build import build
+
+import logging
+import os
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+import sysconfig
+
+
+# NOTE: These should be in sync with Mailnag.common.dist_cfg PACKAGE_NAME
+#       and APP_VERSION.
+PACKAGE_NAME = 'mailnagger'
+APP_VERSION = '2.3.0'
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+logger = logging.getLogger(__name__)
+
+# TODO : This hack won't work with --user and --home options
+PREFIX = sysconfig.get_path('data')
+for arg in sys.argv:
+    if arg.startswith('--prefix='):
+        PREFIX = arg[9:]
+
+BUILD_DIR = 'build'
+for arg in sys.argv:
+    if arg.startswith('--build-base='):
+        BUILD_DIR = arg[13:]
+
+BUILD_LOCALE_DIR = os.path.join(BUILD_DIR, 'locale')
+BUILD_PATCH_DIR = os.path.join(BUILD_DIR, 'patched')
+INSTALL_LIB_DIR = os.path.join(sysconfig.get_path('purelib'), 'Mailnag')
+
+
+class BuildData(build):
+    def run(self):
+        # generate translations
+        try:
+            rc = subprocess.call('./gen_locales ' + BUILD_LOCALE_DIR, shell = True)
+            if (rc != 0):
+                if (rc == 1): err = "MKDIR_ERR"
+                elif (rc == 2): err = "MSGFMT_ERR"
+                else: err = "UNKNOWN_ERR"
+                raise Warning("gen_locales returned %d (%s)" % (rc, err))
+        except Exception as e:
+            logger.error("Building locales failed.")
+            logger.error("Error: %s" % str(e))
+            sys.exit(1)
+
+        # remove patch dir (if existing)
+        shutil.rmtree(BUILD_PATCH_DIR, ignore_errors = True)
+        Path(BUILD_PATCH_DIR).mkdir(parents=True, exist_ok=True)
+
+        # patch paths
+        self._patch_file(
+            './data/mailnagger.desktop',
+            os.path.join(BUILD_PATCH_DIR, 'mailnagger.desktop'),
+            '/usr',
+            PREFIX
+        )
+        self._patch_file(
+            './data/mailnagger-config.desktop',
+            os.path.join(BUILD_PATCH_DIR, 'mailnagger-config.desktop'),
+            '/usr',
+            PREFIX
+        )
+        build.run(self)
+
+
+    def _patch_file(self, infile, outfile, orig, replaced):
+        with open(infile, 'r') as f:
+            strn = f.read()
+            strn = strn.replace(orig, replaced)
+        with open(outfile, 'w') as f:
+            f.write(strn)
+
+
+class InstallData(install_data):
+    def run(self):
+        self._add_locale_data()
+        self._add_icon_data()
+        install_data.run(self)
+
+
+    def _add_locale_data(self):
+        for root, dirs, files in os.walk(BUILD_LOCALE_DIR):
+            for file in files:
+                src_path = os.path.join(root, file)
+                dst_path = os.path.join(
+                    'share/locale',
+                    os.path.dirname(src_path[len(BUILD_LOCALE_DIR)+1:])
+                )
+                self.data_files.append((dst_path, [src_path]))
+
+    def _add_icon_data(self):
+        for root, dirs, files in os.walk('data/icons'):
+            for file in files:
+                src_path = os.path.join(root, file)
+                dst_path = os.path.join(
+                    'share/icons',
+                    os.path.dirname(src_path[len('data/icons')+1:])
+                )
+                self.data_files.append((dst_path, [src_path]))
+
+
+setup(name=PACKAGE_NAME,
+    version=APP_VERSION,
+    description='An extensible mail notification daemon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Timo Kankare',
+    author_email='timo.kankare@iki.fi',
+    url='https://github.com/tikank/mailnagger',
+    license='GNU GPL2',
+    packages=find_packages(
+        include=[
+            "Mailnag*",
+            "mailnagger*"
+        ]
+    ) + ["Mailnag.plugins"],
+    package_data = {
+        'Mailnag.configuration.ui' : [
+            'account_widget.ui',
+            'config_window.ui',
+        ],
+        'Mailnag.configuration.desktop': [
+            'mailnagger.desktop',
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "mailnagger = mailnagger.daemon:main",
+            "mailnagger-config = mailnagger.config:main",
+        ],
+    },
+    data_files=[
+        ('share/mailnagger', ['data/mailnag.ogg']),
+        ('share/mailnagger', ['data/mailnag.png']),
+        ('share/man/man1', ['data/mailnagger.1', 'data/mailnagger-config.1']),
+        ('share/metainfo', ['data/mailnag.appdata.xml']),
+        ('share/applications', [
+                os.path.join(BUILD_PATCH_DIR, 'mailnagger.desktop'),
+                os.path.join(BUILD_PATCH_DIR, 'mailnagger-config.desktop')
+        ])
+    ],
+    include_package_data=True,
+    install_requires=[
+        'pygobject',
+        'pyxdg',
+        'dbus-python',
+    ],
+    extras_require={
+        "dev": [
+            "pygobject-stubs",
+            "types-pyxdg",
+            "pytest",
+            "nox",
+            "types-setuptools",
+        ],
+    },
+    cmdclass={
+        'build': BuildData,
+        'install_data': InstallData,
+    }
+)
