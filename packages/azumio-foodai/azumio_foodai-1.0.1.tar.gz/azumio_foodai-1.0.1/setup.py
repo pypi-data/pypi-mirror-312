6c98369264f6d4284191b5d0e937af7c21fa7f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:51 +0000

    Add package azumio-foodai/1.0.1/azumio_foodai-1.0.1.tar.gz

---
+++ packages/azumio-foodai/azumio_foodai-1.0.1.tar.gz/azumio_foodai-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# this grabs the requirements from requirements.txt
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setuptools.setup(
+    name='azumio_foodai',
+    version='1.0.0',
+    author='Igor Rendulic',
+    author_email='igor@azumio.com',
+    description='Azumio - Instant Food Recognition',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/azumio/foodai-clients/tree/master/foodai',
+    project_urls={
+        "Bug Tracker": "https://github.com/azumio/foodai-clients/issues"
+    },
+    license='Apache 2.0',
+    packages=['azumio_foodai'],
+    install_requires=REQUIREMENTS,
+)
