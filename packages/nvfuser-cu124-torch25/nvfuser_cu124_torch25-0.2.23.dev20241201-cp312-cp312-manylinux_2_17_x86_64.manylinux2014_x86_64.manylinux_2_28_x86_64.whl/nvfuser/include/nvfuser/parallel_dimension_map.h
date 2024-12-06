228202ee8e6197816c93edb54d0868d903f3eb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:44 +0000

    Add package nvfuser-cu124-torch25/0.2.23.dev20241201/nvfuser_cu124_torch25-0.2.23.dev20241201-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nvfuser-cu124-torch25/nvfuser_cu124_torch25-0.2.23.dev20241201-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/parallel_dimension_map.h
@@ -0,0 +1,64 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <exceptions.h>
+#include <ir/all_nodes.h>
+#include <kernel_ir.h>
+#include <visibility.h>
+
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace nvfuser {
+
+//! Maps TID/BID to its dimension.
+class ParallelDimensionMap {
+ public:
+  void build(Fusion* fusion);
+
+  //! Returns the dimension of a ParallelType. nullptr is returned if
+  //! a ParallelType is unused. If a dimension is not a constant, return
+  //! blockDim/gridDim instead.
+  NVF_API Val* get(ParallelType pt) const;
+
+  //! Returns the raw dimension of a ParallelType. nullptr is returned if
+  //! a ParallelType is unused.
+  Val* getRaw(ParallelType pt) const;
+
+  //! True if the dimension of a ParallelType is known to be exact
+  NVF_API bool isExact(ParallelType pt) const;
+
+  std::string toString() const;
+
+  const std::unordered_map<ParallelType, Val*>& getMap() const {
+    return dim_map_;
+  }
+
+  Val* getNumThreadsEachBlock() const;
+
+  bool has(ParallelType pt) const {
+    return dim_map_.count(pt) > 0;
+  }
+
+ private:
+  //! TIDx may need to be marked as non-exact as it may be padded to a
+  //! multiple of the warp size.
+  void adjustMappingsForWarpPadding();
+
+ private:
+  //! Maps from parallel types to dimensions, which are constant if
+  //! a unique value is found.
+  std::unordered_map<ParallelType, Val*> dim_map_;
+  //! Set of parallel types whose dimensions are identified to be
+  //! exactly the same as extents of mapped domains.
+  std::unordered_set<ParallelType> exact_types_;
+};
+
+} // namespace nvfuser
