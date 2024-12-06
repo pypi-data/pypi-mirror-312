7f1b2e1fc293e1051fe2e1351da07d196117f923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:07 +0000

    Add package cubed/0.19.0/cubed-0.19.0-py3-none-any.whl

---
+++ packages/cubed/cubed-0.19.0-py3-none-any.whl/cubed/array_api/searching_functions.py
@@ -0,0 +1,42 @@
+from cubed.array_api.data_type_functions import result_type
+from cubed.array_api.dtypes import _real_numeric_dtypes
+from cubed.array_api.manipulation_functions import reshape
+from cubed.backend_array_api import namespace as nxp
+from cubed.core.ops import arg_reduction, elemwise
+
+
+def argmax(x, /, *, axis=None, keepdims=False, split_every=None):
+    if x.dtype not in _real_numeric_dtypes:
+        raise TypeError("Only real numeric dtypes are allowed in argmax")
+    if axis is None:
+        x = reshape(x, (-1,))
+        axis = 0
+        keepdims = False
+    return arg_reduction(
+        x,
+        nxp.argmax,
+        axis=axis,
+        keepdims=keepdims,
+        split_every=split_every,
+    )
+
+
+def argmin(x, /, *, axis=None, keepdims=False, split_every=None):
+    if x.dtype not in _real_numeric_dtypes:
+        raise TypeError("Only real numeric dtypes are allowed in argmin")
+    if axis is None:
+        x = reshape(x, (-1,))
+        axis = 0
+        keepdims = False
+    return arg_reduction(
+        x,
+        nxp.argmin,
+        axis=axis,
+        keepdims=keepdims,
+        split_every=split_every,
+    )
+
+
+def where(condition, x1, x2, /):
+    dtype = result_type(x1, x2)
+    return elemwise(nxp.where, condition, x1, x2, dtype=dtype)
