2331527167002dc737a14c784b45fb45e8b2b053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:47 +0000

    Add package causara/0.1.0/causara-0.1.0.tar.gz

---
+++ packages/causara/causara-0.1.0.tar.gz/causara-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="causara",
+    version="0.1.0",
+    author="causara UG",
+    author_email="support@causara.com",
+    description="TODO",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://causara.com",
+    packages=find_packages(),
+    license="Proprietary",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9, <3.13",
+    install_requires=[
+        "numpy"
+    ],
+)
