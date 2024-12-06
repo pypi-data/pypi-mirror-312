ebf74455170d1cbc442772f389e5c11b1fea51ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:19 +0000

    Add package oarepo-requests/2.3.5/oarepo_requests-2.3.5.tar.gz

---
+++ packages/oarepo-requests/oarepo_requests-2.3.5.tar.gz/oarepo_requests-2.3.5/oarepo_requests/services/permissions/generators/__init__.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Permission generators."""
+
+from .active import RequestActive
+from .conditional import (
+    IfEventOnRequestType,
+    IfEventType,
+    IfNoEditDraft,
+    IfNoNewVersionDraft,
+    IfRequestedBy,
+    IfRequestType,
+    IfRequestTypeBase,
+)
+
+__all__ = (
+    "RequestActive",
+    "IfEventOnRequestType",
+    "IfRequestType",
+    "IfEventType",
+    "IfRequestedBy",
+    "IfRequestTypeBase",
+    "IfNoEditDraft",
+    "IfNoNewVersionDraft",
+)
