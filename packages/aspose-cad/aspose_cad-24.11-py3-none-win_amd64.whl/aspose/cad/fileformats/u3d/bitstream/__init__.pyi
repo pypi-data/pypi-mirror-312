491b3a8480059aaaf9fb194a68cc810212a6024b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:08 +0000

    Add package aspose-cad/24.11/aspose_cad-24.11-py3-none-win_amd64.whl

---
+++ packages/aspose-cad/aspose_cad-24.11-py3-none-win_amd64.whl/aspose/cad/fileformats/u3d/bitstream/__init__.pyi
@@ -0,0 +1,211 @@
+from typing import List, Optional, Dict, Iterable
+import io
+import aspose.pycore
+import aspose.pydrawing
+import aspose.cad
+import aspose.cad.annotations
+import aspose.cad.cadexceptions
+import aspose.cad.cadexceptions.compressors
+import aspose.cad.cadexceptions.imageformats
+import aspose.cad.exif
+import aspose.cad.exif.enums
+import aspose.cad.exporters
+import aspose.cad.exporters.cadapsentitiesexporter
+import aspose.cad.exporters.cadapsentitiesexporter.helper
+import aspose.cad.fileformats
+import aspose.cad.fileformats.bitmap
+import aspose.cad.fileformats.bmp
+import aspose.cad.fileformats.cad
+import aspose.cad.fileformats.cad.cadconsts
+import aspose.cad.fileformats.cad.cadobjects
+import aspose.cad.fileformats.cad.cadobjects.acadtable
+import aspose.cad.fileformats.cad.cadobjects.attentities
+import aspose.cad.fileformats.cad.cadobjects.background
+import aspose.cad.fileformats.cad.cadobjects.blocks
+import aspose.cad.fileformats.cad.cadobjects.datatable
+import aspose.cad.fileformats.cad.cadobjects.dictionary
+import aspose.cad.fileformats.cad.cadobjects.dimassoc
+import aspose.cad.fileformats.cad.cadobjects.field
+import aspose.cad.fileformats.cad.cadobjects.hatch
+import aspose.cad.fileformats.cad.cadobjects.helpers
+import aspose.cad.fileformats.cad.cadobjects.mlinestyleobject
+import aspose.cad.fileformats.cad.cadobjects.objectcontextdataclasses
+import aspose.cad.fileformats.cad.cadobjects.perssubentmanager
+import aspose.cad.fileformats.cad.cadobjects.polylines
+import aspose.cad.fileformats.cad.cadobjects.section
+import aspose.cad.fileformats.cad.cadobjects.sunstudy
+import aspose.cad.fileformats.cad.cadobjects.tablestyle
+import aspose.cad.fileformats.cad.cadobjects.underlaydefinition
+import aspose.cad.fileformats.cad.cadobjects.vertices
+import aspose.cad.fileformats.cad.cadobjects.wipeout
+import aspose.cad.fileformats.cad.cadparameters
+import aspose.cad.fileformats.cad.cadtables
+import aspose.cad.fileformats.cad.dwg
+import aspose.cad.fileformats.cad.dwg.acdbobjects
+import aspose.cad.fileformats.cad.dwg.appinfo
+import aspose.cad.fileformats.cad.dwg.r2004
+import aspose.cad.fileformats.cad.dwg.revhistory
+import aspose.cad.fileformats.cad.dwg.summaryinfo
+import aspose.cad.fileformats.cad.dwg.vbaproject
+import aspose.cad.fileformats.cad.watermarkguard
+import aspose.cad.fileformats.cf2
+import aspose.cad.fileformats.cgm
+import aspose.cad.fileformats.cgm.classes
+import aspose.cad.fileformats.cgm.commands
+import aspose.cad.fileformats.cgm.elements
+import aspose.cad.fileformats.cgm.enums
+import aspose.cad.fileformats.cgm.export
+import aspose.cad.fileformats.cgm.import
+import aspose.cad.fileformats.collada
+import aspose.cad.fileformats.collada.fileparser
+import aspose.cad.fileformats.collada.fileparser.elements
+import aspose.cad.fileformats.dgn
+import aspose.cad.fileformats.dgn.dgnelements
+import aspose.cad.fileformats.dgn.dgntransform
+import aspose.cad.fileformats.dicom
+import aspose.cad.fileformats.draco
+import aspose.cad.fileformats.dwf
+import aspose.cad.fileformats.dwf.dwfxps
+import aspose.cad.fileformats.dwf.dwfxps.fixedpage
+import aspose.cad.fileformats.dwf.dwfxps.fixedpage.dto
+import aspose.cad.fileformats.dwf.emodelinterface
+import aspose.cad.fileformats.dwf.eplotinterface
+import aspose.cad.fileformats.dwf.whip
+import aspose.cad.fileformats.dwf.whip.objects
+import aspose.cad.fileformats.dwf.whip.objects.drawable
+import aspose.cad.fileformats.dwf.whip.objects.drawable.text
+import aspose.cad.fileformats.dwf.whip.objects.service
+import aspose.cad.fileformats.dwf.whip.objects.service.font
+import aspose.cad.fileformats.fbx
+import aspose.cad.fileformats.glb
+import aspose.cad.fileformats.glb.animations
+import aspose.cad.fileformats.glb.geometry
+import aspose.cad.fileformats.glb.geometry.vertextypes
+import aspose.cad.fileformats.glb.io
+import aspose.cad.fileformats.glb.materials
+import aspose.cad.fileformats.glb.memory
+import aspose.cad.fileformats.glb.runtime
+import aspose.cad.fileformats.glb.scenes
+import aspose.cad.fileformats.glb.toolkit
+import aspose.cad.fileformats.glb.transforms
+import aspose.cad.fileformats.glb.validation
+import aspose.cad.fileformats.ifc
+import aspose.cad.fileformats.ifc.entities
+import aspose.cad.fileformats.ifc.entities.ifc4
+import aspose.cad.fileformats.ifc.entities.ifc4.entities
+import aspose.cad.fileformats.ifc.entities.ifc4x3
+import aspose.cad.fileformats.ifc.entities.ifc4x3.entities
+import aspose.cad.fileformats.ifc.header
+import aspose.cad.fileformats.ifc.ifc2x3
+import aspose.cad.fileformats.ifc.ifc2x3.entities
+import aspose.cad.fileformats.ifc.ifc2x3.types
+import aspose.cad.fileformats.ifc.ifc4
+import aspose.cad.fileformats.ifc.ifc4.entities
+import aspose.cad.fileformats.ifc.ifc4.types
+import aspose.cad.fileformats.ifc.ifc4x3
+import aspose.cad.fileformats.ifc.ifc4x3.entities
+import aspose.cad.fileformats.ifc.ifc4x3.types
+import aspose.cad.fileformats.iges
+import aspose.cad.fileformats.iges.commondefinitions
+import aspose.cad.fileformats.iges.drawables
+import aspose.cad.fileformats.jpeg
+import aspose.cad.fileformats.jpeg2000
+import aspose.cad.fileformats.obj
+import aspose.cad.fileformats.obj.elements
+import aspose.cad.fileformats.obj.mtl
+import aspose.cad.fileformats.obj.vertexdata
+import aspose.cad.fileformats.obj.vertexdata.index
+import aspose.cad.fileformats.pdf
+import aspose.cad.fileformats.plt
+import aspose.cad.fileformats.plt.pltparsers
+import aspose.cad.fileformats.plt.pltparsers.pltparser
+import aspose.cad.fileformats.plt.pltparsers.pltparser.pltplotitems
+import aspose.cad.fileformats.png
+import aspose.cad.fileformats.psd
+import aspose.cad.fileformats.psd.resources
+import aspose.cad.fileformats.shx
+import aspose.cad.fileformats.stl
+import aspose.cad.fileformats.stl.stlobjects
+import aspose.cad.fileformats.stp
+import aspose.cad.fileformats.stp.helpers
+import aspose.cad.fileformats.stp.items
+import aspose.cad.fileformats.stp.reader
+import aspose.cad.fileformats.stp.stplibrary
+import aspose.cad.fileformats.stp.stplibrary.core
+import aspose.cad.fileformats.stp.stplibrary.core.models
+import aspose.cad.fileformats.svg
+import aspose.cad.fileformats.threeds
+import aspose.cad.fileformats.threeds.elements
+import aspose.cad.fileformats.tiff
+import aspose.cad.fileformats.tiff.enums
+import aspose.cad.fileformats.tiff.filemanagement
+import aspose.cad.fileformats.tiff.instancefactory
+import aspose.cad.fileformats.tiff.tifftagtypes
+import aspose.cad.fileformats.u3d
+import aspose.cad.fileformats.u3d.bitstream
+import aspose.cad.fileformats.u3d.elements
+import aspose.cad.imageoptions
+import aspose.cad.imageoptions.svgoptionsparameters
+import aspose.cad.measurement
+import aspose.cad.palettehelper
+import aspose.cad.primitives
+import aspose.cad.sources
+import aspose.cad.timeprovision
+import aspose.cad.watermarkguard
+
+class IContextManager:
+    '''IContextManager.cs
+    
+    This file defines the IContextManager interface.
+    IContextManager is used to access the static and dynamic contexts used
+    for the reading and writing of compressed data.'''
+    
+    def add_symbol(self, context : int, symbol : int) -> None:
+        '''Add an occurance of the symbol to the specified
+        context.
+        
+        :param context: add the occurrence to this context's histogram
+        :param symbol: add an occurrence of this symbol to the histogram'''
+        ...
+    
+    def get_symbol_frequency(self, context : int, symbol : int) -> int:
+        '''Get the number of occurrences of the given symbol
+        in the context.
+        
+        :param context: get the frequency from this context's histogram
+        :param symbol: get the frequency of this symbol the symbol
+        :returns: the number of occurences of the specified symbol in the
+        specified context'''
+        ...
+    
+    def get_cumulative_symbol_frequency(self, context : int, symbol : int) -> int:
+        '''Get the total number of occurrences for all of the
+        symbols that are less than the given symbol in the context.
+        
+        :param context: use this context's histogram
+        :param symbol: use this symbol
+        :returns: sum of all symbol freqs for symbols less than the
+        given symbol in the given context'''
+        ...
+    
+    def get_total_symbol_frequency(self, context : int) -> int:
+        '''Get the total occurrences of all the symbols in this
+        context.
+        
+        :param context: use this context's
+        histogram
+        :returns: total occurances of all symbols for the given context'''
+        ...
+    
+    def get_symbol_from_frequency(self, context : int, symbol_frequency : int) -> int:
+        '''Find the symbol in a histogram that has
+        the cumulative frequency specified.
+        
+        :param context: use this context's histogram
+        :param symbol_frequency: use this frequency
+        :returns: the symbol that corresponds to the given cumulative frequency
+        and context'''
+        ...
+    
+    ...
+
