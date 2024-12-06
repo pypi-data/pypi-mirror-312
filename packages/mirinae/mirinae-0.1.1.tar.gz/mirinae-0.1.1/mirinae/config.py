e522e43f07571aebf3e8b1cdb7b81e827652b0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:59 +0000

    Add package mirinae/0.1.1/mirinae-0.1.1.tar.gz

---
+++ packages/mirinae/mirinae-0.1.1.tar.gz/mirinae-0.1.1/mirinae/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Copyright (c) 2024- MAGO
+# AUTHORS:
+# Sukbong Kwon (Galois)
+
+
+FRAMEWORK_ENDPOINT = "http://localhost:8001/v1/go"          # 개발 환경
+# FRAMEWORK_ENDPOINT = "http://147.47.41.91:8505/go"       # 운영 환경
+# FRAMEWORK_ENDPOINT = "http://st.mago52.com:8505/go"      # Audion
+# FRAMEWORK_ENDPOINT = "http://audion.mago52.com:8505/go"      # Audion
+
+HEADERS = {
+    "Accept": "application/json",
+    "Authorization": "Bearer eadc5d8d-ahno-9559-yesa-8c053e0f1f69",
+}
