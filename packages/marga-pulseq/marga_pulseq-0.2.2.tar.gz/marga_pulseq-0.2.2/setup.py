fcbff1fb58e17055a61147e0353ab17c2cd933b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:01 +0000

    Add package marga-pulseq/0.2.2/marga_pulseq-0.2.2.tar.gz

---
+++ packages/marga-pulseq/marga_pulseq-0.2.2.tar.gz/marga_pulseq-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='marga_pulseq',
+      version='0.2.2',
+      description='A simple interpreter from PulSeq 1.4 to MARGA',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/marcos-mri/marga_pulseq',
+      author='Lincoln Craven-Brightman,'
+             'Benjamin Menkuec,'
+             'José Miguel Algarín',
+      author_email='lcraven-brightman@mgh.harvard.edu,'
+                   'benjamin.menkuec@fh-dortmund.de'
+                   'josalggui@i3m.upv.es',
+      license='MIT',
+      packages=['marga_pulseq'],
+      install_requires=[],
+      classifiers=['Operating System :: OS Independent',
+                   'Programming Language :: Python'],
+      python_requires='>=3.6',
+      )
