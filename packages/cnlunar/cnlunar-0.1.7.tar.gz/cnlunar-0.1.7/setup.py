dbf189299430f0c9193294539a6259b7de8e91c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:25 +0000

    Add package cnlunar/0.1.7/cnlunar-0.1.7.tar.gz

---
+++ packages/cnlunar/cnlunar-0.1.7.tar.gz/cnlunar-0.1.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='cnlunar',
+    version='0.1.7',
+    packages=['cnlunar'],
+    url='https://github.com/OPN48/cnLunar',
+    author='cuba3',
+    author_email='cuba3@163.com',
+    long_description=open('README.rst', encoding='utf-8').read(),
+)
