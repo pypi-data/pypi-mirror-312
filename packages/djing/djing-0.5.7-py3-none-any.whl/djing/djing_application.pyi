c7da2f6186ae22e1db5999a89de488dd5c004095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:28 +0000

    Add package djing/0.5.7/djing-0.5.7-py3-none-any.whl

---
+++ packages/djing/djing-0.5.7-py3-none-any.whl/djing/djing_application.pyi
@@ -0,0 +1,7 @@
+from _typeshed import Incomplete
+from djing.core.Djing import Djing as Djing
+from djing.core.Providers.DjingCoreServiceProvider import DjingCoreServiceProvider as DjingCoreServiceProvider
+
+project_name: Incomplete
+
+def djing_application(): ...
