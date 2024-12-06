454ac59135263afd953f453cb3d26b0e574f0888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:31 +0000

    Add package bip38/1.3.1/bip38-1.3.1.tar.gz

---
+++ packages/bip38/bip38-1.3.1.tar.gz/bip38-1.3.1/bip38/info.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Copyright © 2023-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from typing import List
+
+__name__: str = "bip38"
+__version__: str = "v1.3.1"
+__license__: str = "MIT"
+__author__: str = "Meheret Tesfaye Batu"
+__email__: str = "meherett.batu@gmail.com"
+__documentation__: str = "https://bip38.readthedocs.com"
+__description__: str = "A Python library for implementation of Bitcoin Improvement Proposal - 0038 / BIP38 protocol."
+__source__: str = "https://github.com/talonlab/python-bip38"
+__url__: str = __source__
+__tracker__: str = f"{__url__}/issues"
+__keywords__: List[str] = [
+    "bip38", "bitcoin", "private-key", "hdwallet", "encrypt", "decrypt", "passphrase", "wif", "bip-0038"
+]
+__websites__: List[str] = [
+    "https://talonlab.org",
+    "https://talonlab.gitbook.io/bip38",
+    __documentation__
+]
