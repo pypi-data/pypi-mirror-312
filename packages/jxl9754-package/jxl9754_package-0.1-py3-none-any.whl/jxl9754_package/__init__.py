2db1a5b66f84e1af0619a9e7c5859fda066d872a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:22 +0000

    Add package jxl9754-package/0.1/jxl9754_package-0.1-py3-none-any.whl

---
+++ packages/jxl9754-package/jxl9754_package-0.1-py3-none-any.whl/jxl9754_package/__init__.py
@@ -0,0 +1,6 @@
+# Define a variable called version
+version = "1.0.0"
+
+__all__ = ["module1"]
+
+from . import module1
---
+++ packages/jxl9754-package/jxl9754_package-0.1-py3-none-any.whl/mypackage/module1.py
@@ -0,0 +1,3 @@
+# Define a function called func1
+def func1():
+    print("This is func1 from module1")
