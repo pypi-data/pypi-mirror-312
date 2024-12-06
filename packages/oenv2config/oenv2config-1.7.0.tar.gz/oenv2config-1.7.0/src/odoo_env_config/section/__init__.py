026f92306112ce87d4bc249de9e5c5d1b92ad8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:25 +0000

    Add package oenv2config/1.7.0/oenv2config-1.7.0.tar.gz

---
+++ packages/oenv2config/oenv2config-1.7.0.tar.gz/oenv2config-1.7.0/src/odoo_env_config/section/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "AddonsPathConfigSection",
+    "DatabaseOdooConfigSection",
+    "HttpOdooConfigSection",
+    "LimitOdooConfigSection",
+    "LoggerSection",
+    "MiscSection",
+    "TestOdooConfigSection",
+    "UpdateInstallSection",
+    "ServerWideModuleConfigSection",
+    "WorkersOdooConfigSection",
+    "S3Section",
+]
+from .addons_path_section import AddonsPathConfigSection
+from .db_section import DatabaseOdooConfigSection
+from .http_section import HttpOdooConfigSection
+from .limit_section import LimitOdooConfigSection
+from .log_section import LoggerSection
+from .misc_section import MiscSection
+from .s3_section import S3Section
+from .test_section import TestOdooConfigSection
+from .update_init_section import UpdateInstallSection
+from .widemodule_section import ServerWideModuleConfigSection
+from .worker_section import WorkersOdooConfigSection
