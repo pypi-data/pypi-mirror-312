587dea33bf4646b808da21d292793fa54fe1edfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:45 +0000

    Add package pyrime/0.0.5/pyrime-0.0.5.tar.gz

---
+++ packages/pyrime/pyrime-0.0.5.tar.gz/pyrime-0.0.5/shell.nix
@@ -0,0 +1,20 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+with pkgs;
+mkShell {
+  name = "pyrime";
+  buildInputs = [
+    meson
+    ninja
+    pkg-config
+    librime
+    stdenv.cc
+    (python3.withPackages (
+      p: with p; [
+        build
+      ]
+    ))
+  ];
+}
