c76eb0ed99d39e411d093a044743f7460714478d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:03:31 +0000

    Add package bambu-cli/0.2.4/bambu_cli-0.2.4.tar.gz

---
+++ packages/bambu-cli/bambu_cli-0.2.4.tar.gz/bambu_cli-0.2.4/src/bambucli/actions/addcloud.py
@@ -0,0 +1,36 @@
+
+from bambucli.bambu.httpclient import HttpClient
+from bambucli.bambu.printer import Printer
+from bambucli.config import add_printer, get_cloud_account
+
+
+def add_cloud_printer(args):
+    account = get_cloud_account(args.email)
+
+    if account is None:
+        return
+
+    printers = HttpClient().get_printers(account)
+    for index, printer in enumerate(printers):
+        print(
+            f"{index + 1}: {printer.name} - {printer.model.value} - {printer.serial_number}")
+
+    selection = input("Select a printer: ")
+    try:
+        selection = int(selection)
+        if selection < 1 or selection > len(printers):
+            raise ValueError
+        add_printer(Printer(
+            serial_number=printers[selection - 1].serial_number,
+            name=printers[selection - 1].name,
+            access_code=printers[selection - 1].access_code,
+            model=printers[selection - 1].model,
+            account_email=account.email,
+            ip_address=None
+        ))
+    except ValueError:
+        print("Invalid selection")
+        return
+    except Exception as e:
+        print(f"Failed to save printer configuration: {e}")
+        return
