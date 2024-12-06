ac15d550aec37f5e44d23f658965306480c6933f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:20 +0000

    Add package anjali-aws-helper/1.0.1/anjali_aws_helper-1.0.1.tar.gz

---
+++ packages/anjali-aws-helper/anjali_aws_helper-1.0.1.tar.gz/anjali_aws_helper-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anjali_aws_helper',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=['boto3'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
