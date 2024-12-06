f86150e90635b8cde0e81ba27d12e2d0c66d2d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:10 +0000

    Add package unxt/1.0.0/unxt-1.0.0.tar.gz

---
+++ packages/unxt/unxt-1.0.0.tar.gz/unxt-1.0.0/src/unxt/_src/units/core.py
@@ -0,0 +1,111 @@
+"""Units objects in JAX.
+
+Copyright (c) 2023 Galactic Dynamics. All rights reserved.
+"""
+
+__all__ = [
+    "unit",
+    "unit_of",
+    "Unit",  # legacy
+]
+
+from typing import Any, TypeAlias
+
+import astropy.units as apyu
+from astropy.units import Unit
+from plum import dispatch
+
+from unxt._src.dimensions import AbstractDimension
+
+AbstractUnits: TypeAlias = apyu.UnitBase | apyu.Unit
+
+# ===================================================================
+# Construct units
+
+
+@dispatch
+def unit(obj: AbstractUnits, /) -> AbstractUnits:
+    """Construct the units from a units object.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> m = u.unit("m")
+
+    >>> u.unit(m) is m
+    True
+
+    """
+    return obj
+
+
+@dispatch  # type: ignore[no-redef]
+def unit(obj: str, /) -> AbstractUnits:
+    """Construct units from a string.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> m = u.unit("m")
+    >>> m
+    Unit("m")
+
+    """
+    return apyu.Unit(obj)
+
+
+# ===================================================================
+# Get units
+
+
+@dispatch.abstract
+def unit_of(obj: Any, /) -> AbstractUnits:
+    """Return the units of an object."""
+
+
+@dispatch
+def unit_of(obj: Any, /) -> None:
+    """Return the units of an object.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> print(u.unit_of(1))
+    None
+
+    """
+    return None  # noqa: RET501
+
+
+@dispatch
+def unit_of(obj: AbstractUnits, /) -> AbstractUnits:
+    """Return the units of an unit.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> m = u.unit("m")
+
+    >>> u.unit_of(m)
+    Unit("m")
+
+    """
+    return obj
+
+
+# ===================================================================
+# Get dimensions
+
+
+@dispatch  # type: ignore[misc]
+def dimension_of(obj: AbstractUnits, /) -> AbstractDimension:
+    """Return the dimensions of the given units.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> u.dimension_of(u.unit("km"))
+    PhysicalType('length')
+
+    """
+    return obj.physical_type
---
+++ packages/unxt/unxt-1.0.0.tar.gz/unxt-1.0.0/src/unxt/_src/utils.py
@@ -0,0 +1,41 @@
+"""Utilities.
+
+Copyright (c) 2023 Galactic Dynamics. All rights reserved.
+"""
+
+__all__: list[str] = []
+
+from typing import TYPE_CHECKING, Any, cast
+
+if TYPE_CHECKING:
+    from typing import Self
+
+
+_singleton_insts: dict[type, object] = {}
+
+
+class SingletonMixin:
+    """Singleton class.
+
+    This class is a mixin that can be used to create singletons.
+
+    Examples
+    --------
+    >>> class MySingleton(SingletonMixin):
+    ...     pass
+
+    >>> a = MySingleton()
+    >>> b = MySingleton()
+    >>> a is b
+    True
+
+    """
+
+    def __new__(cls, /, *_: Any, **__: Any) -> "Self":
+        # Check if instance already exists
+        if cls in _singleton_insts:
+            return cast("Self", _singleton_insts[cls])
+        # Create new instance and cache it
+        self = object.__new__(cls)
+        _singleton_insts[cls] = self
+        return self
