407108bfa0d6dadfa93cfeafbfcfb5846931a422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:36 +0000

    Add package KIB-LAP/0.2.5/kib_lap-0.2.5.tar.gz

---
+++ packages/KIB-LAP/kib_lap-0.2.5.tar.gz/kib_lap-0.2.5/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import pybind11
+import subprocess
+
+# Lesen der README-Datei (optional)
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "KIB package description."
+
+# Definiere die Erweiterung für pybind11
+ext_modules = [
+    Extension(
+        "KIB_LAP.plate_buckling_cpp",  # Modulname (muss mit dem Python-Import übereinstimmen)
+        ["KIB_LAP/Plattenbeulen/plate_buckling.cpp"],  # Pfad zur C++-Datei
+        include_dirs=[
+            pybind11.get_include(),  # Include-Verzeichnis von pybind11
+        ],
+        language="c++"  # Sprache ist C++
+    )
+]
+
+# Konfiguration der Bibliothek
+setup(
+    name="KIB_LAP",
+    version="0.2.5",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A package for structural engineering calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="leoth",
+    author_email="thomas.leonard@outlook.de",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "pybind11>=2.6"  # pybind11 als Abhängigkeit hinzufügen
+    ],
+    ext_modules=ext_modules,  # Erweiterungen (C++-Module) hinzufügen
+    cmdclass={"build_ext": build_ext},  # Build-Kommando für Erweiterungen
+)
