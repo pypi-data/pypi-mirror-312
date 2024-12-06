275bb747f11dde3f67d60d8ee0c6d349b54b65bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:40 +0000

    Add package nonebot-plugin-osubot/6.13.1/nonebot-plugin-osubot-6.13.1.tar.gz

---
+++ packages/nonebot-plugin-osubot/nonebot-plugin-osubot-6.13.1.tar.gz/nonebot-plugin-osubot-6.13.1/nonebot_plugin_osubot/matcher/score.py
@@ -0,0 +1,29 @@
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.internal.adapter import Event
+from nonebot_plugin_alconna import UniMessage
+
+from ..utils import NGM
+from .utils import split_msg
+from ..draw import get_score_data
+
+score = on_command("score", priority=11, block=True)
+
+
+@score.handle(parameterless=[split_msg()])
+async def _score(event: Event, state: T_State):
+    if "error" in state:
+        await UniMessage.text(state["error"]).finish(reply_to=True)
+    if not state["target"]:
+        await UniMessage.text("请输入谱面ID").finish(reply_to=True)
+    data = await get_score_data(
+        state["user"],
+        event.get_user_id(),
+        NGM[state["mode"]],
+        mapid=state["target"],
+        mods=state["mods"],
+        is_name=state["is_name"],
+    )
+    if isinstance(data, str):
+        await UniMessage.text(data).finish(reply_to=True)
+    await UniMessage.image(raw=data).finish(reply_to=True)
