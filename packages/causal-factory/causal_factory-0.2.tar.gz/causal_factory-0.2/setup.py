3bbf2fe90ae0ce734ea342b17d81d3ab31358747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:03:32 +0000

    Add package causal-factory/0.2/causal_factory-0.2.tar.gz

---
+++ packages/causal-factory/causal_factory-0.2.tar.gz/causal_factory-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="causal-factory",
+    version="0.2",  # Increment the version for the new release
+    description="CausalFactory is Python package for causal model discovery and inference aims to seamlessly discover, visualize, and operationalize causal relationships",
+    long_description="CausalFactory is Python package for causal model discovery and inference aims to seamlessly discover, visualize, and operationalize causal relationships",  # Ensure you have a README.md file
+    long_description_content_type="text/markdown",
+    author="Awadelrahman M. A. Ahmed",
+    author_email="awadrahman@gmail.com",
+    url="https://github.com/Awadelrahman/causal-factory",  # Update if needed
+    packages=find_packages(),  # Automatically find and include all packages/modules
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+    "mlflow>=2.0.0",  
+    "pandas>=1.0.0",
+    "gastle",  # Ensure you spell this correctly or replace with "castle"
+    "torch",
+    "numpy>=1.18.0",  # Add numpy
+    "networkx>=2.5",  # Add networkx
+    "matplotlib>=3.2.0",  # Add matplotlib
+],
+)
