0180481862b499f94d75fce4dbbc955cfdd2f07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:12 +0000

    Add package phishing-detection-py/0.1.3/phishing_detection_py-0.1.3.tar.gz

---
+++ packages/phishing-detection-py/phishing_detection_py-0.1.3.tar.gz/phishing_detection_py-0.1.3/tests/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from phishing_detection.utils import load_config
+from unittest.mock import mock_open, patch
+
+class TestUtils(unittest.TestCase):
+    def test_load_config(self):
+        mock_yaml = """
+        models:
+          url: "ealvaradob/bert-finetuned-phishing"
+          email: "cybersectony/phishing-email-detection-distilbert_v2.4.1"
+        """
+        with patch("builtins.open", mock_open(read_data=mock_yaml)):
+            config = load_config("phishing_detection/config.yaml")
+            self.assertIn("models", config)
+            self.assertIn("url", config["models"])
+            self.assertIn("email", config["models"])
