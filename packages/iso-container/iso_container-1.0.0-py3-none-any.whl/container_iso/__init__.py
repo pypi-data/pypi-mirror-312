edff986562316eb2ba4cb16e3d75365f55e372cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:08 +0000

    Add package iso-container/1.0.0/iso_container-1.0.0-py3-none-any.whl

---
+++ packages/iso-container/iso_container-1.0.0-py3-none-any.whl/container_iso/__init__.py
@@ -0,0 +1,7 @@
+from .container_iso import get_container_info, validate_container
+
+__all__ = ['get_container_info', 'validate_container']
+
+__version__ = '0.1.0'
+__author__ = 'Jiseoup Lim'
+__email__ = 'jseoup@gmail.com'
