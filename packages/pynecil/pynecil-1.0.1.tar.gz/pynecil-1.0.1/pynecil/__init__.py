fd53d7e57c7d97c945ef008c080f1afc2a31f8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:17 +0000

    Add package pynecil/1.0.1/pynecil-1.0.1.tar.gz

---
+++ packages/pynecil/pynecil-1.0.1.tar.gz/pynecil-1.0.1/pynecil/__init__.py
@@ -0,0 +1,32 @@
+"""Pynecil - Python library to communicate with Pinecil V2 soldering irons via Bluetooth."""
+
+__version__ = "1.0.1"
+
+from .client import Pynecil, discover
+from .exceptions import CommunicationError
+from .types import (
+    CharBulk,
+    CharLive,
+    CharSetting,
+    DeviceInfoResponse,
+    LanguageCode,
+    LiveDataResponse,
+    OperatingMode,
+    PowerSource,
+    SettingsDataResponse,
+)
+
+__all__ = [
+    "CharBulk",
+    "CharLive",
+    "CharSetting",
+    "CommunicationError",
+    "DeviceInfoResponse",
+    "discover",
+    "LanguageCode",
+    "LiveDataResponse",
+    "OperatingMode",
+    "PowerSource",
+    "Pynecil",
+    "SettingsDataResponse",
+]
