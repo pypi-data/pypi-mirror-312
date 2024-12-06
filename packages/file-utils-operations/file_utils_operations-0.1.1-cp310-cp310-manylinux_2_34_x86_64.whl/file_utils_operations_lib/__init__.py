9bf60cdec9f6c24caef69ec766201334c3b4d00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:19 +0000

    Add package file-utils-operations/0.1.1/file_utils_operations-0.1.1-cp310-cp310-manylinux_2_34_x86_64.whl

---
+++ packages/file-utils-operations/file_utils_operations-0.1.1-cp310-cp310-manylinux_2_34_x86_64.whl/file_utils_operations_lib/__init__.py
@@ -0,0 +1,5 @@
+from .file_utils_operations_lib import *
+
+__doc__ = file_utils_operations_lib.__doc__
+if hasattr(file_utils_operations_lib, "__all__"):
+    __all__ = file_utils_operations_lib.__all__
