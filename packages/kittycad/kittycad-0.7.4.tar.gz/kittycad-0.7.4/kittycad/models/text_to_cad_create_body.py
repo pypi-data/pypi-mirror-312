7e69e2e3f1696bb4c5b1d1dee6d14ae8466c53b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:03:44 +0000

    Add package kittycad/0.7.4/kittycad-0.7.4.tar.gz

---
+++ packages/kittycad/kittycad-0.7.4.tar.gz/kittycad-0.7.4/kittycad/models/text_to_cad_create_body.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class TextToCadCreateBody(BaseModel):
+    """Body for generating models from text."""
+
+    prompt: str
+
+    model_config = ConfigDict(protected_namespaces=())
