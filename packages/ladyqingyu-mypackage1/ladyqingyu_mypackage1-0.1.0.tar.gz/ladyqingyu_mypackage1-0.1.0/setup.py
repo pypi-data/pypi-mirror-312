f9ff6b09d0bfc0f99140c16cb9a07e8450f153d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:31 +0000

    Add package ladyqingyu-mypackage1/0.1.0/ladyqingyu_mypackage1-0.1.0.tar.gz

---
+++ packages/ladyqingyu-mypackage1/ladyqingyu_mypackage1-0.1.0.tar.gz/ladyqingyu_mypackage1-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ladyqingyu_mypackage1",
+    version="0.1.0",
+    description="A simple package for addition",
+    author="ladyqingyu",
+    author_email="ladyqingyu@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
