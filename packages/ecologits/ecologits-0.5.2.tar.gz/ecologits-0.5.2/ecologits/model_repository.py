09393f46cf197b92318517eb78913bacb2b24d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:20 +0000

    Add package ecologits/0.5.2/ecologits-0.5.2.tar.gz

---
+++ packages/ecologits/ecologits-0.5.2.tar.gz/ecologits-0.5.2/ecologits/model_repository.py
@@ -0,0 +1,101 @@
+import json
+import os
+from enum import Enum
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+from ecologits.utils.range_value import ValueOrRange
+
+
+class Providers(Enum):
+    anthropic = "anthropic"
+    mistralai = "mistralai"
+    openai = "openai"
+    huggingface_hub = "huggingface_hub"
+    cohere = "cohere"
+    google = "google"
+
+
+class Warnings(Enum):
+    model_architecture_not_released = "model_architecture_not_released"
+    model_architecture_multimodal = "model_architecture_multimodal"
+
+
+
+class ArchitectureTypes(Enum):
+    DENSE = "dense"
+    MOE = "moe"
+
+
+class ParametersMoE(BaseModel):
+    total: ValueOrRange
+    active: ValueOrRange
+
+
+class Architecture(BaseModel):
+    type: ArchitectureTypes
+    parameters: Union[ValueOrRange, ParametersMoE]
+
+
+class Alias(BaseModel):
+    provider: Providers
+    name: str
+    alias: str
+
+
+class Model(BaseModel):
+    provider: Providers
+    name: str
+    architecture: Architecture
+    warnings: Optional[list[Warnings]] = None
+    sources: Optional[list[str]] = None
+
+
+class Models(BaseModel):
+    aliases: Optional[list[Alias]] = None
+    models: Optional[list[Model]] = None
+
+
+class ModelRepository:
+
+    def __init__(self, models: list[Model], aliases: Optional[list[Alias]] = None) -> None:
+        self.__models: dict[tuple[str, str], Model] = {}
+        if models is not None:
+            for m in models:
+                key = m.provider.value, m.name
+                if key in self.__models:
+                    raise ValueError(f"duplicated models with: {key}")
+                self.__models[key] = m
+
+        if aliases is not None:
+            for a in aliases:
+                model_key = a.provider.value, a.alias
+                if model_key not in self.__models:
+                    raise ValueError(f"model alias not found: {model_key}")
+                alias_key = a.provider.value, a.name
+                model = self.__models[model_key].model_copy()
+                model.name = a.name
+                self.__models[alias_key] = model
+
+    def find_model(self, provider: str, model_name: str) -> Optional[Model]:
+        return self.__models.get((provider, model_name))
+
+    def list_models(self) -> list[Model]:
+        return list(self.__models.values())
+
+    @classmethod
+    def from_json(cls, filepath: Optional[str] = None) -> "ModelRepository":
+        if filepath is None:
+            filepath = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "data", "models.json"
+            )
+        with open(filepath) as fd:
+            data = json.load(fd)
+            mf = Models.model_validate(data)
+        if mf.models is None:
+            raise ValueError("Cannot initialize on an empty model repository.")
+        return cls(models=mf.models, aliases=mf.aliases)
+
+
+models = ModelRepository.from_json()
---
+++ packages/ecologits/ecologits-0.5.2.tar.gz/ecologits-0.5.2/ecologits/tracers/utils.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+from ecologits.electricity_mix_repository import electricity_mixes
+from ecologits.impacts.llm import compute_llm_impacts
+from ecologits.impacts.modeling import Impacts
+from ecologits.log import logger
+from ecologits.model_repository import ParametersMoE, models
+
+
+def _avg(value_range: tuple) -> float:
+    return sum(value_range) / len(value_range)
+
+
+def llm_impacts(
+    provider: str,
+    model_name: str,
+    output_token_count: int,
+    request_latency: float,
+    electricity_mix_zone: str = "WOR",
+) -> Optional[Impacts]:
+    """
+    High-level function to compute the impacts of an LLM generation request.
+
+    Args:
+        provider: Name of the provider.
+        model_name: Name of the LLM used.
+        output_token_count: Number of generated tokens.
+        request_latency: Measured request latency in seconds.
+        electricity_mix_zone: ISO 3166-1 alpha-3 code of the electricity mix zone (WOR by default).
+
+    Returns:
+        The impacts of an LLM generation request.
+    """
+
+    model = models.find_model(provider=provider, model_name=model_name)
+    if model is None:
+        logger.debug(f"Could not find model `{model_name}` for {provider} provider.")
+        return None
+
+    if isinstance(model.architecture.parameters, ParametersMoE):
+        model_total_params = model.architecture.parameters.total
+        model_active_params = model.architecture.parameters.active
+    else:
+        model_total_params = model.architecture.parameters
+        model_active_params = model.architecture.parameters
+
+    electricity_mix = electricity_mixes.find_electricity_mix(zone=electricity_mix_zone)
+    if electricity_mix is None:
+        logger.debug(f"Could not find electricity mix `{electricity_mix_zone}` in the ADEME database")
+        return None
+    if_electricity_mix_adpe=electricity_mix.adpe
+    if_electricity_mix_pe=electricity_mix.pe
+    if_electricity_mix_gwp=electricity_mix.gwp
+    return compute_llm_impacts(
+        model_active_parameter_count=model_active_params,
+        model_total_parameter_count=model_total_params,
+        output_token_count=output_token_count,
+        request_latency=request_latency,
+        if_electricity_mix_adpe=if_electricity_mix_adpe,
+        if_electricity_mix_pe=if_electricity_mix_pe,
+        if_electricity_mix_gwp=if_electricity_mix_gwp,
+    )
