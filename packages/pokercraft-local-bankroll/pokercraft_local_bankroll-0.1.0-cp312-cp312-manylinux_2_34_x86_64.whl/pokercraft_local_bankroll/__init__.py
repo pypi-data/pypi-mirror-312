fa4c18fd3ee793d4e75038c6305de1e14e0e78ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:01 +0000

    Add package pokercraft-local-bankroll/0.1.0/pokercraft_local_bankroll-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/pokercraft-local-bankroll/pokercraft_local_bankroll-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl/pokercraft_local_bankroll/__init__.py
@@ -0,0 +1,5 @@
+from .pokercraft_local_bankroll import *
+
+__doc__ = pokercraft_local_bankroll.__doc__
+if hasattr(pokercraft_local_bankroll, "__all__"):
+    __all__ = pokercraft_local_bankroll.__all__
