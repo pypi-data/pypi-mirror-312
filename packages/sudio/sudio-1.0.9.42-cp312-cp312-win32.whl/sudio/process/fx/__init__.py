485db73689399961a722dab2d3b8241f77d33ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:43 +0000

    Add package sudio/1.0.9.42/sudio-1.0.9.42-cp312-cp312-win32.whl

---
+++ packages/sudio/sudio-1.0.9.42-cp312-cp312-win32.whl/sudio/process/fx/__init__.py
@@ -0,0 +1,5 @@
+from .fx import FX
+from .gain import Gain
+from .tempo import Tempo
+from .fade_envelope import FadeEnvelope, FadePreset
+from sudio._process_fx_fade_envelope import generate_envelope, prepare_envelope
