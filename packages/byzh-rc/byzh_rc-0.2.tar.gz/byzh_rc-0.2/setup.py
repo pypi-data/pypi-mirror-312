6306bdc312d2170dddde73cb3b7f7cfa12a31fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:47 +0000

    Add package byzh-rc/0.2/byzh_rc-0.2.tar.gz

---
+++ packages/byzh-rc/byzh_rc-0.2.tar.gz/byzh_rc-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='byzh_rc',
+    version='0.2',
+    author="byzh_rc",
+    description="更方便的深度学习",
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+)
