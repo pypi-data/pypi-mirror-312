e18c83095ed33957c7b2153564e5be36d63207e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:22 +0000

    Add package ThermiaOnlineAPI/6.3.1/thermiaonlineapi-6.3.1.tar.gz

---
+++ packages/ThermiaOnlineAPI/thermiaonlineapi-6.3.1.tar.gz/thermiaonlineapi-6.3.1/ThermiaOnlineAPI/__init__.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from ThermiaOnlineAPI.api.ThermiaAPI import ThermiaAPI
+from ThermiaOnlineAPI.exceptions import AuthenticationException, NetworkException
+from ThermiaOnlineAPI.model.HeatPump import ThermiaHeatPump
+
+
+class Thermia:
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+
+        self.api_interface = ThermiaAPI(username, password)
+        self.connected = self.api_interface.authenticated
+
+        self.heat_pumps = self.fetch_heat_pumps()
+
+    def fetch_heat_pumps(self) -> List[ThermiaHeatPump]:
+        devices = self.api_interface.get_devices()
+        heat_pumps = []
+
+        for device in devices:
+            heat_pumps.append(ThermiaHeatPump(device, self.api_interface))
+
+        return heat_pumps
+
+    def update_data(self) -> None:
+        for heat_pump in self.heat_pumps:
+            heat_pump.update_data()
---
+++ packages/ThermiaOnlineAPI/thermiaonlineapi-6.3.1.tar.gz/thermiaonlineapi-6.3.1/ThermiaOnlineAPI/const.py
@@ -0,0 +1,104 @@
+###############################################################################
+# General configuration
+###############################################################################
+
+THERMIA_CONFIG_URL = "https://online.thermia.se/api/configuration"
+THERMIA_INSTALLATION_PATH = "/api/v1/Registers/Installations/"
+
+###############################################################################
+# Azure AD configuration
+###############################################################################
+
+THERMIA_AZURE_AUTH_URL = "https://thermialogin.b2clogin.com/thermialogin.onmicrosoft.com/b2c_1a_signuporsigninonline"
+THERMIA_AZURE_AUTH_CLIENT_ID_AND_SCOPE = "09ea4903-9e95-45fe-ae1f-e3b7d32fa385"
+THERMIA_AZURE_AUTH_REDIRECT_URI = "https://online.thermia.se/login"
+
+###############################################################################
+# Register groups
+###############################################################################
+
+REG_GROUP_TEMPERATURES = "REG_GROUP_TEMPERATURES"
+REG_GROUP_OPERATIONAL_STATUS = "REG_GROUP_OPERATIONAL_STATUS"
+REG_GROUP_OPERATIONAL_TIME = "REG_GROUP_OPERATIONAL_TIME"
+REG_GROUP_OPERATIONAL_OPERATION = "REG_GROUP_OPERATIONAL_OPERATION"
+REG_GROUP_HOT_WATER = "REG_GROUP_HOT_WATER"
+
+###############################################################################
+# Temperature registers
+###############################################################################
+
+REG_OUTDOOR_TEMPERATURE = "REG_OUTDOOR_TEMPERATURE"  # Not used
+REG_OPER_DATA_OUTDOOR_TEMP_MA_SA = "REG_OPER_DATA_OUTDOOR_TEMP_MA_SA"  # Not used
+REG_INDOOR_TEMPERATURE = "REG_INDOOR_TEMPERATURE"
+REG_SUPPLY_LINE = "REG_SUPPLY_LINE"
+REG_HOT_WATER_TEMPERATURE = "REG_HOT_WATER_TEMPERATURE"
+REG_BRINE_OUT = "REG_BRINE_OUT"
+REG_BRINE_IN = "REG_BRINE_IN"
+REG_OPER_DATA_BUFFER_TANK = "REG_OPER_DATA_BUFFER_TANK"
+
+###############################################################################
+# Temperature registers ("classic" specific)
+###############################################################################
+
+REG_RETURN_LINE = "REG_RETURN_LINE"
+REG_DESIRED_SUPPLY_LINE = "REG_DESIRED_SUPPLY_LINE"
+REG_OPER_DATA_SUPPLY_MA_SA = "REG_OPER_DATA_SUPPLY_MA_SA"
+REG_DESIRED_SUPPLY_LINE_TEMP = "REG_DESIRED_SUPPLY_LINE_TEMP"
+REG_DESIRED_INDOOR_TEMPERATURE = "REG_DESIRED_INDOOR_TEMPERATURE"
+
+###############################################################################
+# Temperature registers ("genesis" specific)
+###############################################################################
+
+REG_OPER_DATA_RETURN = "REG_OPER_DATA_RETURN"
+REG_DESIRED_SYS_SUPPLY_LINE_TEMP = "REG_DESIRED_SYS_SUPPLY_LINE_TEMP"
+REG_COOL_SENSOR_TANK = "REG_COOL_SENSOR_TANK"
+REG_COOL_SENSOR_SUPPLY = "REG_COOL_SENSOR_SUPPLY"
+REG_ACTUAL_POOL_TEMP = "REG_ACTUAL_POOL_TEMP"
+
+###############################################################################
+# Operational operation registers
+###############################################################################
+
+REG_OPERATIONMODE = "REG_OPERATIONMODE"
+
+###############################################################################
+# Operational status registers
+###############################################################################
+
+REG_OPERATIONAL_STATUS_PRIO1 = (
+    "REG_OPERATIONAL_STATUS_PRIO1"  # Operational status for most heat pumps
+)
+COMP_STATUS = "COMP_STATUS"  # Operational status for Diplomat heat pumps
+COMP_STATUS_ITEC = "COMP_STATUS_ITEC"  # Operational status for ITEC heat pumps
+REG_OPERATIONAL_STATUS_PRIORITY_BITMASK = (
+    "REG_OPERATIONAL_STATUS_PRIORITY_BITMASK"  # Operational status for Atlas heat pumps
+)
+REG_INTEGRAL_LSD = "REG_INTEGRAL_LSD"
+REG_PID = "REG_PID"
+
+COMP_POWER_STATUS = "COMP_POWER_STATUS"
+
+###############################################################################
+# Hot water registers
+###############################################################################
+
+REG_HOT_WATER_STATUS = "REG_HOT_WATER_STATUS"
+REG__HOT_WATER_BOOST = "REG__HOT_WATER_BOOST"
+
+###############################################################################
+# Operational time registers
+###############################################################################
+
+REG_OPER_TIME_IMM1 = "REG_OPER_TIME_IMM1"  # Auxiliary heater 1
+REG_OPER_TIME_IMM2 = "REG_OPER_TIME_IMM2"  # Auxiliary heater 2
+REG_OPER_TIME_IMM3 = "REG_OPER_TIME_IMM3"  # Auxiliary heater 3
+REG_OPER_TIME_COMPRESSOR = "REG_OPER_TIME_COMPRESSOR"
+REG_OPER_TIME_HEATING = "REG_OPER_TIME_HEATING"
+REG_OPER_TIME_HOT_WATER = "REG_OPER_TIME_HOT_WATER"
+
+###############################################################################
+# Other
+###############################################################################
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
