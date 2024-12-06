b3fb618203dfcf5d2029e5b1d87a4376fbfc95d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:45 +0000

    Add package DevilCore/1/devilcore-1.tar.gz

---
+++ packages/DevilCore/devilcore-1.tar.gz/devilcore-1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+setup(
+	version='1',
+	name='DevilCore',
+	author='DevilCore',
+	description='Hello Devil',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
