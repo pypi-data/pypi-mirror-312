4f2aa9790c51f65c584adddcdb3fdaf4cef29d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:10 +0000

    Add package vectice/24.4.8.0/vectice-24.4.8.0.tar.gz

---
+++ packages/vectice/vectice-24.4.8.0.tar.gz/vectice-24.4.8.0/src/vectice/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from vectice.api import json
+from vectice.api.client import Client
+from vectice.api.gql_phase import GqlPhaseApi
+from vectice.api.http_error_handlers import InvalidReferenceError, MissingReferenceError
+from vectice.api.iteration import IterationApi
+from vectice.api.workspace import WorkspaceApi
+
+__all__ = [
+    "MissingReferenceError",
+    "InvalidReferenceError",
+    "Client",
+    "WorkspaceApi",
+    "json",
+    "GqlPhaseApi",
+    "IterationApi",
+]
