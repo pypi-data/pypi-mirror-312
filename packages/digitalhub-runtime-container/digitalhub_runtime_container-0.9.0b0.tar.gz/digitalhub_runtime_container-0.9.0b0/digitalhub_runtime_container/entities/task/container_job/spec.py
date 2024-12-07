d229eb5bfb70fd1982b4e9141beadc5c980400f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:26 +0000

    Add package digitalhub-runtime-container/0.9.0b0/digitalhub_runtime_container-0.9.0b0.tar.gz

---
+++ packages/digitalhub-runtime-container/digitalhub_runtime_container-0.9.0b0.tar.gz/digitalhub_runtime_container-0.9.0b0/digitalhub_runtime_container/entities/task/container_job/spec.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from digitalhub.entities.task._base.spec import TaskSpecFunction, TaskValidatorFunction
+from pydantic import Field
+
+
+class TaskSpecContainerJob(TaskSpecFunction):
+    """
+    TaskSpecContainerJob specifications.
+    """
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: list[dict] | None = None,
+        volumes: list[dict] | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list[dict] | None = None,
+        envs: list[dict] | None = None,
+        secrets: list[str] | None = None,
+        profile: str | None = None,
+        backoff_limit: int | None = None,
+        schedule: str | None = None,
+        fsGroup: int | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function,
+            node_selector,
+            volumes,
+            resources,
+            affinity,
+            tolerations,
+            envs,
+            secrets,
+            profile,
+            **kwargs,
+        )
+        self.backoff_limit = backoff_limit
+        self.schedule = schedule
+        self.fsGroup = fsGroup
+
+
+class TaskValidatorContainerJob(TaskValidatorFunction):
+    """
+    TaskValidatorContainerJob validator.
+    """
+
+    backoff_limit: int = Field(default=None, ge=0)
+    """Backoff limit."""
+
+    schedule: str = None
+    """Schedule."""
+
+    fsGroup: int = Field(default=None, ge=1)
+    """FSGroup."""
