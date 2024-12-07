974637e7eaad14b6af7bdd84b69d782c5e040370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:31:46 +0000

    Add package oarepo-requests/2.3.7/oarepo_requests-2.3.7.tar.gz

---
+++ packages/oarepo-requests/oarepo_requests-2.3.7.tar.gz/oarepo_requests-2.3.7/oarepo_requests/services/permissions/identity.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Request needs."""
+
+from invenio_access.permissions import SystemRoleNeed
+
+request_active = SystemRoleNeed("request")
+"""Need that is added to identity whenever a request is being handled (inside the 'accept' action)."""
