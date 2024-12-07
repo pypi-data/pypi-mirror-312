ca9f0829831bd6ea3d7111ff851afac936d28682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:48 +0000

    Add package CogniSolve/5/CogniSolve-5-py3-none-any.whl

---
+++ packages/CogniSolve/CogniSolve-5-py3-none-any.whl/CogniSolve/__init__.py
@@ -0,0 +1,12 @@
+from .pyalgorithms import factorial, binary_gcd, is_prime
+
+__all__ = [
+    "factorial",
+    "binary_gcd",
+    "is_prime",
+    "EratosfenSieve",
+    "heuristic",
+    "astar",
+    "binomial_coefficient",
+    "Pascal_triangle",
+]
