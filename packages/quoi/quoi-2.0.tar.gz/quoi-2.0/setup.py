c7b6e768cf984c737ee2b8ee869bf43282bc2095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:04:52 +0000

    Add package quoi/2.0/quoi-2.0.tar.gz

---
+++ packages/quoi/quoi-2.0.tar.gz/quoi-2.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_namespace_packages
+# Install quoi, feur and coubeh, who are all in src/
+setup(
+    name="quoi",
+    url="https://github.com", 
+    author="Timothé Boulet",
+    author_email="timothe.boulet0@gmail.com",
+    
+    packages=find_namespace_packages(),
+
+    version="2.0",
+    license="MIT",
+    description="bruh",
+)
+
+# setup(
+#     name="feur",
+#     url="https://github.com", 
+#     author="Timothé Boulet",
+#     author_email="timothe.boulet0@gmail.com",
+    
+#     packages=find_namespace_packages(),
+
+#     version="2.0",
+#     license="MIT",
+#     description="bruh",
+# )
+
+# setup(
+#     name="coubeh",
+#     url="https://github.com", 
+#     author="Timothé Boulet",
+#     author_email="timothe.boulet0@gmail.com",
+    
+#     packages=find_namespace_packages(),
+
+#     version="2.0",
+#     license="MIT",
+#     description="bruh",
+# )
