561808f978e8682af21306f7ec4d97056b15a036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:46 +0000

    Add package TANHACKER/0.6/tanhacker-0.6.tar.gz

---
+++ packages/TANHACKER/tanhacker-0.6.tar.gz/tanhacker-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.6',
+	name='TANHACKER',
+	author='TANHACKER',
+	description='TAN ENCODERS',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
