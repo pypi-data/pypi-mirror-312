86cd32281c7c4c2bac9817ec8459f2965cf1677b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:30:46 +0000

    Add package my-code-display/3.0.0/my_code_display-3.0.0.tar.gz

---
+++ packages/my-code-display/my_code_display-3.0.0.tar.gz/my_code_display-3.0.0/README.md
@@ -0,0 +1,8 @@
+# My Code Display
+
+A Python package that displays code snippets when asked.
+
+## Installation
+
+```bash
+pip install my_code_display
---
+++ packages/my-code-display/my_code_display-3.0.0.tar.gz/my_code_display-3.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_code_display",  # Unique name for your package
+    version='3.0.0',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "code-display=my_code_display.main:main",
+        ],
+    },
+    author="Mmm",
+    description="A simple package that displays code when asked.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_code_display",  # Replace with your GitHub URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
