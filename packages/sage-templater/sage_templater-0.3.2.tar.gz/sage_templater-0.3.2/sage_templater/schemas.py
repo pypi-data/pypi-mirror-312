9508d7979284a91fa5c39637924cd9b54ea0748f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:13 +0000

    Add package sage-templater/0.3.2/sage_templater-0.3.2.tar.gz

---
+++ packages/sage-templater/sage_templater-0.3.2.tar.gz/sage_templater-0.3.2/sage_templater/schemas.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from typing import Annotated, AnyStr, Optional
+
+from pydantic import BaseModel, BeforeValidator
+
+
+def validate_tax(value: AnyStr):
+    """Validate the tax value."""
+    if isinstance(value, tuple):
+        value = value[0]
+    if value is None or value == "" or value == "None":
+        return Decimal("0.0")
+    return Decimal(value)
+
+
+Tax = Annotated[Decimal, BeforeValidator(validate_tax)]
+
+
+class SmallBoxRecordSchema(BaseModel):
+    """Small box record schema."""
+
+    code: Optional[str] = None
+    national_id: Optional[str] = None
+    verification_digit: Optional[str] = None
+    name: str
+    invoice: Optional[str] = None
+    date: str
+    amount: Decimal
+    tax: Tax
+    total: Decimal
+    description: str
+    source_file: Optional[str] = None
+    source_sheet: Optional[str] = None
