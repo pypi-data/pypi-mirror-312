2659e829c0b3551073163f2bd3ad2090c987c74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:03 +0000

    Add package kaya-module-sdk/2.0.10/kaya_module_sdk-2.0.10.tar.gz

---
+++ packages/kaya-module-sdk/kaya_module_sdk-2.0.10.tar.gz/kaya_module_sdk-2.0.10/kaya_module_sdk/src/utils/metadata/less.py
@@ -0,0 +1,19 @@
+from kaya_module_sdk.src.utils.metadata.abstract_metadata import KMetadata
+from kaya_module_sdk.src.utils.metadata.abstract_validation import KValidation
+
+
+class LT(KMetadata, KValidation):
+    def __init__(self, value: float | str) -> None:
+        self._data = {
+            "less": value,
+        }
+
+    def __str__(self) -> str:
+        return f'<:{self._data["less"]}'
+
+    def load(self, str_repr: str) -> dict:
+        segmented = str_repr.split(":")
+        if not len(segmented) == 2 or segmented[0] != "<":
+            return {}
+        self._data["less"] = float(segmented[1])
+        return self._data
