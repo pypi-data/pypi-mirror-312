5e34b63ab413688d1d621c3c3cf9fddea037671d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:04:59 +0000

    Add package pynxtools-em/0.3.1/pynxtools_em-0.3.1.tar.gz

---
+++ packages/pynxtools-em/pynxtools_em-0.3.1.tar.gz/pynxtools_em-0.3.1/src/pynxtools_em/configurations/conventions_cfg.py
@@ -0,0 +1,110 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Dict mapping values for conventions and reference frames."""
+
+from typing import Any, Dict
+
+# /ENTRY[entryID]/ROI[roiID]/ebsd/conventions"
+CONV_ROTATIONS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/coordinate_system_set",
+    "prefix_src": "rotation_conventions/",
+    "map": [
+        "rotation_handedness",
+        "rotation_convention",
+        "euler_angle_convention",
+        "axis_angle_convention",
+        "sign_convention",
+    ],
+}
+
+
+CONV_PROCESSING_CSYS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/coordinate_system_set/processing_reference_frame",
+    "prefix_src": "processing_reference_frame/",
+    "map": [
+        "type",
+        "handedness",
+        "origin",
+        "x_alias",
+        "x_direction",
+        "y_alias",
+        "y_direction",
+        "z_alias",
+        "z_direction",
+    ],
+}
+
+
+CONV_SAMPLE_CSYS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/coordinate_system_set/sample_reference_frame",
+    "prefix_src": "sample_reference_frame/",
+    "map": [
+        "type",
+        "handedness",
+        "origin",
+        "x_alias",
+        "x_direction",
+        "y_alias",
+        "y_direction",
+        "z_alias",
+        "z_direction",
+    ],
+}
+
+
+CONV_DETECTOR_CSYS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/coordinate_system_set/COORDINATE_SYSTEM[detector_reference_frame1]",
+    "prefix_src": "detector_reference_frame/",
+    "map": [
+        "type",
+        "handedness",
+        "origin",
+        "x_alias",
+        "x_direction",
+        "y_alias",
+        "y_direction",
+        "z_alias",
+        "z_direction",
+    ],
+}
+
+
+CONV_GNOMONIC_CSYS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/ROI[roi*]/ebsd/gnomonic_reference_frame",
+    "prefix_src": "gnomonic_reference_frame/",
+    "map": [
+        "type",
+        "handedness",
+        "origin",
+        "x_direction",
+        "y_direction",
+        "z_direction",
+    ],
+}
+
+
+CONV_PATTERN_CSYS_TO_NEXUS: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/ROI[roi*]/ebsd/pattern_centre",
+    "prefix_src": "pattern_centre/",
+    "map": [
+        "x_boundary_convention",
+        "x_normalization_direction",
+        "y_boundary_convention",
+        "y_normalization_direction",
+    ],
+}
---
+++ packages/pynxtools-em/pynxtools_em-0.3.1.tar.gz/pynxtools_em-0.3.1/src/pynxtools_em/configurations/image_png_protochips_cfg.py
@@ -0,0 +1,133 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Configuration of the image_png_protochips parser."""
+
+import re
+from typing import Any, Dict
+
+from pynxtools_em.utils.pint_custom_unit_registry import ureg
+
+
+def specific_to_variadic(token):
+    # "MicroscopeControlImageMetadata.AuxiliaryData.AuxiliaryDataCategory.[0].DataValues.AuxiliaryDataValue.[20].HeatingPower"
+    # to "MicroscopeControlImageMetadata.AuxiliaryData.AuxiliaryDataCategory.[*].DataValues.AuxiliaryDataValue.[*].HeatingPower"
+    if isinstance(token, str) and token != "":
+        concept = token.strip()
+        idxs = re.finditer(r".\[[0-9]+\].", concept)
+        if sum(1 for _ in idxs) > 0:
+            variadic = concept
+            for idx in re.finditer(r".\[[0-9]+\].", concept):
+                variadic = variadic.replace(concept[idx.start(0) : idx.end(0)], ".[*].")
+            return variadic
+        else:
+            return concept
+    return None
+
+
+AXON_STATIC_STAGE_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/em_lab/STAGE_LAB[stage_lab]",
+    "prefix_src": "MicroscopeControlImageMetadata.ActivePositionerSettings.PositionerSettings.[*].Stage.",
+    "use": [("design", "heating_chip")],
+    "map": [("alias", "Name")],
+}
+
+
+AXON_STATIC_DETECTOR_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/em_lab/detectorID[detector*]",
+    "prefix_src": "",
+    "use": [
+        (
+            "local_name",
+            "As tested with AXON 10.4.4.21, 2021-04-26T22:51:28.4539893-05:00 not included in Protochips PNG metadata",
+        )
+    ],
+}
+
+
+AXON_DYNAMIC_STAGE_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]/em_lab/STAGE_LAB[stage_lab]",
+    "prefix_src": "MicroscopeControlImageMetadata.ActivePositionerSettings.PositionerSettings.[*].Stage.",
+    "map_to_f8": [
+        ("position", ureg.meter, ["X", "Y", "Z"], ureg.meter)
+    ],  # values are much to large to be in m!
+}
+
+
+AXON_DYNAMIC_CHIP_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]/em_lab/heater",
+    "prefix_src": "MicroscopeControlImageMetadata.AuxiliaryData.AuxiliaryDataCategory.[*].DataValues.AuxiliaryDataValue.[*].",
+    "map_to_f8": [
+        ("current", ureg.ampere, "HeatingCurrent", ureg.ampere),
+        ("power", ureg.watt, "HeatingPower", ureg.watt),
+        ("voltage", ureg.volt, "HeatingVoltage", ureg.volt),
+    ],
+}
+
+
+AXON_DYNAMIC_AUX_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]/em_lab/ebeam_column",
+    "prefix_src": "MicroscopeControlImageMetadata.AuxiliaryData.AuxiliaryDataCategory.[*].DataValues.AuxiliaryDataValue.[*].",
+    "use": [
+        ("sensorID[sensor1]/measurement", "temperature"),
+        ("sensorID[sensor2]/measurement", "pressure"),
+    ],
+    "map_to_f8": [
+        ("sensorID[sensor1]/value", ureg.degC, "HolderTemperature", ureg.degC),
+        ("sensorID[sensor2]/value", ureg.bar, "HolderPressure", ureg.torr),
+    ],
+}
+
+
+AXON_DYNAMIC_VARIOUS_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]",
+    "prefix_src": "MicroscopeControlImageMetadata.MicroscopeSettings.",
+    "use": [
+        (
+            "event_type",
+            "As tested with AXON 10.4.4.21, 2021-04-26T22:51:28.4539893-05:00 not included in Protochips PNG metadata",
+        ),
+        (
+            "em_lab/DETECTOR[detector*]/mode",
+            "As tested with AXON 10.4.4.21, 2021-04-26T22:51:28.4539893-05:00 not included in Protochips PNG metadata",
+        ),
+    ],
+    "map": [
+        (
+            "em_lab/EBEAM_COLUMN[ebeam_column]/DEFLECTOR[beam_blanker1]/state",
+            "BeamBlankerState",
+        ),
+    ],
+    "map_to_f8": [
+        (
+            "em_lab/EBEAM_COLUMN[ebeam_column]/electron_source/voltage",
+            ureg.volt,
+            "AcceleratingVoltage",
+            ureg.volt,
+        ),
+        (
+            "em_lab/OPTICAL_SYSTEM_EM[optical_system_em]/camera_length",
+            ureg.meter,
+            "CameraLengthValue",
+            ureg.meter,
+        ),
+        (
+            "em_lab/OPTICAL_SYSTEM_EM[optical_system_em]/magnification",
+            "MagnificationValue",
+        ),
+    ],
+}
