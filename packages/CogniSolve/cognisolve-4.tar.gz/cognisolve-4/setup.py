d0a6b28e89a8bb3c20d40d0f61e595ba3bbf650f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:26 +0000

    Add package CogniSolve/4/cognisolve-4.tar.gz

---
+++ packages/CogniSolve/cognisolve-4.tar.gz/cognisolve-4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="CogniSolve",
+      version="4",
+      description="Algorithms in one python library.",
+      packages=find_packages(),
+      author="Andrei Sergeev",
+      author_email="andreiefremov135@gmail.com",
+      zip_safe=False)
