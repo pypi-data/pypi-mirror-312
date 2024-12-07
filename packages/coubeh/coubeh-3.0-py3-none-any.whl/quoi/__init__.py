3168ef27537e5b344645f40062dd6b19548e5b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:00 +0000

    Add package coubeh/3.0/coubeh-3.0-py3-none-any.whl

---
+++ packages/coubeh/coubeh-3.0-py3-none-any.whl/quoi/__init__.py
@@ -0,0 +1,6 @@
+import torch as team_fortress
+
+def key_function_generation_inverse_function_fast():
+    return team_fortress
+
+raise NotImplementedError("This package is no longer maintained and is now replaced by another one (cmon you know the name...).")
