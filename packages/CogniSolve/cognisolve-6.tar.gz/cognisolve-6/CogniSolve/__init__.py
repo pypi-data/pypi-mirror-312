68c7f7a9b7ca7f2c4fcae40cd2e504759e9a0af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:00 +0000

    Add package CogniSolve/6/cognisolve-6.tar.gz

---
+++ packages/CogniSolve/cognisolve-6.tar.gz/cognisolve-6/CogniSolve/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "factorial",
+    "binary_gcd",
+    "is_prime",
+    "EratosfenSieve",
+    "heuristic",
+    "astar",
+    "binomial_coefficient",
+    "Pascal_triangle",
+]
+
+
+from .pyalgorithms import *
