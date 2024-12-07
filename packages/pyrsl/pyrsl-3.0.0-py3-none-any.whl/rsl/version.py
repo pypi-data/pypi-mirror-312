152756877ca478da0bc565464d9dfaf993b038e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:35:37 +0000

    Add package pyrsl/3.0.0/pyrsl-3.0.0-py3-none-any.whl

---
+++ packages/pyrsl/pyrsl-3.0.0-py3-none-any.whl/rsl/version.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+# Copyright (C) 2016 John Törnblom
+'''
+Keep version info in one single place
+'''
+
+
+name = 'pyrsl'
+date = '2024-12-02'
+version = '3.0'
+release = '3.0.0' #  # ensure that this is the same as in setup.py
+
+complete_string = '%s v%s (%s)' % (name, release, date)
