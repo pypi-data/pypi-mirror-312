f306ebf6e96f4e93fb827d57ffc14045ebacec1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:31 +0000

    Add package conkeyscan/1.1.0/conkeyscan-1.1.0.tar.gz

---
+++ packages/conkeyscan/conkeyscan-1.1.0.tar.gz/conkeyscan-1.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="conkeyscan",
+    author="Jan Friedli",
+    url="https://github.com/CompassSecurity/conkeyscan",
+    description="A Pentesters Confluence Keyword Scanner",
+    version="v1.1.0",
+    license="mit",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["conkeyscan"],
+    install_requires=[
+        "loguru",
+        "atlassian-python-api>=3",
+        "beautifulsoup4>=4",
+        "requests-ratelimiter",
+        "clize>=5",
+        "random-user-agent>=1",
+        "readchar",
+        "PySocks>=1",
+    ],
+    entry_points={
+        "console_scripts": ["conkeyscan = conkeyscan.conkeyscan:entry_point"]
+    },
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+)
