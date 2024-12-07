6634107c83f6341f7098083647685899413349af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:00 +0000

    Add package rbms/0.1/rbms-0.1.tar.gz

---
+++ packages/rbms/rbms-0.1.tar.gz/rbms-0.1/docs/source/auto_examples/plot_test.py
@@ -0,0 +1,10 @@
+"""
+"This" is my example-script
+===========================
+
+This example doesn't do much, it just makes a simple plot
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [2, 1, 2])
