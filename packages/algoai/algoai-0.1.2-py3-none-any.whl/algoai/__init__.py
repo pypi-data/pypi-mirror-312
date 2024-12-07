07ad69012bcdcdf5cf7b849ba446929f97c9fe3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:23 +0000

    Add package algoai/0.1.2/algoai-0.1.2-py3-none-any.whl

---
+++ packages/algoai/algoai-0.1.2-py3-none-any.whl/algoai/__init__.py
@@ -0,0 +1,18 @@
+
+
+from .a_star import a_star_search
+from .alpha_beta import alpha_beta_pruning
+from .eight_puzzle import solve_puzzle
+from .backward_chaining import KnowledgeBase
+from .bayesian_network import bayesian_network
+from .bfs import breadth_first_search
+from .constraint_satisfaction_problem import map_coloring_csp
+from .cryptarithmetic import cryptarithmetic
+from .dfs import depth_first_search
+from .dpll_algo import dpll_algorithm
+from .forward_chaining import KnowledgeBase
+from .gbfs import greedy_best_first_search
+from .memory_bounded_heuristic import ida_star
+from .minmax import minimax_with_alpha_beta
+from .n_queens import n_queens_solver
+from .uniform_cost_search import uniform_cost_search
