1457dbb24d22ccf2447fb55b5efce5c06049d64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:15 +0000

    Add package sentry-dingtalks-notify/1.0.0/sentry_dingtalks_notify-1.0.0.tar.gz

---
+++ packages/sentry-dingtalks-notify/sentry_dingtalks_notify-1.0.0.tar.gz/sentry_dingtalks_notify-1.0.0/sentry_dingtalk_notify/__init__.py
@@ -0,0 +1,8 @@
+"""
+  @Project     : sentry-dingtalks-notify
+  @Time        : 2024/12/2 11:24:53
+  @File        : __init__.py
+  @Author      : damengzhang
+  @Software    : VSCode
+  @Desc        : 
+"""
