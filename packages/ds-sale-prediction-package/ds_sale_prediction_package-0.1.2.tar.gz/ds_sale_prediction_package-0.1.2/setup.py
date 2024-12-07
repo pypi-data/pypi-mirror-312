8a76eca19ed42c2a45dae0a6b5955f4eaf02658e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:22 +0000

    Add package ds-sale-prediction-package/0.1.2/ds_sale_prediction_package-0.1.2.tar.gz

---
+++ packages/ds-sale-prediction-package/ds_sale_prediction_package-0.1.2.tar.gz/ds_sale_prediction_package-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="ds_sale_prediction_package",
+    version="0.1.2",
+    description="A package for data preparation, feature extraction\
+    model training, and making prediction",
+    author="Aliona Hrynkevich",
+    packages=find_packages(),
+    install_requires=requirements,
+)
