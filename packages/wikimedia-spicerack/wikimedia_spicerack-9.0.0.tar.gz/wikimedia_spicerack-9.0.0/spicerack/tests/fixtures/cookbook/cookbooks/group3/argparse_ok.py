9aa8db7dfeea7d49fa24dc113c591cfdf70b072c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:36 +0000

    Add package wikimedia-spicerack/9.0.0/wikimedia_spicerack-9.0.0.tar.gz

---
+++ packages/wikimedia-spicerack/wikimedia_spicerack-9.0.0.tar.gz/wikimedia_spicerack-9.0.0/spicerack/tests/fixtures/cookbook/cookbooks/group3/argparse_ok.py
@@ -0,0 +1,13 @@
+"""Group3 argparse_ok."""
+import argparse
+
+__title__ = __doc__
+
+
+def argument_parser():
+    """As required by spicerack._cookbook."""
+    return argparse.ArgumentParser("group3.argparse_ok")
+
+
+def run(_args, _spicerack):
+    """As required by spicerack._cookbook."""
