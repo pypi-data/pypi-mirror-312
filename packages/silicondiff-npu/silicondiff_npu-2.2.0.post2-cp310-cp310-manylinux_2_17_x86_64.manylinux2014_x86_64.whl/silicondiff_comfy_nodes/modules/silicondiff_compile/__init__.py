d9dc2d02e8c7bb7c112702b6eac054c3f86a7f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:46 +0000

    Add package silicondiff-npu/2.2.0.post2/silicondiff_npu-2.2.0.post2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/silicondiff-npu/silicondiff_npu-2.2.0.post2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/silicondiff_comfy_nodes/modules/silicondiff_compile/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+from .hijack_comfyui_instantid import comfyui_instantid_hijacker
+from .hijack_ipadapter_plus import ipadapter_plus_hijacker
+from .hijack_pulid_comfyui import pulid_comfyui_hijacker
+from .hijack_samplers import samplers_hijack
+
+samplers_hijack.hijack(last=False)
+ipadapter_plus_hijacker.hijack(last=False)
+pulid_comfyui_hijacker.hijack(last=False)
+comfyui_instantid_hijacker.hijack(last=False)
