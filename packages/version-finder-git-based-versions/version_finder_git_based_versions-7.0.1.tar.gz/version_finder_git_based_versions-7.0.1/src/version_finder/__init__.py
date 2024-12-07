c1e160062f4e06fcfdea69c2b3d0acb678b3f424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:37 +0000

    Add package version-finder-git-based-versions/7.0.1/version_finder_git_based_versions-7.0.1.tar.gz

---
+++ packages/version-finder-git-based-versions/version_finder_git_based_versions-7.0.1.tar.gz/version_finder_git_based_versions-7.0.1/src/version_finder/__init__.py
@@ -0,0 +1,39 @@
+"""
+main package for version_finder
+"""
+
+from typing import List
+
+# version_finder/__init__.py
+from .protocols import LoggerProtocol, NullLogger
+from .core import VersionFinder, Commit, GitError, InvalidGitRepository, GitRepositoryNotClean, RepositoryNotTaskReady, InvalidCommitError, InvalidSubmoduleError, InvalidBranchError, GitNotInstalledError, VersionNotFoundError
+from .git_executer import GitConfig, GitCommandError, GitCommandExecutor
+from .logger.logger import setup_logger
+__version__ = "7.0.1"
+
+__all__: List[str] = [
+    '__version__',
+
+    # Git Executer
+    'GitCommandExecutor',
+    'GitConfig',
+    'GitCommandError',
+
+    # Core
+    'VersionFinder',
+    'Commit',
+    'GitError',
+    'InvalidGitRepository',
+    'GitRepositoryNotClean',
+    'RepositoryNotTaskReady',
+    'InvalidCommitError',
+    'InvalidSubmoduleError',
+    'InvalidBranchError',
+    'GitNotInstalledError',
+    'VersionNotFoundError',
+
+    # Logger
+    'LoggerProtocol',
+    'NullLogger',
+    'setup_logger',
+]
