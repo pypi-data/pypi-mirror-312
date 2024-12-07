7e50e7575d0e08f9f288dc84c3459fe90713bc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:07 +0000

    Add package taas-api-client/1.2.4/taas_api_client-1.2.4-py3-none-any.whl

---
+++ packages/taas-api-client/taas_api_client-1.2.4-py3-none-any.whl/taas_api/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Strategy(Enum):
+    IS = "IS"
+    TWAP = "TWAP"
+    VWAP = "VWAP"
+    LIMIT = "Limit"
+    MARKET = "Market"
+
+
+class Side(Enum):
+    BUY = "buy"
+    SELL = "sell"
+
+
+class PosSide(Enum):
+    LONG = "long"
+    SHORT = "short"
