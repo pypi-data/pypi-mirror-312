4198bd32e5d3e1b0d85fda57653e29fd3ecd360b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:13 +0000

    Add package npqtools/0.1.9/npqtools-0.1.9.tar.gz

---
+++ packages/npqtools/npqtools-0.1.9.tar.gz/npqtools-0.1.9/npqtools/__init__.py
@@ -0,0 +1,8 @@
+from .problems.Clique.QuboClique import QUBOClique
+from .problems.Salesman.QuboSalesman import QUBOSalesman
+from .problems.Hamiltonian.QuboHam import QUBOHam
+from .problems.Knapsack.QuboKnapsack import QUBOKnapsack
+from .problems.MaxWeightClique.QuboMaxWeightClique import QUBOMaxWeightClique
+
+
+
