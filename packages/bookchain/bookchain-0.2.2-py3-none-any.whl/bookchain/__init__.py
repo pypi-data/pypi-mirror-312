bd017257ed06c454e566482251b520055339a15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:15 +0000

    Add package bookchain/0.2.2/bookchain-0.2.2-py3-none-any.whl

---
+++ packages/bookchain/bookchain-0.2.2-py3-none-any.whl/bookchain/__init__.py
@@ -0,0 +1,63 @@
+from .models import (
+    Account,
+    AccountCategory,
+    AccountType,
+    Correspondence,
+    Currency,
+    Customer,
+    Entry,
+    EntryType,
+    Identity,
+    Ledger,
+    LedgerType,
+    Transaction,
+    Vendor,
+)
+from sqloquent import DeletedModel, Attachment
+import sqloquent.tools
+
+__version__ = '0.2.2'
+
+def set_connection_info(db_file_path: str):
+    """Set the connection info for all models to use the specified
+        sqlite3 database file path.
+    """
+    Account.connection_info = db_file_path
+    AccountCategory.connection_info = db_file_path
+    Correspondence.connection_info = db_file_path
+    Currency.connection_info = db_file_path
+    Customer.connection_info = db_file_path
+    Entry.connection_info = db_file_path
+    Identity.connection_info = db_file_path
+    Ledger.connection_info = db_file_path
+    Transaction.connection_info = db_file_path
+    Vendor.connection_info = db_file_path
+    DeletedModel.connection_info = db_file_path
+    Attachment.connection_info = db_file_path
+
+def publish_migrations(migration_folder_path: str):
+    """Writes migration files for the models."""
+    sqloquent.tools.publish_migrations(migration_folder_path)
+    models = [
+        Account,
+        AccountCategory,
+        Correspondence,
+        Currency,
+        Customer,
+        Entry,
+        Identity,
+        Ledger,
+        Transaction,
+        Vendor,
+        DeletedModel,
+        Attachment,
+    ]
+    for model in models:
+        name = model.__name__
+        m = sqloquent.tools.make_migration_from_model(model)
+        with open(f'{migration_folder_path}/create_{name}.py', 'w') as f:
+            f.write(m)
+
+def automigrate(migration_folder_path: str, db_file_path: str):
+    """Executes the sqloquent automigrate tool."""
+    sqloquent.tools.automigrate(migration_folder_path, db_file_path)
