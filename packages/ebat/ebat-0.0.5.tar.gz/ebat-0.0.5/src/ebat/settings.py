76f55499eb23aaed43f6eb85107e83f0c4e85716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:23 +0000

    Add package ebat/0.0.5/ebat-0.0.5.tar.gz

---
+++ packages/ebat/ebat-0.0.5.tar.gz/ebat-0.0.5/src/ebat/settings.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+PROJECT_ROOT = Path(os.path.abspath(os.path.dirname(__file__)))
+
+DATASET_LINKS = {
+    "medba": "https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/Behaviouralbiometrics/iot_dataset.zip"
+}
