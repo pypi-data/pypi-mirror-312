58746534ce9b179e05f67ac33ec38b4cf39ec7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:35:46 +0000

    Add package pynecil/2.0.0/pynecil-2.0.0-py3-none-any.whl

---
+++ packages/pynecil/pynecil-2.0.0-py3-none-any.whl/pynecil/__init__.py
@@ -0,0 +1,48 @@
+"""Pynecil - Python library to communicate with Pinecil V2 soldering irons via Bluetooth."""
+
+__version__ = "2.0.0"
+
+from .client import Pynecil, discover
+from .exceptions import CommunicationError
+from .types import (
+    AnimationSpeed,
+    AutostartMode,
+    BatteryType,
+    CharBulk,
+    CharLive,
+    CharSetting,
+    DeviceInfoResponse,
+    LanguageCode,
+    LiveDataResponse,
+    LockingMode,
+    LogoDuration,
+    OperatingMode,
+    PowerSource,
+    ScreenOrientationMode,
+    ScrollSpeed,
+    SettingsDataResponse,
+    TempUnit,
+)
+
+__all__ = [
+    "AnimationSpeed",
+    "AutostartMode",
+    "BatteryType",
+    "CharBulk",
+    "CharLive",
+    "CharSetting",
+    "CommunicationError",
+    "DeviceInfoResponse",
+    "discover",
+    "LanguageCode",
+    "LiveDataResponse",
+    "LockingMode",
+    "LogoDuration",
+    "OperatingMode",
+    "PowerSource",
+    "Pynecil",
+    "ScreenOrientationMode",
+    "ScrollSpeed",
+    "SettingsDataResponse",
+    "TempUnit",
+]
