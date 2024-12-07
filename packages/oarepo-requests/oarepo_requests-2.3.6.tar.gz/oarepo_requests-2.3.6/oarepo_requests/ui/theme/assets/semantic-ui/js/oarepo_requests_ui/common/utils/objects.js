4573ef126be05580a20cca551b376688bbe6cbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:29 +0000

    Add package oarepo-requests/2.3.6/oarepo_requests-2.3.6.tar.gz

---
+++ packages/oarepo-requests/oarepo_requests-2.3.6.tar.gz/oarepo_requests-2.3.6/oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/utils/objects.js
@@ -0,0 +1,13 @@
+export const REQUEST_TYPE = {
+  CREATE: 'create',
+  SUBMIT: 'submit',
+  CANCEL: 'cancel',
+  ACCEPT: 'accept',
+  DECLINE: 'decline',
+  SAVE: 'save',
+}
+
+export const REQUEST_MODAL_TYPE = {
+  READ_ONLY: 'read-only',
+  SUBMIT_FORM: 'submit-form',
+}
