64f2cced4ce49623877b0c830758e044ee12416e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:21 +0000

    Add package djing/0.6.1/djing-0.6.1.tar.gz

---
+++ packages/djing/djing-0.6.1.tar.gz/djing-0.6.1/djing/core/Http/Controllers/MetricController.pyi
@@ -0,0 +1,6 @@
+from djing.core.Http.Requests.MetricRequest import MetricRequest as MetricRequest
+from typing import Any
+
+class MetricController:
+    def show(self, request: MetricRequest) -> Any: ...
+    def detail(self, request: MetricRequest) -> Any: ...
