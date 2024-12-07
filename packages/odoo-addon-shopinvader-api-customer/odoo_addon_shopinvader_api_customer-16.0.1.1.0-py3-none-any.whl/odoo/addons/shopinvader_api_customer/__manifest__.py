56069a5cd8e3be37756d20e1f4b26d905aa0cf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:27 +0000

    Add package odoo-addon-shopinvader-api-customer/16.0.1.1.0/odoo_addon_shopinvader_api_customer-16.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-shopinvader-api-customer/odoo_addon_shopinvader_api_customer-16.0.1.1.0-py3-none-any.whl/odoo/addons/shopinvader_api_customer/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 ACSONE SA/NV,Odoo Community Association (OCA)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Api Customer",
+    "summary": """
+        Adds service to manage shopinvader customer""",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA),Odoo Community Association (OCA)",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": [
+        "extendable_fastapi",
+        "fastapi",
+        # Odoo
+        "mail",
+        "product",
+    ],
+    "data": [],
+    "demo": [],
+}
