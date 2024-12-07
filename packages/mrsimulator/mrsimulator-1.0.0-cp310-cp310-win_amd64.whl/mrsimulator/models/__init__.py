6002c2e3235a5b28140e88a3542e4c49b1eeffe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:56 +0000

    Add package mrsimulator/1.0.0/mrsimulator-1.0.0-cp310-cp310-win_amd64.whl

---
+++ packages/mrsimulator/mrsimulator-1.0.0-cp310-cp310-win_amd64.whl/mrsimulator/models/__init__.py
@@ -0,0 +1,5 @@
+from .czjzek import CzjzekDistribution  # noqa: F401
+from .czjzek import ExtCzjzekDistribution  # noqa: F401
+
+__author__ = "Deepansh J. Srivastava"
+__email__ = "srivastava.89@osu.edu"
