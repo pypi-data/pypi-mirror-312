3ec9ba6ad9e81f797d306d859d0b01c74824a81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:04:47 +0000

    Add package mathwork/1.1/mathwork-1.1.tar.gz

---
+++ packages/mathwork/mathwork-1.1.tar.gz/mathwork-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="mathwork",
+        version="1.1",
+        description="The great package.",
+        author="unknown",
+        packages=["mathwork"],
+        install_requires=[
+            'tensorflow', 'pandas', 'scikit-learn', 'torch', 'opencv-python',
+            'pillow', 'scikit-image', 'numpy', 'scipy',
+            'matplotlib', 'pywavelets', 'seaborn', 'torchvision', 'torchaudio',
+            'keras', 'transformers', 'tqdm', 'shutil'
+        ],
+        dependency_links=[
+            'git+https://github.com/facebookresearch/detectron2.git#egg=detectron2'
+        ],
+        zip_safe=False,
+    )
