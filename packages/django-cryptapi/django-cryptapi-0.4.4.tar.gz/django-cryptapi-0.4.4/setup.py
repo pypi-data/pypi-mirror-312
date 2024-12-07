fc6de55c04011c142fcdb1f8782cbbc576c6fd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:21 +0000

    Add package django-cryptapi/0.4.4/django-cryptapi-0.4.4.tar.gz

---
+++ packages/django-cryptapi/django-cryptapi-0.4.4.tar.gz/django-cryptapi-0.4.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import cryptapi
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+
+    name='django-cryptapi',
+    version=cryptapi.__version__,
+    packages=find_packages(exclude=['django_store']),
+    author="CryptAPI",
+    author_email="info@cryptapi.io",
+    install_requires=required,
+    description="Django implementation of CryptAPI's payment gateway",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+
+    include_package_data=True,
+
+    url='https://github.com/cryptapi/django-cryptapi',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+
+    license="MIT",
+
+    zip_safe=False,
+)
