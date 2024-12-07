295c53a34940cc8760553857438dc629a61125fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:00 +0000

    Add package chromadb/0.5.21/chromadb-0.5.21.tar.gz

---
+++ packages/chromadb/chromadb-0.5.21.tar.gz/chromadb-0.5.21/rust/worker/src/segment/mod.rs
@@ -0,0 +1,10 @@
+pub(crate) mod config;
+pub(crate) mod distributed_hnsw_segment;
+pub mod test;
+
+pub(crate) use types::*;
+
+// Required for benchmark
+pub mod metadata_segment;
+pub mod record_segment;
+pub mod types;
