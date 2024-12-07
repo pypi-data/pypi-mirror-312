390f793056ba57f06db5b8178fd0cc30397fae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:30 +0000

    Add package st-link-analysis-ie2/0.3.2/st_link_analysis_ie2-0.3.2.tar.gz

---
+++ packages/st-link-analysis-ie2/st_link_analysis_ie2-0.3.2.tar.gz/st_link_analysis_ie2-0.3.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="st_link_analysis_ie2",
+    version="0.3.2",
+    author="AlrasheedA, Dylan Lau",
+    description="A streamlit custom component for visualizing and interacting with graphs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["streamlit", "cytoscape", "graph", "link analysis"],
+    install_requires=["streamlit>=0.63"],
+    packages=find_packages(include=["st_link_analysis", "st_link_analysis.*"]),
+    package_data={
+        "st_link_analysis": [
+            "component/*.py",
+            "frontend/build/*",
+        ]
+    },
+    project_urls={
+        "Homepage": "https://github.com/AlrasheedA/st-link-analysis",
+        "Issues": "https://github.com/AlrasheedA/st-link-analysis/issues",
+    },
+    extras_require={
+        "dev": [
+            "ruff",
+            "pytest",
+        ]
+    },
+    entry_points={
+        "console_scripts": []
+    },
+)
