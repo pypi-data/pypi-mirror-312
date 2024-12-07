508b5f3e7e4e307029b8f8563d1454a5b925228a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:41 +0000

    Add package asam-qc-baselib/1.0.0/asam_qc_baselib-1.0.0-py3-none-any.whl

---
+++ packages/asam-qc-baselib/asam_qc_baselib-1.0.0-py3-none-any.whl/qc_baselib/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2024, ASAM e.V.
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+__version__ = "1.0.0rc1"
+
+
+from .configuration import Configuration as Configuration
+from .result import Result as Result
+from .models import IssueSeverity as IssueSeverity
+from .models import StatusType as StatusType
