2bd776008a69ce4c7a9a82918975b3bc440e8c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:04 +0000

    Add package zth-20241203/0.1.0/zth_20241203-0.1.0.tar.gz

---
+++ packages/zth-20241203/zth_20241203-0.1.0.tar.gz/zth_20241203-0.1.0/README.md
@@ -0,0 +1,7 @@
+# My Package
+A simple package that greets the user.
+
+## Installation
+
+```bash
+pip install my-package
---
+++ packages/zth-20241203/zth_20241203-0.1.0.tar.gz/zth_20241203-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zth-20241203',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='11136456',
+    author_email='zhaotianhao@vivo.com',
+    description='A simple greeting package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+
+)
