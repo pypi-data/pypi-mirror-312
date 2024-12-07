a202b84fa52419cea25cdac47000140e26af3142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:30 +0000

    Add package TAN-ENC/0.2/tan_enc-0.2.tar.gz

---
+++ packages/TAN-ENC/tan_enc-0.2.tar.gz/tan_enc-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.2',
+	name='TAN-ENC',
+	author='TAN-ENC',
+	description='TAN ENCODERS',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
