62b517d057a3c88a9871dcacfc6d880a660a3acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:21 +0000

    Add package fast-targetprice/0.0.5/fast_targetprice-0.0.5.tar.gz

---
+++ packages/fast-targetprice/fast_targetprice-0.0.5.tar.gz/fast_targetprice-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+setup(
+    name="fast_targetprice",  # Tên thư viện
+    version="0.0.5",  # Phiên bản thư viện
+    author="targetprice",  # Tên tác giả
+    packages=find_packages(), 
+    classifiers=[  
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    ext_modules=cythonize("fast_targetprice/*.py"), 
+    install_requires=["cython"],
+    python_requires='>=3.6',  # Yêu cầu phiên bản Python tối thiểu
+)
