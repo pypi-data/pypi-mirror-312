8e77c01c9c849b3a6cf1b237135651bd7e8cf810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:04:59 +0000

    Add package CASSIA/0.1.8/cassia-0.1.8.tar.gz

---
+++ packages/CASSIA/cassia-0.1.8.tar.gz/cassia-0.1.8/CASSIA/__init__.py
@@ -0,0 +1,2 @@
+from .main_function_code import *
+from .tools_function import *
---
+++ packages/CASSIA/cassia-0.1.8.tar.gz/cassia-0.1.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CASSIA",
+    version="0.1.8",
+    packages=find_packages(),
+    package_data={
+        'CASSIA': ['data/*.csv'],  # Include all CSV files in data directory
+    },
+    include_package_data=True,  # This tells setuptools to include package_data
+    install_requires=[
+        "numpy",
+        "pandas",
+        "openai>=1.0.0",
+        "anthropic>=0.3.0",
+        "requests>=2.25.0",
+    ],
+    author="Elliot Yixuan Xie",
+    author_email="xie227@wisc.edu",
+    description="CASSIA (Cell type Annotation using Specialized System with Integrated AI) is a Python package for automated cell type annotation in single-cell RNA sequencing data using large language models.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/elliotxe/CASSIA",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+)
+
