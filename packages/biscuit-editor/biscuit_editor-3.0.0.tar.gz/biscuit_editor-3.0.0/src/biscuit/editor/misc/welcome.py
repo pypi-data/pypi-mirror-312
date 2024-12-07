3b594a5a27bef6cd876d639d861426a69a3dd056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:24 +0000

    Add package biscuit-editor/3.0.0/biscuit_editor-3.0.0.tar.gz

---
+++ packages/biscuit-editor/biscuit_editor-3.0.0.tar.gz/biscuit_editor-3.0.0/src/biscuit/editor/misc/welcome.py
@@ -0,0 +1,114 @@
+import os
+import tkinter as tk
+
+from biscuit.common.icons import Icons
+from biscuit.common.ui import Frame, IconLabelButton, Label, LinkLabel, Shortcut
+from biscuit.editor.misc.quickitem import QuickItem, RecentItem
+
+from ..editorbase import BaseEditor
+
+
+class Welcome(BaseEditor):
+    name = "welcome"
+
+    def __init__(self, master, exists=False, editable=False, *args, **kwargs) -> None:
+        super().__init__(master, *args, **kwargs)
+        self.config(padx=100, pady=50, **self.base.theme.editors)
+
+        self.filename = "Welcome"
+
+        self.container = Frame(self, **self.base.theme.editors)
+        self.container.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+        # try:
+        #     self.logo = Label(
+        #         self.container,
+        #         image=self.base.settings.resources.logo.subsample(100, 100),
+        #         **self.base.theme.editors.biscuit_labels
+        #     )
+        #     self.logo.grid(row=0, column=0, sticky=tk.NSEW)
+        # except tk.TclError:
+        #     pass
+
+        self.title = Label(
+            self.container,
+            text="BISCUIT",
+            font=("Fira Code", int(60 * self.base.scale), "bold"),
+            fg=self.base.theme.biscuit,
+            **self.base.theme.editors.biscuit_labels
+        )
+        self.title.pack(fill=tk.BOTH, padx=20)
+
+        self.create_quick_group()
+
+        # TODO: make this optional
+        # self.create_recent_group()
+
+    def create_quick_group(self):
+        quick = Frame(self.container, **self.base.theme.editors)
+        quick.pack(fill=tk.BOTH, expand=True, pady=(20, 0))
+
+        QuickItem(
+            quick,
+            "New File",
+            Icons.NEW_FILE,
+            self.base.commands.new_file,
+            ["Ctrl", "N"],
+        ).pack(fill=tk.X, expand=True)
+
+        QuickItem(
+            quick,
+            "Open File...",
+            Icons.FOLDER_OPENED,
+            self.base.commands.open_file,
+            ["Ctrl", "O"],
+        ).pack(fill=tk.X, expand=True)
+
+        QuickItem(
+            quick,
+            "Open Folder...",
+            Icons.FOLDER,
+            self.base.commands.open_directory,
+            ["Ctrl", "Shift", "O"],
+        ).pack(fill=tk.X, expand=True)
+
+        # TODO add following shortcuts
+        QuickItem(
+            quick,
+            "Restore recent session",
+            Icons.HISTORY,
+            self.base.commands.restore_recent_session,
+            ["Ctrl", "Alt", "R"],
+        ).pack(fill=tk.X, expand=True)
+
+        QuickItem(
+            quick,
+            "Config",
+            Icons.SETTINGS,
+            self.base.commands.open_settings,
+            ["Ctrl", ","],
+        ).pack(fill=tk.X, expand=True)
+
+        QuickItem(
+            quick,
+            "Extensions",
+            Icons.EXTENSIONS,
+            self.base.commands.show_extensions,
+            ["Ctrl", "Shift", "X"],
+        ).pack(fill=tk.X, expand=True)
+
+    def create_recent_group(self):
+        Label(
+            self.container,
+            text="Recently opened",
+            font=("Fira Code", 15),
+            **self.base.theme.editors.labels
+        ).pack(pady=(40, 0), anchor=tk.W)
+        recents = Frame(self.container, **self.base.theme.editors)
+        recents.pack(fill=tk.BOTH, expand=True, padx=5)
+        recents.grid_columnconfigure(0, weight=1)
+
+        for i, p in enumerate(self.base.history.folder_history.list):
+            RecentItem(recents, os.path.basename(p[0]), p[1]).grid(
+                row=i, column=0, sticky=tk.EW, pady=2
+            )
---
+++ packages/biscuit-editor/biscuit_editor-3.0.0.tar.gz/biscuit_editor-3.0.0/src/biscuit/views/extensions/extension.py
@@ -0,0 +1,173 @@
+from __future__ import annotations
+
+import os
+import tkinter as tk
+import typing
+
+from biscuit.common.ui import Button, Frame, Label
+
+if typing.TYPE_CHECKING:
+    from .results import Results
+
+
+class ExtensionGUI(Frame):
+    """Extension item in the Extensions view.
+
+    The Extension class represents an extension item in the Extensions view.
+    """
+
+    def __init__(
+        self, master: Results, name: str, data: list[str], *args, **kwargs
+    ) -> None:
+        """Initialize the Extension class.
+
+        Args:
+            master (tk.Tk): The root window.
+            name (str): The name of the extension.
+            data (list): The extension data."""
+
+        super().__init__(master, *args, **kwargs)
+        self.master: Results = master
+        self.config(**self.base.theme.views.sidebar.item)
+        self.selected = False
+        self.manager = self.base.extensions_manager
+
+        self.data = data
+        self.name = name
+        self.filename = data[0]
+        self.file = os.path.join(self.base.extensiondir, data[0])
+        self.author = data[1]
+        self.description = data[2][:50] + "..." if len(data[2]) > 30 else data[2]
+
+        self.url = f"{self.manager.repo_url}extensions/{data[0]}"
+        self.installed = os.path.isfile(self.file)
+
+        self.bg = self.base.theme.views.sidebar.item.background
+        self.hbg = self.base.theme.views.sidebar.item.highlightbackground
+
+        self.container = Frame(self, padx=10, pady=10)
+        self.container.pack(fill=tk.BOTH, expand=True)
+
+        self.namelbl = Label(
+            self.container,
+            text=name[0].upper() + name[1:],
+            font=("Segoi UI", 11, "bold"),
+            anchor=tk.W,
+            **self.base.theme.views.sidebar.item.content,
+        )
+        self.namelbl.pack(fill=tk.X)
+
+        self.descriptionlbl = Label(
+            self.container,
+            text=self.description,
+            font=("Segoi UI", 9),
+            anchor=tk.W,
+            **self.base.theme.views.sidebar.item.content,
+        )
+        self.descriptionlbl.config(fg="grey")
+        self.descriptionlbl.pack(fill=tk.X, expand=True)
+
+        self.subcontainer = Frame(self.container)
+        self.subcontainer.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
+
+        self.authorlbl = Label(
+            self.subcontainer,
+            text=f"@{self.author}",
+            font=("Segoi UI", 7, "bold"),
+            anchor=tk.W,
+            **self.base.theme.views.sidebar.item.content,
+        )
+        self.authorlbl.config(fg="grey")
+        self.authorlbl.pack(side=tk.LEFT, fill=tk.X)
+
+        self.install = Button(
+            self.subcontainer,
+            "Install",
+            self.run_fetch_extension,
+            padx=10,
+        )
+        self.install.config(font=("Segoi UI", 8), pady=2)
+        self.install.pack(side=tk.RIGHT, fill=tk.X)
+        if self.installed:
+            self.install.config(text="Installed", bg=self.base.theme.biscuit_dark)
+            self.install.set_command(self.remove_extension)
+
+        self.bind("<Button-1>", self.set_selected)
+        self.namelbl.bind("<Button-1>", self.set_selected)
+        self.descriptionlbl.bind("<Button-1>", self.set_selected)
+        self.authorlbl.bind("<Button-1>", self.set_selected)
+        self.container.bind("<Button-1>", self.set_selected)
+        self.subcontainer.bind("<Button-1>", self.set_selected)
+
+        self.bind("<Enter>", self.hoverin)
+        self.bind("<Leave>", self.hoveroff)
+        self.hoveroff()
+
+    def remove_extension(self, *_):
+        self.manager.remove_extension(self)
+
+    def run_fetch_extension(self, *_):
+        self.manager.run_fetch_extension(self)
+
+    def set_unavailable(self):
+        self.install.config(text="Unavailable", bg=self.base.theme.biscuit_dark)
+
+    def set_installed(self):
+        self.install.config(text="Installed", bg=self.base.theme.biscuit_dark)
+        self.install.set_command(self.remove_extension)
+
+    def set_uninstalled(self):
+        self.install.config(text="Install", bg=self.base.theme.biscuit)
+        self.install.set_command(self.run_fetch_extension)
+
+    def set_fetching(self):
+        self.install.config(text="Fetching...", bg=self.base.theme.biscuit_dark)
+
+    def set_selected(self, *_):
+        self.master.set_selected(self)
+
+    def select(self):
+        self.selected = True
+        self.config(bg=self.hbg)
+        self.namelbl.config(bg=self.hbg)
+        self.authorlbl.config(bg=self.hbg)
+        self.descriptionlbl.config(bg=self.hbg)
+        self.container.config(bg=self.hbg)
+        self.subcontainer.config(bg=self.hbg)
+
+    def deselect(self):
+        self.selected = False
+        self.config(bg=self.bg)
+        self.namelbl.config(bg=self.bg)
+        self.authorlbl.config(bg=self.bg)
+        self.descriptionlbl.config(bg=self.bg)
+        self.container.config(bg=self.bg)
+        self.subcontainer.config(bg=self.bg)
+
+    def hoverin(self, *_) -> None:
+        if self.selected:
+            return
+
+        try:
+            self.config(bg=self.hbg)
+            self.namelbl.config(bg=self.hbg)
+            self.authorlbl.config(bg=self.hbg)
+            self.descriptionlbl.config(bg=self.hbg)
+            self.container.config(bg=self.hbg)
+            self.subcontainer.config(bg=self.hbg)
+        except:
+            pass
+
+    def hoveroff(self, *_) -> None:
+        if self.selected:
+            return
+
+        try:
+            self.config(bg=self.bg)
+            self.namelbl.config(bg=self.bg)
+            self.authorlbl.config(bg=self.bg)
+            self.descriptionlbl.config(bg=self.bg)
+            self.container.config(bg=self.bg)
+            self.subcontainer.config(bg=self.bg)
+        except:
+            pass
