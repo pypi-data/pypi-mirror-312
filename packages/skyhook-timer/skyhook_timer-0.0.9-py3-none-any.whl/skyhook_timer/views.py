791a162a61e5acbbf6e9a84eaf32e204f56bebb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:03 +0000

    Add package skyhook-timer/0.0.9/skyhook_timer-0.0.9-py3-none-any.whl

---
+++ packages/skyhook-timer/skyhook_timer-0.0.9-py3-none-any.whl/skyhook_timer/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import Group
+from .models import SkyhookTimer
+from allianceauth.authentication.decorators import state_required
+
+@state_required(['Member'])
+def skyhook_timer_view(request):
+    # Get all timers for the member to view
+    timers = SkyhookTimer.objects.all()
+
+    # Render the view, allow "Members" to see but not interact with the data
+    return render(request, 'skyhook_timer/view_timers.html', {'timers': timers})
