48f9a6b5cbbd513986a2e4d125c6faaa523fb3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:41 +0000

    Add package oarepo-requests/2.3.7/oarepo_requests-2.3.7-py3-none-any.whl

---
+++ packages/oarepo-requests/oarepo_requests-2.3.7-py3-none-any.whl/oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/actions/index.js
@@ -0,0 +1,68 @@
+import { REQUEST_TYPE } from "@js/oarepo_requests_common";
+import Accept from "./Accept";
+import Decline from "./Decline";
+import Cancel from "./Cancel";
+import Create from "./Create";
+import Submit from "./Submit";
+import ModalCreateAndSubmit from "./ModalCreateAndSubmit";
+import DirectCreateAndSubmit from "./DirectCreateAndSubmit";
+
+export const mapLinksToActions = (
+  requestOrRequestType,
+  customFields,
+  extraData
+) => {
+  const hasLongForm = extraData?.editable;
+
+  const actionComponents = [];
+  for (const actionKey of Object.keys(requestOrRequestType.links?.actions)) {
+    switch (actionKey) {
+      case REQUEST_TYPE.ACCEPT:
+        actionComponents.push({
+          name: REQUEST_TYPE.ACCEPT,
+          component: Accept,
+        });
+        actionComponents.push({
+          name: REQUEST_TYPE.DECLINE,
+          component: Decline,
+        });
+        break;
+      case REQUEST_TYPE.CANCEL:
+        actionComponents.push({
+          name: REQUEST_TYPE.CANCEL,
+          component: Cancel,
+        });
+        break;
+      case REQUEST_TYPE.CREATE:
+        // requestOrRequestType is requestType here
+        if (customFields?.ui && hasLongForm) {
+          actionComponents.push({
+            name: REQUEST_TYPE.SAVE,
+            component: Create,
+          });
+        }
+        actionComponents.push({
+          name: REQUEST_TYPE.SUBMIT,
+          component: ModalCreateAndSubmit,
+        });
+        break;
+      case REQUEST_TYPE.SUBMIT:
+        actionComponents.push({
+          name: REQUEST_TYPE.SUBMIT,
+          component: Submit,
+        });
+        if (customFields?.ui) {
+          actionComponents.push({
+            name: REQUEST_TYPE.SAVE,
+            component: Create,
+          });
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  return actionComponents;
+};
+
+export { DirectCreateAndSubmit };
