aeef95359f13c6cdc349342c827ce998cd4b4c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:00 +0000

    Add package PyLocalizer/0.0.1/pylocalizer-0.0.1.tar.gz

---
+++ packages/PyLocalizer/pylocalizer-0.0.1.tar.gz/pylocalizer-0.0.1/src/PyLocalizer/All.py
@@ -0,0 +1,13 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+A small library with a bunch of functions and thingies I made for fun.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from . import internal
+
+from .EntryFormatting import *
+
+from .WebLocalization import *
