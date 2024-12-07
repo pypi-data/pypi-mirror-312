63d07d07d9d4e2aeab981edb44d949508e8beb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:23 +0000

    Add package flowable.external-worker-client/1.0.2/flowable.external_worker_client-1.0.2-py3-none-any.whl

---
+++ packages/flowable.external-worker-client/flowable.external_worker_client-1.0.2-py3-none-any.whl/flowable/external_worker_client/__init__.py
@@ -0,0 +1,110 @@
+import sys
+import threading
+import time
+from os import getpid
+from socket import gethostname
+from typing import Callable, Union
+
+from requests import Session
+from requests.auth import AuthBase
+
+from flowable.external_worker_client.engine_rest_variable import EngineRestVariable
+from flowable.external_worker_client.external_worker_acquire_job_response import ExternalWorkerAcquireJobResponse
+from flowable.external_worker_client.restclient import FlowableExternalWorkerRestClient
+from flowable.external_worker_client.worker_result import WorkerResultSuccess, WorkerResultFailure, WorkerResultBpmnError, WorkerResultCmmnTerminate, WorkResult
+
+
+class WorkerResultBuilder(object):
+
+    def __init__(self, job: ExternalWorkerAcquireJobResponse):
+        self._job = job
+
+    def success(self) -> WorkerResultSuccess:
+        return WorkerResultSuccess(self._job)
+
+    def failure(self) -> WorkerResultFailure:
+        return WorkerResultFailure(self._job)
+
+    def cmmn_terminate(self) -> WorkerResultCmmnTerminate:
+        return WorkerResultCmmnTerminate(self._job)
+
+    def bpmn_error(self) -> WorkerResultBpmnError:
+        return WorkerResultBpmnError(self._job)
+
+
+class ExternalWorkerSubscription(object):
+
+    def __init__(self, thread: threading.Thread, event: threading.Event):
+        self.thread = thread
+        self.event = event
+
+    def unsubscribe(self, timeout: float = None):
+        self.event.set()
+        self.thread.join(timeout)
+
+
+CallbackHandlerType = Union[
+    Callable[[ExternalWorkerAcquireJobResponse, WorkerResultBuilder], WorkResult],
+    Callable[[ExternalWorkerAcquireJobResponse, WorkerResultBuilder], None]
+]
+
+
+class ExternalWorkerClient(object):
+
+    def __init__(
+            self,
+            flowable_host: str = "https://trial.flowable.com/work",
+            worker_id: str = None,
+            auth: AuthBase = None,
+            customize_session: Callable[[Session], None] = lambda session: None
+    ) -> None:
+        self._restClient = FlowableExternalWorkerRestClient(
+            flowable_host=flowable_host,
+            worker_id=worker_id or gethostname() + '-' + str(getpid()),
+            auth=auth,
+            customize_session=customize_session
+        )
+
+    def subscribe(
+            self,
+            topic,
+            callback_handler: CallbackHandlerType,
+            lock_duration: str = "PT1M",
+            number_of_retries: int = 5,
+            scope_type: str = None,
+            wait_period_seconds: int = 30,
+            number_of_tasks: int = 1
+    ) -> ExternalWorkerSubscription:
+        event = threading.Event()
+        thread = threading.Thread(target=self._consume,
+                                  args=(event, topic, callback_handler, lock_duration, number_of_retries, scope_type, wait_period_seconds, number_of_tasks))
+        thread.start()
+        return ExternalWorkerSubscription(thread, event)
+
+    def _consume(
+            self,
+            event: threading.Event,
+            topic: str,
+            callback_handler: CallbackHandlerType,
+            lock_duration: str,
+            number_of_retries: int,
+            scope_type: str,
+            wait_period_seconds: int,
+            number_of_tasks: int
+    ):
+        while not event.is_set():
+            jobs = self._restClient.acquire_jobs(topic, lock_duration, number_of_tasks, number_of_retries=number_of_retries, scope_type=scope_type)
+            if len(jobs) == 0:
+                time.sleep(wait_period_seconds)
+
+            for job in jobs:
+                try:
+                    result = callback_handler(job, WorkerResultBuilder(job))
+                    if result is not None:
+                        result.execute(self._restClient)
+                    else:
+                        self._restClient.complete_job(job.id)
+                except Exception as e:
+                    print("An error occurred during job execution " + job.id,  file=sys.stderr)
+                    print(e,  file=sys.stderr)
+                    self._restClient.fail_job(job.id, e.__str__())
---
+++ packages/flowable.external-worker-client/flowable.external_worker_client-1.0.2-py3-none-any.whl/flowable/external_worker_client/restclient.py
@@ -0,0 +1,153 @@
+from typing import Callable
+
+import requests
+from requests import Session
+from requests.auth import AuthBase
+
+from flowable.external_worker_client import response_converter
+from flowable.external_worker_client.engine_rest_variable import EngineRestVariable
+from flowable.external_worker_client.external_worker_acquire_job_response import ExternalWorkerAcquireJobResponse
+from flowable.external_worker_client.external_worker_job_response import ExternalWorkerJobResponse
+from flowable.external_worker_client.flowable_rest_exception import FlowableRestException
+from flowable.external_worker_client.list_result import ListResult
+from flowable.external_worker_client.request_converter import convert_from_engine_rest_variable
+
+JOB_API = '/external-job-api'
+
+
+class FlowableExternalWorkerRestClient(object):
+    """
+    Create a REST API Client for the Flowable External Worker.
+    Used to communicate with the REST API and exchange information.
+    """
+
+    def __init__(
+            self,
+            flowable_host: str,
+            worker_id: str,
+            auth: AuthBase = None,
+            customize_session: Callable[[Session], None] = lambda session: None
+    ) -> None:
+        self.flowable_host: str = flowable_host
+        if self.flowable_host.endswith('/'):
+            self.flowable_host = flowable_host[:-1]
+        self.worker_id: str = worker_id
+        self.request_session: Session = requests.Session()
+        if auth is not None:
+            self.request_session.auth = auth
+        if customize_session is not None:
+            customize_session(self.request_session)
+
+    def list_jobs(self) -> ListResult[ExternalWorkerJobResponse]:
+        r = self.request_session.get(self.flowable_host + JOB_API + '/jobs')
+
+        if r.status_code != 200:
+            raise FlowableRestException(r.status_code, r.text)
+
+        result = r.json()
+        jobs = list(map(response_converter.convert_to_external_worker_job_response, result.get('data')))
+
+        return ListResult(
+            jobs,
+            result.get('total'),
+            result.get('start'),
+            result.get('sort'),
+            result.get('order'),
+            result.get('size')
+        )
+
+    def get_job(self, job_id: str) -> ExternalWorkerJobResponse:
+        r = self.request_session.get(self.flowable_host + JOB_API + "/jobs/" + job_id)
+        if r.status_code != 200:
+            raise FlowableRestException(r.status_code, r.text)
+
+        return response_converter.convert_to_external_worker_job_response(r.json())
+
+    def acquire_jobs(
+            self,
+            topic: str,
+            lock_duration: str,
+            number_of_tasks: int = 1,
+            number_of_retries: int = 5,
+            worker_id: str = None,
+            scope_type: str = None
+    ) -> list[ExternalWorkerAcquireJobResponse]:
+        r = self.request_session.post(
+            self.flowable_host + JOB_API + '/acquire/jobs',
+            json={
+                "topic": topic,
+                "lockDuration": lock_duration,
+                "numberOfTasks": number_of_tasks,
+                "numberOfRetries": number_of_retries,
+                "workerId": worker_id or self.worker_id,
+                "scopeType": scope_type
+            },
+            headers={
+                "Content-Type": "application/json"
+            }
+        )
+        if r.status_code != 200:
+            print(r.text)
+            raise FlowableRestException(r.status_code, r.text)
+
+        return list(map(response_converter.convert_to_external_worker_acquire_job_response, r.json()))
+
+    def complete_job(self, job_id: str, variables: list[EngineRestVariable] = None, worker_id: str = None) -> None:
+        request_variables = list(map(convert_from_engine_rest_variable, variables or []))
+
+        r = self.request_session.post(
+            self.flowable_host + JOB_API + '/acquire/jobs/' + job_id + '/complete',
+            json={
+                "workerId": worker_id or self.worker_id,
+                "variables": request_variables
+            },
+            headers={
+                "Content-Type": "application/json"
+            }
+        )
+        if r.status_code != 204:
+            raise FlowableRestException(r.status_code, r.text)
+
+    def job_with_bpmn_error(self, job_id: str, variables: list[EngineRestVariable] = None, error_code: str = None, worker_id: str = None) -> None:
+        request_variables = list(map(convert_from_engine_rest_variable, variables or []))
+
+        r = self.request_session.post(
+            self.flowable_host + JOB_API + '/acquire/jobs/' + job_id + '/bpmnError',
+            json={
+                "workerId": worker_id or self.worker_id,
+                "variables": request_variables,
+                "errorCode": error_code
+            },
+            headers={
+                "Content-Type": "application/json"
+            }
+        )
+        if r.status_code != 204:
+            raise FlowableRestException(r.status_code, r.text)
+
+    def fail_job(self, job_id: str, error_message: str = None, error_details: str = None, retries: int = None, retry_timeout: str = None, worker_id: str = None) -> None:
+        r = self.request_session.post(
+            self.flowable_host + JOB_API + '/acquire/jobs/' + job_id + '/fail',
+            json={
+                "workerId": worker_id or self.worker_id,
+                "errorMessage": error_message,
+                "errorDetails": error_details,
+                "retries": retries,
+                "retryTimeout": retry_timeout
+            }
+        )
+        if r.status_code != 204:
+            raise FlowableRestException(r.status_code, r.text)
+
+    def job_with_cmmn_terminate(self, job_id: str, variables: list[EngineRestVariable], worker_id: str = None):
+        request_variables = list(map(convert_from_engine_rest_variable, variables or []))
+        r = self.request_session.post(
+            self.flowable_host + JOB_API + '/acquire/jobs/' + job_id + '/cmmnTerminate',
+            json={
+                "workerId": worker_id or self.worker_id,
+                "variables": request_variables
+            }
+        )
+        if r.status_code != 204:
+            raise FlowableRestException(r.status_code, r.text)
+
