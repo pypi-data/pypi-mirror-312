8dc5ce09bc23fc655531c96f018ba841ba61bd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:42 +0000

    Add package indico-plugin-vc-zoom/3.3.1/indico_plugin_vc_zoom-3.3.1-py3-none-any.whl

---
+++ packages/indico-plugin-vc-zoom/indico_plugin_vc_zoom-3.3.1-py3-none-any.whl/indico_vc_zoom/cli.py
@@ -0,0 +1,36 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2020 - 2024 CERN and ENEA
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+import click
+from terminaltables import AsciiTable
+
+from indico.cli.core import cli_group
+from indico.modules.vc.models.vc_rooms import VCRoom, VCRoomStatus
+
+
+@cli_group(name='zoom')
+def cli():
+    """Manage the Zoom plugin."""
+
+
+@cli.command()
+@click.option('--status', type=click.Choice(['deleted', 'created']))
+def rooms(status=None):
+    """Lists all Zoom rooms"""
+
+    room_query = VCRoom.query.filter_by(type='zoom')
+    table_data = [['ID', 'Name', 'Status', 'Zoom ID']]
+
+    if status:
+        room_query = room_query.filter(VCRoom.status == VCRoomStatus.get(status))
+
+    table_data.extend([str(room.id), room.name, room.status.name, str(room.data['zoom_id'])] for room in room_query)
+
+    table = AsciiTable(table_data)
+    for col in (0, 3, 4):
+        table.justify_columns[col] = 'right'
+    print(table.table)
