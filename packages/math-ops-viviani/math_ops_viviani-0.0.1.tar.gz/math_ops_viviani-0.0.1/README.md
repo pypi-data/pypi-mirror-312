76869cc6f229602eca7e581e3e0e9358b0cccdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:34 +0000

    Add package math-ops-viviani/0.0.1/math_ops_viviani-0.0.1.tar.gz

---
+++ packages/math-ops-viviani/math_ops_viviani-0.0.1.tar.gz/math_ops_viviani-0.0.1/README.md
@@ -0,0 +1,19 @@
+# math_operations
+
+Description. 
+
+The package math_operations is used to perform basic mathematical operations such as addition, subtraction, multiplication, division, and exponentiation.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install math_operations
+
+```bash
+pip install math_ops_viviani
+```
+
+## Author
+Viviani Pedroso
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/math-ops-viviani/math_ops_viviani-0.0.1.tar.gz/math_ops_viviani-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="math_ops_viviani",
+    version="0.0.1",
+    author="Viviani Pedroso",
+    author_email="viani22@gmail.com",
+    description="The package math_operations",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vivianikelly/math-operations-package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
