5121c4c94beecf90f35ba59f1c3aa9357d51baaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:30 +0000

    Add package uhg/0.1.11/uhg-0.1.11-py3-none-any.whl

---
+++ packages/uhg/uhg-0.1.11-py3-none-any.whl/uhg/manifolds/__init__.py
@@ -0,0 +1,5 @@
+from .base import Manifold, ScalingInfo
+from .lorentz import LorentzManifold
+from .siegel import SiegelManifold
+
+__all__ = ["Manifold", "ScalingInfo", "LorentzManifold", "SiegelManifold"]
---
+++ packages/uhg/uhg-0.1.11-py3-none-any.whl/uhg/samplers/__init__.py
@@ -0,0 +1,8 @@
+from .hyperbolic import HyperbolicSampler, HyperbolicHMC
+from .langevin import HyperbolicLangevin
+
+__all__ = [
+    'HyperbolicSampler',
+    'HyperbolicHMC',
+    'HyperbolicLangevin'
+]
