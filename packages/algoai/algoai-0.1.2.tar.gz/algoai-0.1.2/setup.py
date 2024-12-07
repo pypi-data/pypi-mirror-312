99ad62ef5029b4983f441ba6d1a710e343aedc8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:23 +0000

    Add package algoai/0.1.2/algoai-0.1.2.tar.gz

---
+++ packages/algoai/algoai-0.1.2.tar.gz/algoai-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algoai',  
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[  
+        'numpy',   
+        'networkx', 
+        'pgmpy',
+        
+    ],
+    author='Leander',
+    author_email='leander.antony2023@gmail.com',
+    description='A collection of AI algorithms',
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+)
