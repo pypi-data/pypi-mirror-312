1c232476bd8abbd12480008018b1fa6d31d622be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:16 +0000

    Add package dbt-snowflake/1.9.0rc1/dbt_snowflake-1.9.0rc1-py3-none-any.whl

---
+++ packages/dbt-snowflake/dbt_snowflake-1.9.0rc1-py3-none-any.whl/dbt/adapters/snowflake/relation_configs/__init__.py
@@ -0,0 +1,17 @@
+from dbt.adapters.snowflake.relation_configs.catalog import (
+    SnowflakeCatalogConfig,
+    SnowflakeCatalogConfigChange,
+)
+from dbt.adapters.snowflake.relation_configs.dynamic_table import (
+    SnowflakeDynamicTableConfig,
+    SnowflakeDynamicTableConfigChangeset,
+    SnowflakeDynamicTableRefreshModeConfigChange,
+    SnowflakeDynamicTableWarehouseConfigChange,
+    SnowflakeDynamicTableTargetLagConfigChange,
+)
+from dbt.adapters.snowflake.relation_configs.formats import TableFormat
+from dbt.adapters.snowflake.relation_configs.policies import (
+    SnowflakeIncludePolicy,
+    SnowflakeQuotePolicy,
+    SnowflakeRelationType,
+)
