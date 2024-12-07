d738dbb709bba07b2823359e04323df2ceddb9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:47 +0000

    Add package xnote-web/0.1.3/xnote-web-0.1.3.tar.gz

---
+++ packages/xnote-web/xnote-web-0.1.3.tar.gz/xnote-web-0.1.3/xnote/core/xnote_event.py
@@ -0,0 +1,36 @@
+# encoding=utf-8
+from xutils import Storage
+
+class FileUploadEvent(Storage):
+    """文件上传事件"""
+
+    def __init__(self):
+        super().__init__()
+        self.user_name = ""
+        self.user_id = 0
+        self.fpath = ""
+        self.remark = ""
+
+
+class FileDeleteEvent(FileUploadEvent):
+    """文件删除事件"""
+    pass
+
+class FileRenameEvent(Storage):
+    """文件重命名事件"""
+    def __init__(self):
+        super().__init__()
+        self.user_name = ""
+        self.user_id = 0
+        self.fpath = ""
+        self.old_fpath = ""
+
+class NoteViewEvent(Storage):
+    """笔记访问事件"""
+    def __init__(self, id=0, user_name="", user_id=0):
+        super().__init__()
+        self.id = id
+        self.user_name = user_name
+        self.user_id = user_id
+
+
