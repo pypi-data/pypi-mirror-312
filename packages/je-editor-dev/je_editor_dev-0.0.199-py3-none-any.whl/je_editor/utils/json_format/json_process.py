41278a9cfd94564ab439d03308c08978aaed9e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:05 +0000

    Add package je-editor-dev/0.0.199/je_editor_dev-0.0.199-py3-none-any.whl

---
+++ packages/je-editor-dev/je_editor_dev-0.0.199-py3-none-any.whl/je_editor/utils/json_format/json_process.py
@@ -0,0 +1,29 @@
+import json.decoder
+import sys
+from json import dumps
+from json import loads
+
+from je_editor.utils.exception.exception_tags import cant_reformat_json_error
+from je_editor.utils.exception.exception_tags import wrong_json_data_error
+from je_editor.utils.exception.exceptions import JEditorJsonException
+
+
+def __process_json(json_string: str, **kwargs) -> str:
+    try:
+        return dumps(loads(json_string), indent=4, sort_keys=True, **kwargs)
+    except json.JSONDecodeError as error:
+        print(wrong_json_data_error, file=sys.stderr)
+        raise error
+    except TypeError:
+        try:
+            return dumps(json_string, indent=4, sort_keys=True, **kwargs)
+        except TypeError:
+            raise JEditorJsonException(wrong_json_data_error)
+
+
+def reformat_json(json_string: str, **kwargs) -> str:
+    # Make json pretty
+    try:
+        return __process_json(json_string, **kwargs)
+    except JEditorJsonException:
+        raise JEditorJsonException(cant_reformat_json_error)
