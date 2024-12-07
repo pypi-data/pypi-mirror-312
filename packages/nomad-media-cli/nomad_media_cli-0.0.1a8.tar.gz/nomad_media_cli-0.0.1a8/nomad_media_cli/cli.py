5f1ec028b9b08ab70da794cd4e9b716244ce6b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:04:55 +0000

    Add package nomad-media-cli/0.0.1a8/nomad_media_cli-0.0.1a8.tar.gz

---
+++ packages/nomad-media-cli/nomad_media_cli-0.0.1a8.tar.gz/nomad_media_cli-0.0.1a8/nomad_media_cli/cli.py
@@ -0,0 +1,35 @@
+import click
+
+import os
+from platformdirs import user_config_dir
+
+from commands.init import init
+from commands.list_assets import list_assets
+from commands.list_buckets import list_buckets
+from commands.list_config_path import list_config_path
+from commands.login import login
+from commands.set_bucket import set_bucket
+from commands.update_config import update_config
+
+# Set the configuration directory and path
+CONFIG_DIR = user_config_dir("nomad_media_cli")
+CONFIG_PATH = os.path.join(CONFIG_DIR, "config.json")
+
+@click.group()
+@click.option("--config-path", default=CONFIG_PATH, help="Path to the configuration file (optional)")
+@click.pass_context
+def cli(ctx, config_path):
+    """Nomad Media CLI"""
+    ctx.ensure_object(dict)
+    ctx.obj["config_path"] = config_path    
+    
+cli.add_command(init)
+cli.add_command(login)
+cli.add_command(list_assets)
+cli.add_command(list_buckets)
+cli.add_command(list_config_path)
+cli.add_command(set_bucket)
+cli.add_command(update_config)
+
+if __name__ == "__main__":
+    cli(obj={})
