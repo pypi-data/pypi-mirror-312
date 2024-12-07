6536f47f91bbecd61b8f7364b8c9690dbd504748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:17 +0000

    Add package Bloomerp/0.1.5/bloomerp-0.1.5.tar.gz

---
+++ packages/Bloomerp/bloomerp-0.1.5.tar.gz/bloomerp-0.1.5/bloomerp/widgets/multiple_model_select_widget.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.forms.models import modelform_factory
+from django.db.models import Model
+from django.contrib.contenttypes.models import ContentType
+from bloomerp.models import ApplicationField
+
+class MultipleModelSelect(forms.SelectMultiple):
+    template_name = 'widgets/foreign_field_widget.html'
+
+    def __init__(self, model: Model, *args, **kwargs):
+        self.model = model
+        super().__init__(*args, **kwargs)
+    
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        # Create new form in case user wants to update the foreign key
+        Form = modelform_factory(self.model, fields='__all__')
+        
+        # Get first 5 objects from the model
+        context['objects'] = self.model.objects.all()[:5]
+        context['content_type_id'] = ContentType.objects.get_for_model(self.model).id
+        context['form'] = Form()
+
+        # Get the object(s) that is currently selected
+        if context['widget']['value']:
+            values : str = context['widget']['value']
+            selected_choices = self.model.objects.filter(pk__in=values)
+            context['selected_choices'] = selected_choices
+
+        # Check if an invalid entry was made
+        if attrs.get('aria-invalid', 'false') == 'true':
+            context['invalid'] = True
+
+        # Add application fields to the context
+        context['application_fields'] = ApplicationField.objects.filter(content_type=ContentType.objects.get_for_model(self.model)).exclude(field_type='Property')
+
+        context['widget_type'] = 'm2m'
+        return context
+    
+        
+        
+
+
