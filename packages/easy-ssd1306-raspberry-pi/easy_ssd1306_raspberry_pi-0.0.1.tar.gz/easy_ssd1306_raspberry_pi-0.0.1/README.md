6fc35852da8cc895b2a08d885edb61f7edc41dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:35:50 +0000

    Add package easy-ssd1306-raspberry-pi/0.0.1/easy_ssd1306_raspberry_pi-0.0.1.tar.gz

---
+++ packages/easy-ssd1306-raspberry-pi/easy_ssd1306_raspberry_pi-0.0.1.tar.gz/easy_ssd1306_raspberry_pi-0.0.1/README.md
@@ -0,0 +1,1 @@
+For now this project is empty.
---
+++ packages/easy-ssd1306-raspberry-pi/easy_ssd1306_raspberry_pi-0.0.1.tar.gz/easy_ssd1306_raspberry_pi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easy_ssd1306_raspberry_pi",
+    version="0.0.1",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
